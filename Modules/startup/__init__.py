d87cedf5f9cc066b5fec7e33b229d935d3c7b30d
Neil Cook <neil.james.cook@gmail.com>
2017-10-13 17:11:08 -0400

    added PutFile and UpdateMaster functions

--- Modules/startup/__init__.py
+++ Modules/startup/__init__.py
@@ -14,19 +14,31 @@ Version 0.0.1
 from . import configsetup
 from . import startupfunctions
 from . import log
+from . import spirouCDB
 
-__all__ = ['ReadConfigFile', 'RunInitialStartup', 'RunStartup', 'wlog']
+__all__ = ['PutFile', 'ReadConfigFile', 'RunInitialStartup', 'RunStartup',
+           'UpdateMaster', 'wlog']
 __author__ = 'Neil Cook'
 __version__ = '0.0.1'
 
 # =============================================================================
 # Define functions
 # =============================================================================
+# Copies the "inputfile" to the calibDB folder
+PutFile = spirouCDB.put_file
+
 # Reads the config file and loads all variables into dictionary
 ReadConfigFile = configsetup.read_config_file
+
 # Runs the initial start up script (checking of parameters and title)
 RunInitialStartup = startupfunctions.run_inital_startup
+
 # Runs the start up script
 RunStartup = startupfunctions.run_startup
+
+# Updates (or creates) the calibDB with an entry or entries in the form:
+#    {key} {arg_night_name} {filename} {human_time} {unix_time}
+UpdateMaster = spirouCDB.update_datebase
+
 # The stdout/log file controller
 wlog = log.logger
