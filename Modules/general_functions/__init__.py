f8ac5a1cd2f7b0de0786fec9cef34209eabd89b1
Neil Cook <neil.james.cook@gmail.com>
2017-10-25 12:27:45 -0400

    Added 'BoxSmoothedImage, ConvertToE and FlipImage functions

--- Modules/general_functions/__init__.py
+++ Modules/general_functions/__init__.py
@@ -12,9 +12,16 @@ Created on 2017-10-12 15:51:49
 Version 0.0.1
 """
 from . import spirouFITS
-from . import image
+from . import spirouImage
 
-__all__ = ['CopyOriginalKeys', 'GetKey', 'GetKeys', 'ReadImage', 'ResizeImage',
+__all__ = ['BoxSmoothedImage',
+           'ConvertToE',
+           'CopyOriginalKeys',
+           'FlipImage ',
+           'GetKey',
+           'GetKeys',
+           'ReadImage',
+           'ResizeImage',
            'WriteImage']
 __author__ = 'Neil Cook'
 __version__ = '0.0.1'
@@ -23,12 +30,21 @@ __version__ = '0.0.1'
 # Define functions
 # =============================================================================
 
+# Produce a (box) smoothed image
+BoxSmoothedImage = spirouImage.smoothed_boxmean_image
+
+#
+ConvertToE = spirouImage.convert_to_e
+
 # Copies keys from hdr dictionary to hdict, if forbid_keys is True some
 #     keys will not be copies (defined in python code)
 CopyOriginalKeys = spirouFITS.copy_original_keys
 
 # Corrects an image for darks (using calibDB)
-CorrectForDark = image.correct_for_dark
+CorrectForDark = spirouImage.correct_for_dark
+
+# Flips the image in the x and/or the y direction
+FlipImage = spirouImage.flip_image
 
 # Looks for a key(s) in dictionary p, named 'name'
 #     if has_default sets value of key to 'default' if not found
@@ -41,7 +57,7 @@ GetKeys = spirouFITS.keyslookup
 ReadImage = spirouFITS.readimage
 
 # Resize an image based on a pixel values
-ResizeImage = image.resize
+ResizeImage = spirouImage.resize
 
 # Write the image to file and adds header file
 WriteImage = spirouFITS.writeimage
