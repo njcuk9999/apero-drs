18cf198f0e1115e814d7155f60f6ea19c34651d3
Neil Cook <neil.james.cook@gmail.com>
2017-11-08 18:10:03 -0500

    added doc string for extract and added ExtractABorder alias to init

--- SpirouDRS/spirouEXTOR/__init__.py
+++ SpirouDRS/spirouEXTOR/__init__.py
@@ -17,5 +17,37 @@ Version 0.0.0
 from . import spirouEXTOR
 
 
-# wrapper for performing extraction
 Extraction = spirouEXTOR.extract_wrapper
+"""
+
+:param p: dictionary, parameter dictionary, containing constants and
+          variables (at least 'ic_extopt', 'ic_extnbsig' and 'gain')
+
+          where 'ic_extopt' gives the extraction option
+                'ic_extnbsig' gives the distance away from center to
+                   extract out to +/-
+                'gain' is the image gain (used to convert from ADU/s to e-)
+
+:param image: numpy array (2D), the image
+:param pos: numpy array (1D), the position fit coefficients
+            size = number of coefficients for fit
+:param sig: numpy array (1D), the width fit coefficients
+            size = number of coefficients for fit
+:param tilt: bool, whether to extract using a tilt (not used yet)
+:param weighted: bool, whether to extract using weights (not used yet)
+
+:return spe: numpy array (1D), the extracted pixel values,
+             size = image.shape[1] (along the order direction)
+:return nbcos: int, zero in this case
+"""
+
+ExtractABorder = spirouEXTOR.extract_AB_order
+"""
+Perform the extraction on the AB fibers separately using the summation
+over constant range
+
+:param pp: dictionary, parameter dictionary
+:param lloc: dictionary, parameter dictionary containing the data
+:param order_num: int, the order number for this iteration
+:return lloc: dictionary, parameter dictionary containing the data
+"""
