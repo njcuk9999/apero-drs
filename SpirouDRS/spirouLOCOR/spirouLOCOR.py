4a4e9cc94b7a8277ccfc21afe50783ebc7d62848
Neil Cook <neil.james.cook@gmail.com>
2017-11-10 17:11:56 -0500

    moved measure_background_and_get_central_pixels, measure_box_min_max to spirouBACK

--- SpirouDRS/spirouLOCOR/spirouLOCOR.py
+++ SpirouDRS/spirouLOCOR/spirouLOCOR.py
@@ -130,60 +130,7 @@ def get_loc_coefficients(p, hdr=None):
     return loc
 
 
-def measure_background_and_get_central_pixels(pp, loc, image):
-    """
-    Takes the image and measure the background
 
-    :param pp: dictionary, parameter dictionary
-    :param loc: dictionary, localisation parameter dictionary
-    :param image: numpy array (2D), the image
-
-    :return ycc: the normalised values the central pixels
-    """
-    # clip the data - start with the ic_offset row and only
-    # deal with the central column column=ic_cent_col
-    y = image[pp['IC_OFFSET']:, pp['IC_CENT_COL']]
-    # Get the box-smoothed min and max for the central column
-    miny, maxy = measure_box_min_max(y, pp['IC_LOCNBPIX'])
-    # record the maximum signal in the central column
-    # QUESTION: Why the third biggest?
-    max_signal = np.sort(y)[-3]
-    # get the difference between max and min pixel values
-    diff_maxmin = maxy - miny
-    # normalised the central pixel values above the minimum amplitude
-    #   zero by miny and normalise by (maxy - miny)
-    #   Set all values below ic_min_amplitude to zero
-    max_amp = pp['IC_MIN_AMPLITUDE']
-    ycc = np.where(diff_maxmin > max_amp, (y - miny)/diff_maxmin, 0)
-    # get the normalised minimum values for those rows above threshold
-    #   i.e. good background measurements
-    normed_miny = miny/diff_maxmin
-    goodback = np.compress(ycc > pp['IC_LOCSEUIL'], normed_miny)
-    # measure the mean good background as a percentage
-    # (goodback and ycc are between 0 and 1)
-    mean_backgrd = np.mean(goodback) * 100
-    # Log the maximum signal and the mean background
-    WLOG('info', pp['log_opt'], ('Maximum flux/pixel in the spectrum: '
-                                 '{0:.1f} [e-]').format(max_signal))
-    WLOG('info', pp['log_opt'], ('Average background level: '
-                                 '{0:.2f} [%]').format(mean_backgrd))
-    # if in debug mode plot y, miny and maxy else just plot y
-    if pp['IC_DEBUG'] and pp['DRS_PLOT']:
-        sPlt.locplot_y_miny_maxy(y, miny, maxy)
-    if pp['DRS_PLOT']:
-        sPlt.locplot_y_miny_maxy(y)
-
-    # set function name (for source)
-    __funcname__ = '/measure_background_and_get_central_pixels()'
-    # Add to loc
-    loc['ycc'] = ycc
-    loc.set_source('ycc', __NAME__ + __funcname__)
-    loc['mean_backgrd'] = mean_backgrd
-    loc.set_source('mean_backgrd', __NAME__ + __funcname__)
-    loc['max_signal'] = max_signal
-    loc.set_source('max_signal', __NAME__ + __funcname__)
-    # return the localisation dictionary
-    return loc
 
 
 def find_order_centers(pp, image, loc, order_num):
@@ -206,7 +153,7 @@ def find_order_centers(pp, image, loc, order_num):
     # get constants from parameter dictionary
     locstep, centralcol = pp['IC_LOCSTEPC'], pp['IC_CENT_COL']
     ext_window, image_gap = pp['IC_EXT_WINDOW'], pp['IC_IMAGE_GAP']
-    sigdet, locthreshold = pp['ccdsigdet'], pp['IC_LOCSEUIL']
+    sigdet, locthreshold = pp['sigdet'], pp['IC_LOCSEUIL']
     widthmin = pp['IC_WIDTHMIN']
     nx2 = image.shape[1]
     # get columns (start from the center and work outwards right side first
@@ -759,46 +706,6 @@ def __test_smoothed_boxmean_image(image, size, row=1000, column=1000):
     if not plt.isinteractive():
         plt.show()
         plt.close()
-
-
-def measure_box_min_max(image, size):
-    """
-    Measure the minimum and maximum pixel value for each row using a box which
-    contains all pixels for rows:  row-size to row+size and all columns.
-
-    Edge pixels (0-->size and (image.shape[0]-size)-->image.shape[0] are
-    set to the values for row=size and row=(image.shape[0]-size)
-
-    :param image: numpy array (2D), the image
-    :param size: int, the half size of the box to use (half height)
-                 so box is defined from  row-size to row+size
-
-    :return min_image: numpy array (1D length = image.shape[0]), the row values
-                       for minimum pixel defined by a box of row-size to
-                       row+size for all columns
-    :retrun max_image: numpy array (1D length = image.shape[0]), the row values
-                       for maximum pixel defined by a box of row-size to
-                       row+size for all columns
-    """
-    # get length of rows
-    ny = image.shape[0]
-    # Set up min and max arrays (length = number of rows)
-    min_image = np.zeros(ny, dtype=float)
-    max_image = np.zeros(ny, dtype=float)
-    # loop around each pixel from "size" to length - "size" (non-edge pixels)
-    # and get the minimum and maximum of each box
-    for it in range(size, ny - size):
-        min_image[it] = np.min(image[it-size:it+size])
-        max_image[it] = np.max(image[it-size:it+size])
-
-    # deal with leading edge --> set to value at size
-    min_image[0:size] = min_image[size]
-    max_image[0:size] = max_image[size]
-    # deal with trailing edge --> set to value at (image.shape[0]-size-1)
-    min_image[ny-size:] = min_image[ny-size-1]
-    max_image[ny-size:] = max_image[ny-size-1]
-    # return arrays for minimum and maximum (box smoothed)
-    return min_image, max_image
 
 
 def image_localization_superposition(image, coeffs):
