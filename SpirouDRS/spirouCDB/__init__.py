c76c12aeecd780f6806d131f0a2c3046e151b497
Neil Cook <neil.james.cook@gmail.com>
2017-11-13 17:38:43 -0500

    added GetFile command to init

--- SpirouDRS/spirouCDB/__init__.py
+++ SpirouDRS/spirouCDB/__init__.py
@@ -17,14 +17,17 @@ Version 0.0.0
 from . import spirouCDB
 
 
-
+# copy calib database files to folder
 CopyCDBfiles = spirouCDB.copy_files
 
+# Gets the acquisition time for use in getting the database entries
 GetAcqTime = spirouCDB.get_acquision_time
-
 
 # Gets all entries from calibDB where unix time <= max_time
 GetDatabase = spirouCDB.get_database
+
+# Gets a filename from the calibDB
+GetFile = spirouCDB.get_file_name
 
 # Copies the "inputfile" to the calibDB folder
 PutFile = spirouCDB.put_file
