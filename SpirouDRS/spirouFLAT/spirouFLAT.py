80e1ee14a8404e10d283d46311171b829b52540e
Neil Cook <neil.james.cook@gmail.com>
2017-11-14 18:09:32 -0500

    first commit spirouFLAT.py added measure_blaze_for_order function

---
+++ SpirouDRS/spirouFLAT/spirouFLAT.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+# CODE NAME HERE
+
+# CODE DESCRIPTION HERE
+
+Created on 2017-11-14 at 15:37
+
+@author: cook
+
+
+
+Version 0.0.0
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+from astropy.io import fits
+from astropy.table import Table
+from astropy import units as u
+from tqdm import tqdm
+import warnings
+
+
+# =============================================================================
+# Define variables
+# =============================================================================
+
+# -----------------------------------------------------------------------------
+
+# =============================================================================
+# Define functions
+# =============================================================================
+def measure_blaze_for_order(y, fitdegree):
+    """
+    Measure the blaze function (for good pixels this is a polynomial fit of
+    order = fitdegree, for bad pixels = 1.0).
+
+    bad pixels are defined as less than or equal to zero
+
+    :param y: numpy array (1D), the extracted pixels for this order
+    :param fitdegree: int, the polynomial degree
+
+    :return blaze: numpy array (1D), size = len(y), the blaze function: for
+                   good pixels this is the value of the fit, for bad pixels the
+                   value = 1.0
+    """
+
+    # set up x range
+    x = np.arange(len(y))
+    # remove bad pixels
+    mask = y > 0
+    yc = y[mask]
+    xc = x[mask]
+    # do poly fit
+    coeffs = np.polyfit(xc, yc, deg=fitdegree)
+    # get the fit values for these coefficients
+    fity = np.polyval(coeffs, x)
+    # calculate the blaze as the fit values for all good pixels and 1 elsewise
+    blaze = np.where(mask, fity, 1.0)
+    # return blaze
+    return blaze
+
+# =============================================================================
+# Start of code
+# =============================================================================
+# Main code here
+if __name__ == "__main__":
+    # ----------------------------------------------------------------------
+    # print 'Hello World!'
+    print("Hello World!")
+
+# =============================================================================
+# End of code
+# =============================================================================
