4f76ed3e9acc4b5be6123a67059aad90b5160195
Neil Cook <neil.james.cook@gmail.com>
2017-11-22 17:46:15 -0500

    added aliases for ReNormCosmic2D and CalcRVdrift2D

--- SpirouDRS/spirouRV/__init__.py
+++ SpirouDRS/spirouRV/__init__.py
@@ -25,13 +25,20 @@ __NAME__ = 'spirouRV.__init__()'
 # Get version and author
 __version__ = spirouConfig.Constants.VERSION()
 __author__ = spirouConfig.Constants.AUTHORS()
-__all__ = ['DeltaVrms2D']
+__all__ = ['DeltaVrms2D', 'ReNormCosmic2D', 'CalcRVdrift2D']
 
 # =============================================================================
 # Function aliases
 # =============================================================================
 # Compute the photon noise uncertainty for all order
 DeltaVrms2D = spirouRV.delta_v_rms_2d
+
+# Correction of the cosmics and renormalisation by comparison with
+#    reference spectrum
+ReNormCosmic2D = spirouRV.renormalise_cosmic2d
+
+# Compute radial velocity drift between reference spectrum and current spectrum
+CalcRVdrift2D = spirouRV.calculate_RV_drifts_2D
 
 
 # =============================================================================
