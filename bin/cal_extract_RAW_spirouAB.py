d157ba9a01ec4b89f8f7c0deee9cf8a90bbbf651
Neil Cook <neil.james.cook@gmail.com>
2017-11-23 13:38:42 -0500

    wrapper around cal_extract_RAW_spirou to define AB as the fiber type

---
+++ bin/cal_extract_RAW_spirouAB.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+cal_extract_RAW_spirou
+
+# CODE DESCRIPTION HERE
+
+Created on 2017-10-12 at 15:21
+
+@author: cook
+
+
+
+Version 0.0.1
+"""
+import cal_extract_RAW_spirou
+from SpirouDRS import spirouConfig
+from SpirouDRS import spirouCore
+
+# =============================================================================
+# Define variables
+# =============================================================================
+# Name of program
+__NAME__ = 'cal_extract_RAW_spirouAB.py'
+# Get version and author
+__version__ = spirouConfig.Constants.VERSION()
+__author__ = spirouConfig.Constants.AUTHORS()
+# Get the parameter dictionary class
+ParamDict = spirouConfig.ParamDict
+# Get Logging function
+WLOG = spirouCore.wlog
+# Get plotting functions
+sPlt = spirouCore.sPlt
+
+# =============================================================================
+# Start of code
+# =============================================================================
+# Main code here
+if __name__ == "__main__":
+    # run cal_extract_RAW_spirou main with fibertype set
+    # (get other arguments from command line - sys.argv)
+    cal_extract_RAW_spirou.main(fiber_type='AB')
+
+# =============================================================================
+# End of code
+# =============================================================================
