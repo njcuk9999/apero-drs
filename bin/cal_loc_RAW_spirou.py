69cf34a45fdd94161496be7491b812082f5717e5
Neil Cook <neil.james.cook@gmail.com>
2017-12-11 15:27:05 -0500

    checked against old versions and updated edit date

--- bin/cal_loc_RAW_spirou.py
+++ bin/cal_loc_RAW_spirou.py
@@ -9,9 +9,9 @@ Created on 2017-10-12 at 15:21
 
 @author: cook
 
+Last modified: 2017-12-11 at 15:09
 
-
-Version 0.0.1
+Up-to-date with cal_loc_RAW_spirou AT-4 V47
 """
 from __future__ import division
 import numpy as np
