8905a171974a177d69c52945dd779a8084bbdce8
Neil Cook <neil.james.cook@gmail.com>
2018-01-10 16:32:37 -0500

    make ic_debug drs_debug==2

--- bin/cal_loc_RAW_spirou.py
+++ bin/cal_loc_RAW_spirou.py
@@ -157,7 +157,7 @@ def main(night_name=None, files=None):
     # log progress
     WLOG('', p['log_opt'], 'Searching order center on central column')
     # plot the minimum of ycc and ic_locseuil if in debug and plot mode
-    if p['DRS_DEBUG'] and p['DRS_PLOT']:
+    if p['DRS_DEBUG'] == 2 and p['DRS_PLOT']:
         sPlt.debug_locplot_min_ycc_loc_threshold(p, loc['ycc'])
     # find the central positions of the orders in the central
     posc_all = spirouLOCOR.FindPosCentCol(loc['ycc'], p['IC_LOCSEUIL'])
