443db7f29007fb5be7f7eb4d6451b3dbcb4aac46
Neil Cook <neil.james.cook@gmail.com>
2018-01-22 16:21:15 -0500

    fixed typo in log message

--- INTROOT/bin/cal_loc_RAW_spirou.py
+++ INTROOT/bin/cal_loc_RAW_spirou.py
@@ -446,7 +446,7 @@ def main(night_name=None, files=None):
     # ----------------------------------------------------------------------
     # End Message
     # ----------------------------------------------------------------------
-    wmsg = 'Recipe {0} has been succesfully completed'
+    wmsg = 'Recipe {0} has been successfully completed'
     WLOG('info', p['log_opt'], wmsg.format(p['program']))
 
     return locals()
