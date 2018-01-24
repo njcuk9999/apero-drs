2599e41092c35dc52555b5b16481fc010abe8c6e
Neil Cook <neil.james.cook@gmail.com>
2018-01-24 17:27:34 -0500

    better error handling + reporting

--- INTROOT/SpirouDRS/spirouConfig/spirouConfigFile.py
+++ INTROOT/SpirouDRS/spirouConfig/spirouConfigFile.py
@@ -1,9 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-# CODE NAME HERE
-
-# CODE DESCRIPTION HERE
+Spirou configuration module (file loading specifics)
 
 Created on 2018-01-10 at 11:38
 
@@ -15,7 +13,7 @@ from __future__ import division
 import numpy as np
 import os
 import pkg_resources
-
+import sys
 
 # =============================================================================
 # Define variables
@@ -29,12 +27,70 @@ __release__ = 'Unknown'
 __date__ = 'Unknown'
 # -----------------------------------------------------------------------------
 
+# =============================================================================
+# Define Custom classes
+# =============================================================================
+class ConfigException(Exception):
+    """Raised when config file is incorrect"""
+    pass
+
 
 # =============================================================================
 # Define functions
 # =============================================================================
 def read_config_file(package, configfolder, configfile, config_file=None,
                      return_raw=True):
+    """
+    Read a configruation file called "configfile" at relative path
+    "configfolder" (relative to "package" path)
+
+            i.e. if package is X  is located at /home/bin/packageX
+            and the config file is located at /home/bin/packageX/config/Y.txt
+
+            then package = "packageX"
+                 configfolder = "./config/"
+                 configfile = "Y.txt"
+
+    Note the config file should be in the following format:
+
+                # BEGINNING OF CONFIG FILE
+
+                # Comment
+                key1 = value1
+                key2 = value2
+
+                # Comment
+                key3 = value3
+
+                # END OF CONFIG FILE
+
+    (i.e. comments start with #, blank lines are ignored and the resulting
+     key value pairs would be as follows:
+
+        keys = ['key1', 'key2', 'key3']
+        values = ['value1', 'value2', 'value3']
+
+        or if return_raw = False
+
+        {'key1':'value1', 'key2':'value2', 'key3':'value3'}
+
+    :param package: string, python package name
+                    i.e. import packageX
+                      --> package = "packageX"
+
+    :param configfolder: string, the relative path of the configuration file
+                         (relative to the package)
+
+    :param configfile: string, the configuration filename (filename only)
+
+    :param config_file: string or None, if not None then overrides the path
+                        created from package/configfolder/configfile and uses
+                        "config_file" as the path to read the config file
+
+    :param return_raw: bool, if True returns key and value pairs, if False
+                       returns a dictionary of key value pairs
+    :return:
+    """
     if config_file is None:
         # get config file path
         config_file = get_default_config_file(package, configfolder,
@@ -54,7 +110,10 @@ def gettxt(filename):
         comments have a '#' at the start
         format of variables:   key = value
 
-    :param filename:
+    If file cannot be read will generate an IOError
+
+    :param filename: string, the filename (+ absolute path) of file to open
+
     :return keys: list of strings, upper case strings for each variable
     :return values: list of strings, value of each key
     """
@@ -63,11 +122,7 @@ def gettxt(filename):
         raw = np.genfromtxt(filename, comments="#", delimiter='=',
                             dtype=str).astype(str)
     except Exception:
-        try:
-            raw = read_lines(filename, comments='#', delimiter='=')
-        except Exception:
-            print('FATAL ERROR: Config file {0} is corrupted (invalid '
-                  'charaters or encoding')
+        raw = read_lines(filename, comments='#', delimiter='=')
 
     # check that we have open config file correctly
     try:
@@ -89,13 +144,24 @@ def gettxt(filename):
 
 
 def read_lines(filename, comments='#', delimiter=' '):
+
+    func_name = __NAME__ + '.read_lines()'
     # manually open file (slow)
-    f = open(filename, 'r')
-    lines = f.readlines()
+    try:
+        # open the file
+        f = open(filename, 'r')
+        # read the lines
+        lines = f.readlines()
+        # close the opened file
+        f.close()
+    except Exception as e:
+        emsg = ('\n\t\t {0}: File "{1}" cannot be read by {2}. '
+                '\n\t\t Error was: {3}')
+        raise ConfigException(emsg.format(type(e), filename, func_name, e))
     # valid lines
     raw = []
     # loop around lines
-    for line in lines:
+    for l, line in enumerate(lines):
         # remove line endings and blanks at start and end
         line = line.replace('\n', '').strip()
         # do not include blank lines
@@ -106,7 +172,19 @@ def read_lines(filename, comments='#', delimiter=' '):
             continue
         else:
             # append to raw
-            raw.append(line.split(delimiter))
+            try:
+                key, value = line.split(delimiter)
+            except ValueError as e:
+                emsg = ('\n\t\t Wrong format for line {0} in file {1}'
+                        '\n\t\t Lines must be "key" = "value"'
+                        '\n\t\t Where "key" and "value" are a valid python '
+                        'strings and contains no equal signs')
+                raise ConfigException(emsg.format(l+1, filename, line))
+
+            raw.append([key, value])
+    # check that raw has entries
+    if len(raw) == 0:
+        raise ConfigException("No valid lines found in {0}".format(filename))
     # return raw
     return np.array(raw)
 
@@ -120,14 +198,30 @@ def get_relative_folder(package, folder):
     :param folder: string, the relative path of the configuration folder
 
     :return data: string, the absolute path and filename of the default config
-                         file
+                  file
     """
-    init = pkg_resources.resource_filename(package, '__init__.py')
+    func_name = __NAME__ + '.get_relative_folder()'
+    # get the package.__init__ file path
+    try:
+        init = pkg_resources.resource_filename(package, '__init__.py')
+    except ImportError:
+        emsg = 'Package name = "{0}" is invalid (function = {1})'
+        raise ConfigException(emsg.format(package, func_name))
     # Get the config_folder from relative path
     current = os.getcwd()
-    os.chdir(os.path.dirname(init))
+    # get directory name of folder
+    dirname = os.path.dirname(init)
+    # change to directory in init
+    os.chdir(dirname)
+    # get the absolute path of the folder
     data_folder = os.path.abspath(folder)
+    # change back to working dir
     os.chdir(current)
+    # test that folder exists
+    if not os.path.exists(data_folder):
+        emsg = 'Folder "{0}" does not exist in {1}'
+        eargs = [os.path.basename(data_folder), os.path.dirname(data_folder)]
+        raise ConfigException(emsg.format(*eargs))
     # return the absolute data_folder path
     return data_folder
 
@@ -144,14 +238,15 @@ def get_default_config_file(package, configfolder, configfile):
     :return config_file: string, the absolute path and filename of the
                          default config file
     """
-    init = pkg_resources.resource_filename(package, '__init__.py')
-    # Get the config_folder from relative path
-    current = os.getcwd()
-    os.chdir(os.path.dirname(init))
-    config_folder = os.path.abspath(configfolder)
-    os.chdir(current)
+    func_name = __NAME__ + '.get_default_config_file()'
+    # get the config folder
+    config_folder = get_relative_folder(package, configfolder)
     # Get the config file path
     config_file = os.path.join(config_folder, configfile)
+    # test that config file exists
+    if not os.path.exists(config_file):
+        emsg = 'Config file "{0}" not found at "{1}" (function={2})'
+        raise ConfigException(emsg.format(configfile, config_folder, func_name))
     # return the config file path
     return config_file
 
@@ -175,15 +270,6 @@ def evaluate_value(value):
     except Exception:
         return str(value)
 
-
-# =============================================================================
-# Start of code
-# =============================================================================
-# Main code here
-if __name__ == "__main__":
-    # ----------------------------------------------------------------------
-    # print 'Hello World!'
-    print("Hello World!")
 
 # =============================================================================
 # End of code
