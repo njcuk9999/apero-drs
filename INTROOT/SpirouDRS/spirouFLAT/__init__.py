f1005a32c8ecc1a087481f89f686526c78d0a808
Neil Cook <neil.james.cook@gmail.com>
2018-01-26 17:03:21 -0500

    update doc strings

--- INTROOT/SpirouDRS/spirouFLAT/__init__.py
+++ INTROOT/SpirouDRS/spirouFLAT/__init__.py
@@ -1,17 +1,12 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-# CODE NAME HERE
-
-# CODE DESCRIPTION HERE
+initialization code for Spirou flat fielding module
 
 Created on 2017-11-14 at 15:37
 
 @author: cook
 
-
-
-Version 0.0.0
 """
 
 from SpirouDRS import spirouConfig
@@ -34,6 +29,19 @@ __all__ = ['MeasureBlazeForOrder']
 # Function aliases
 # =============================================================================
 MeasureBlazeForOrder = spirouFLAT.measure_blaze_for_order
+"""
+Measure the blaze function (for good pixels this is a polynomial fit of
+order = fitdegree, for bad pixels = 1.0).
+
+bad pixels are defined as less than or equal to zero
+
+:param y: numpy array (1D), the extracted pixels for this order
+:param fitdegree: int, the polynomial degree
+
+:return blaze: numpy array (1D), size = len(y), the blaze function: for
+               good pixels this is the value of the fit, for bad pixels the
+               value = 1.0
+"""
 
 # =============================================================================
 # End of code
