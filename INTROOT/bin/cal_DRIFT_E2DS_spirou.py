84d71f2544011ce9f97dbd2ea6e1ad96d0dae82f
Neil Cook <neil.james.cook@gmail.com>
2018-02-13 15:17:58 -0500

    updated shebang and __main__ exiting

--- INTROOT/bin/cal_DRIFT_E2DS_spirou.py
+++ INTROOT/bin/cal_DRIFT_E2DS_spirou.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 """
 cal_DRIFT_RAW_spirou.py [night_directory] [Reference file name]
@@ -342,16 +342,18 @@ def main(night_name=None, reffile=None):
     # ----------------------------------------------------------------------
     wmsg = 'Recipe {0} has been successfully completed'
     WLOG('info', p['log_opt'], wmsg.format(p['program']))
+    # return a copy of locally defined variables in the memory
+    return dict(locals())
 
-    return locals()
 
 # =============================================================================
 # Start of code
 # =============================================================================
 if __name__ == "__main__":
     # run main with no arguments (get from command line - sys.argv)
-    locals = main()
-
+    ll = main()
+    # exit message
+    spirouStartup.Exit(ll)
 
 # =============================================================================
 # End of code
