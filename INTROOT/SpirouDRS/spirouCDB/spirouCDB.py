61785b20d102cade1906e95a3e184bc483d839ac
Neil Cook <neil.james.cook@gmail.com>
2018-04-13 16:16:37 -0400

    Issue #185 and #186 - DATE_FMT_HEADER now requires p to function

--- INTROOT/SpirouDRS/spirouCDB/spirouCDB.py
+++ INTROOT/SpirouDRS/spirouCDB/spirouCDB.py
@@ -118,7 +118,7 @@ def update_datebase(p, keys, filenames, hdrs, timekey=None):
             # get the header time
             header_time = hdr[acqtime_key]
             # get the header format for dates
-            header_fmt = spirouConfig.Constants.DATE_FMT_HEADER()
+            header_fmt = spirouConfig.Constants.DATE_FMT_HEADER(p)
             # get the calib DB format for dates
             calibdb_fmt = spirouConfig.Constants.DATE_FMT_CALIBDB()
             # get the unix time from header time (header time assumes GMT/UTC)
@@ -264,7 +264,7 @@ def get_database(p, max_time=None, update=False):
     # check that max_time is a valid unix time (i.e. a float)
     try:
         # get the header format for dates
-        header_fmt = spirouConfig.Constants.DATE_FMT_HEADER()
+        header_fmt = spirouConfig.Constants.DATE_FMT_HEADER(p)
         # get the unix time from header time (assume max_time is in GMT/UTC)
         max_time = spirouMath.stringtime2unixtime(max_time, header_fmt)
     except ValueError:
