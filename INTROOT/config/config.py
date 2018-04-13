8d755dcde6b5711a1cf3af28a91e7f8f300097f4
Neil Cook <neil.james.cook@gmail.com>
2018-04-13 16:09:42 -0400

    Issue #186 - modified DRS_UCONFIG for H2RG/H4RG configs

--- INTROOT/config/config.py
+++ INTROOT/config/config.py
@@ -46,7 +46,8 @@ DRS_DATA_WORKING = '/drs/spirou/data/tmp'
 #   Define the user directory
 #        (overwritten by DRS_UCONFIG environmental variable)
 #      If only using default config file (i.e this file) comment this line out
-DRS_UCONFIG = '~/spirou_config/'
+DRS_UCONFIG = '~/spirou_config_H2RG/'
+# DRS_UCONFIG = '~/spirou_config_H4RG/'
 
 # -----------------------------------------------------------------------------
 # Logging settings
