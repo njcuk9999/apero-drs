9cc9bd675f2c46b612de3e5d51bba0990ecc9db1
FrancoisBouchy <francois.bouchy@unige.ch>
2018-05-25 15:06:13 +0200

    Add targets keywords + Date of observations for cal_CCF_E2DS_spirou

--- INTROOT/SpirouDRS/spirouConfig/spirouKeywords.py
+++ INTROOT/SpirouDRS/spirouConfig/spirouKeywords.py
@@ -115,7 +115,14 @@ USE_KEYS = ['kw_ACQTIME_KEY',
             'kw_EM_TILT',
             'kw_EM_MINWAVE',
             'kw_EM_MAXWAVE',
-            'kw_EM_TRASCUT']
+            'kw_EM_TRASCUT',
+            'kw_DATE_OBS',
+            'kw_UTC_OBS',
+            'kw_OBJRA',
+            'kw_OBJDEC',
+            'kw_OBJEQUIN',
+            'kw_OBJRAPM',
+            'kw_OBJDECPM']
 
 # MUST UPDATE THIS IF VARIABLES FROM CONFIG FILES USED
 USE_PARAMS = ['DRS_NAME',
@@ -205,6 +212,17 @@ if p['IC_IMAGE_TYPE'] == 'H4RG':
     kw_ACQTIME_KEY_UNIX = ['MJDATE', None, '']
 else:
     kw_ACQTIME_KEY_UNIX = ['ACQTIME', None, '']
+
+kw_DATE_OBS = ['DATE-OBS', None, '']
+kw_UTC_OBS = ['UTC-OBS', None, '']
+kw_OBJRA = ['OBJRA', None, '']
+kw_OBJDEC = ['OBJDEC', None, '']
+kw_OBJEQUIN = ['OBJEQUIN', None, '']
+kw_OBJRAPM = ['OBJRAPM', None, '']
+kw_OBJDECPM = ['OBJDECPM', None, '']
+
+
+
 
 # define the read noise HEADER key a.k.a sigdet (used to get value only)
 kw_RDNOISE = ['RDNOISE', None, '']
