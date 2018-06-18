6e7133098926706e06261b4098b207bbdbc64716
Neil Cook <neil.james.cook@gmail.com>
2018-06-18 14:40:34 -0400

    Issue #330 - fix setup and a few other minor (run tested correction)

--- INTROOT/bin/pol_spirou.py
+++ INTROOT/bin/pol_spirou.py
@@ -39,6 +39,14 @@ ParamDict = spirouConfig.ParamDict
 # Get plotting functions
 sPlt = spirouCore.sPlt
 
+# TODO: Remove test
+# import sys
+# sys.argv = [__NAME__, 'TEST/20180528', '2279383o_pp_e2dsff_A.fits',
+#             '2279384o_pp_e2dsff_B.fits', '2279386o_pp_e2dsff_A.fits',
+#             '2279383o_pp_e2dsff_B.fits', '2279385o_pp_e2dsff_A.fits',
+#             '2279386o_pp_e2dsff_B.fits', '2279384o_pp_e2dsff_A.fits',
+#             '2279385o_pp_e2dsff_B.fits']
+
 
 # =============================================================================
 # Define functions
@@ -59,6 +67,7 @@ def main(night_name=None, files=None):
     :return ll: dictionary, containing all the local variables defined in
                 main
     """
+
     # ----------------------------------------------------------------------
     # Set up
     # ----------------------------------------------------------------------
@@ -67,33 +76,35 @@ def main(night_name=None, files=None):
     # get parameters from configuration files and run time arguments
     p = spirouStartup.LoadArguments(p, night_name, files,
                                     mainfitsdir='reduced')
-
-    # set up polar storage
-    polardict = ParamDict()
-    # set up data storage
-    loc = ParamDict()
+    p = spirouStartup.InitialFileSetup(p, recipe=__NAME__, calibdb=True)
 
     # ----------------------------------------------------------------------
     # Loop through files, identify and sort files and identify fiber types
     # ----------------------------------------------------------------------
+    # set up polar storage
+    polardict = ParamDict()
+    # sort files
     polardict = spirouPOLAR.SortPolarFiles(p, polardict)
 
     # ----------------------------------------------------------------------
     # Load the input polarimetry data and check if provided data are
     #    sufficient for polarimetry calculation
     # ----------------------------------------------------------------------
-    loc = spirouPOLAR.LoadPolarData(p, polardict, loc)
+    # set up data storage
+    loc = ParamDict()
+    # load files
+    p, loc = spirouPOLAR.LoadPolarData(p, polardict, loc)
 
     # ------------------------------------------------------------------
     # Read wavelength solution
     # ------------------------------------------------------------------
-    loc['WAVE'] = spirouImage.ReadWaveFile(p, p['HDR'])
+    loc['WAVE'] = spirouImage.ReadWaveFile(p, loc['HDR'])
     loc.set_source('WAVE', __NAME__ + '/main() + /spirouImage.ReadWaveFile')
 
     # ----------------------------------------------------------------------
     # Polarimetry computation
     # ----------------------------------------------------------------------
-    polardict = spirouPOLAR.CalculatePolarimetry(p, loc)
+    loc = spirouPOLAR.CalculatePolarimetry(p, loc)
 
     # ----------------------------------------------------------------------
     # Calculate continuum (for plotting)
@@ -150,7 +161,7 @@ def main(night_name=None, files=None):
     wmsg = 'Recipe {0} has been successfully completed'
     WLOG('info', p['LOG_OPT'], wmsg.format(p['PROGRAM']))
     
-    # return a copy of locally defined variables in the memory
+    # # return a copy of locally defined variables in the memory
     return dict(locals())
 
 
