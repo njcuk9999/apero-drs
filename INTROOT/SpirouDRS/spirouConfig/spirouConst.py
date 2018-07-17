3d6eea6d93ec990a2bedcbb9fbae536b2dd62504
njcuk9999 <neil.james.cook@gmail.com>
2018-07-17 06:12:37 -0400

    update date/version/changelog

--- INTROOT/SpirouDRS/spirouConfig/spirouConst.py
+++ INTROOT/SpirouDRS/spirouConfig/spirouConst.py
@@ -24,10 +24,10 @@ from . import spirouConfigFile
 # Name of program
 __NAME__ = 'spirouConst.py'
 # Get version and author
-__version__ =  '0.2.67'
+__version__ =  '0.2.68'
 __author__ = 'N. Cook, F. Bouchy, E. Artigau, , M. Hobson, C. Moutou, I. Boisse, E. Martioli'
 __release__ = 'alpha pre-release'
-__date__ =  '2018-07-16'
+__date__ =  '2018-07-17'
 
 
 # =============================================================================
