1b14bb8245e19943e8256e0cdacdf3e16d5474ff
Neil Cook <neil.james.cook@gmail.com>
2018-09-18 18:42:14 -0400

    test_recipe.py - continue work on getting new input method to work

--- INTROOT/SpirouDRS/spirouStartup/test_recipe.py
+++ INTROOT/SpirouDRS/spirouStartup/test_recipe.py
@@ -23,13 +23,13 @@ from SpirouDRS import spirouStartup
 # =============================================================================
 # Define variables
 # =============================================================================
-__NAME__ = 'cal_DARK_spirou.py'
+__NAME__ = 'test.py'
 # -----------------------------------------------------------------------------
 
 # =============================================================================
 # Define functions
 # =============================================================================
-def main(**kwargs):
+def main(directory=None, ufiles=None, **kwargs):
 
     # TODO: New setup, including:
     # TODO:    - loading config/constants
@@ -38,8 +38,14 @@ def main(**kwargs):
     # TODO:    - showing header/paths/sys info
     # TODO:    - checking files based on inputs (all in **kwargs)
 
-    for kwarg in kwargs:
-        print('{0} = {1}'.format(kwarg, kwargs[kwarg]))
+    # assign function calls
+    fkwargs = dict(directory=directory, ufiles=ufiles, **kwargs)
+    # deal with command line inputs / function call inputs
+    kwargs = spirouStartup.spirouStartup2.input_setup(__NAME__, fkwargs)
+
+
+    for kwarg in kwargs['INPUT']:
+        print('{0} = {1}'.format(kwarg, kwargs['INPUT'][kwarg]))
 
     # return a copy of locally defined variables in the memory
     return dict(locals())
@@ -48,10 +54,8 @@ def main(**kwargs):
 # Start of code
 # =============================================================================
 if __name__ == "__main__":
-    # deal with command line inputs
-    kwargs = spirouStartup.spirouStartup2.input_setup(__NAME__)
     # run main with no arguments (get from command line - sys.argv)
-    ll = main(**kwargs)
+    ll = main()
     # exit message if in debug mode
     spirouStartup.Exit(ll, has_plots=False)
 
