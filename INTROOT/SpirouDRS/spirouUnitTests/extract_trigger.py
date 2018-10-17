f43d4563a08746a3c245ea545956abb8777f1abb
Neil Cook <neil.james.cook@gmail.com>
2018-10-17 17:00:25 -0400

    extract_trigger.py - first working version

--- INTROOT/SpirouDRS/spirouUnitTests/extract_trigger.py
+++ INTROOT/SpirouDRS/spirouUnitTests/extract_trigger.py
@@ -60,6 +60,8 @@ __date__ = spirouConfig.Constants.LATEST_EDIT()
 __release__ = spirouConfig.Constants.RELEASE()
 # Get Logging function
 WLOG = spirouCore.wlog
+# Get plotting functions
+sPlt = spirouCore.sPlt
 # Get param dictionary
 ParamDict = spirouConfig.ParamDict
 # skip found files
@@ -71,17 +73,22 @@ RAW_CODES = ['a.fits', 'c.fits', 'd.fits', 'f.fits', 'o.fits']
 # =============================================================================
 # Define functions
 # =============================================================================
-def recipe_lookup(key):
+def recipe_lookup(p, key):
     lookup = dict()
     lookup['cal_preprocess_spirou'] = cal_preprocess_spirou
-    lookup['cal_BAXPIX_spirou'] = cal_BADPIX_spirou.main
+    lookup['cal_BADPIX_spirou'] = cal_BADPIX_spirou.main
     lookup['cal_DARK_spirou'] = cal_DARK_spirou.main
     lookup['cal_loc_RAW_spirou'] = cal_loc_RAW_spirou.main
     lookup['cal_SLIT_spirou'] = cal_SLIT_spirou.main
     lookup['cal_SHAPE_spirou'] = cal_SHAPE_spirou.main
     lookup['cal_FF_RAW_spirou'] = cal_FF_RAW_spirou.main
     lookup['cal_extract_RAW_spirou'] = cal_extract_RAW_spirou.main
-    return lookup[key]
+    if key not in lookup:
+        emsg = 'Recipe {0} not found in lookup table. Recipe unsupported.'
+        WLOG('error', p['LOG_OPT'], emsg.format(key))
+        return 1
+    else:
+        return lookup[key]
 
 
 def find_all_raw_files(p):
@@ -167,6 +174,88 @@ def get_night_name(path, filelist):
     return night_names, filenames
 
 
+def get_control_index(control, index_file, recipe):
+    # load index file
+    index = Table.read(index_file)
+    # mask control file by recipe name
+    cmask = np.array(control['Recipe']) == recipe
+    control = control[cmask]
+    # get valied files from index
+    if 'None' in control['dprtype']:
+        vmask = np.ones(len(index), dtype=bool)
+    else:
+        vmask = np.in1d(index['DPRTYPE'], control['dprtype'])
+    # apply mask
+    vindex = index[vmask]
+    # return control and vindex
+    return control, vindex
+
+
+def get_file_args(p, control, vindex, recipe):
+    # get the files in index that match the correct arguments
+    args = dict()
+    # loop around control
+    for row in range(len(control)):
+        # get parameters from control
+        number = control['number'][row]
+        dprtype = control['dprtype'][row]
+        # construct a mask of files
+        if dprtype == 'None':
+            filemask = np.ones(len(vindex), dtype=bool)
+        else:
+            filemask = dprtype == vindex['DPRTYPE']
+        # check that we have some files
+        if np.sum(filemask) == 0:
+            wmsg = 'No files found for recipe "{0}" with key {1}'
+            wargs = [recipe, dprtype]
+            WLOG('warning', p['LOG_OPT'], wmsg.format(*wargs))
+        # construct file number
+        argnumber = 'FILE{0}'.format(number)
+        # make selected files into a list of strings
+        values = []
+        for value in vindex['FILENAME'][filemask]:
+            values.append(str(value))
+        # add to args dictionary
+        if argnumber not in args:
+            args[argnumber] = list(values)
+        else:
+            args[argnumber] += list(values)
+    # return args
+    return args
+
+
+def manage_runs(p, lls, errors, combinations, recipe):
+    # get command
+    command = recipe_lookup(p, recipe)
+    # loop around combinations
+    for it, combination in enumerate(combinations):
+        # log progress
+        wmsgs = [spirouStartup.spirouStartup.HEADER,
+                 spirouStartup.spirouStartup.HEADER]
+        wargs = [recipe, it + 1, len(combinations)]
+        wmsgs.append(' TRIGGER {0} File {1} of {2}'.format(*wargs))
+        wmsgs.append(spirouStartup.spirouStartup.HEADER)
+        wmsgs.append(spirouStartup.spirouStartup.HEADER)
+        WLOG('warning', p['LOG_OPT'], wmsgs)
+        # run command
+        try:
+            ll = command(*list(combination))
+            sPlt.closeall()
+        except Exception as e:
+            # log error
+            emsgs = ['Error occured']
+            emsgs.append('{0}'.format(e))
+            # push to ll
+            pp = ParamDict()
+            pp['RECIPE'] = recipe
+            pp['NIGHT_NAME'] = night_name
+            pp['ARGS'] = combination
+            pp['ERROR'] = emsgs
+            ll = dict(p=pp)
+            errors.append(ll)
+        lls.append(ll)
+    return lls, errors
+
 
 # =============================================================================
 # trigger functions
@@ -209,78 +298,45 @@ def trigger_preprocess(p, filelist):
 
 
 def trigger_main(p, loc, recipe):
+    """
 
+    :param p:
+    :param loc:
+    :param recipe:
+    :return:
+    """
     night_names = loc['INDEX_NIGHTNAME']
-    index_files = loc['INDEX_FILES']
+    index_files = loc['RAW_INDEX_FILES']
+    fullcontrol = loc['CONTROL']
+    # log progress
+    wmsgs = [spirouStartup.spirouStartup.HEADER,
+             spirouStartup.spirouStartup.HEADER]
+    wmsgs.append(' TRIGGER RECIPE: {0}'.format(recipe))
+    wmsgs.append(spirouStartup.spirouStartup.HEADER)
+    wmsgs.append(spirouStartup.spirouStartup.HEADER)
+    WLOG('warning', p['LOG_OPT'], wmsgs)
     # loop through index files
-    lls = []
+    lls, errors = [], []
     for it, index_file in enumerate(index_files):
-        # log progress
-        wmsgs = [spirouStartup.spirouStartup.HEADER,
-                 spirouStartup.spirouStartup.HEADER]
-        wmsgs.append(' TRIGGER RECIPE: {0}'.format(recipe))
-        wmsgs.append(spirouStartup.spirouStartup.HEADER)
-        wmsgs.append(spirouStartup.spirouStartup.HEADER)
-        # load index file
-        index = Table.read(index_file)
-        # get night_name
+        # Get the night name for this recipes
         night_name = night_names[it]
-        # mask control file by recipe name
-        cmask = np.array(p['CONTROL']['Recipe']) == recipe
-        control = p['CONTROL'][cmask]
+        # get the control and index for this
+        control, vindex = get_control_index(fullcontrol, index_file, recipe)
+        # get number of file args expected for this recipe
         file_args_expected = np.max(control['number'])
-
-        # get valied files from index
-        vmask = np.in1d(index['DPRTYPE'], control['dprtype'])
-        vindex = index[vmask]
-
-        # get the files in index that match the correct arguments
-        args = dict()
-        # loop around control
-        for row in range(len(control)):
-            # get parameters from control
-            number = control['number'][row]
-            dprtype = control['dprtype'][row]
-            # construct a mask of files
-            filemask = dprtype == vindex['DPRTYPE']
-            # check that we have some files
-            if np.sum(filemask) == 0:
-                print("ERROR")
-            # construct file number
-            argnumber = 'FILE{0}'.format(number)
-            if argnumber not in args:
-                args[argnumber] = list(vindex['FILENAME'][filemask])
-            else:
-                args[argnumber] += list(vindex['FILENAME'][filemask])
+        # get the files expected
+        file_args = get_file_args(p, control, vindex, recipe)
         # make combinations of files
         arguments = [[night_name]]
         # add files
         for it in range(file_args_expected):
-            arguments.append(args['FILE{0}'.format(it + 1)])
+            arguments.append(file_args['FILE{0}'.format(it + 1)])
         combinations = list(itertools.product(*arguments))
+        # manage the running of this recipe
+        lls, errors = manage_runs(p, lls, errors, combinations, recipe)
+    # return local spaces and errors
+    return lls, errors
 
-        # get command
-        command = recipe_lookup(recipe)
-
-        # loop around combinations
-        for it, combination in enumerate(combinations):
-            # log progress
-            wmsgs = [spirouStartup.spirouStartup.HEADER,
-                     spirouStartup.spirouStartup.HEADER]
-            wargs = [recipe, it + 1, len(combinations)]
-            wmsgs.append(' TRIGGER {0} File {1} of {2}'.format(*wargs))
-            wmsgs.append(spirouStartup.spirouStartup.HEADER)
-            wmsgs.append(spirouStartup.spirouStartup.HEADER)
-            # run command
-            print([command] + list(combination))
-
-
-            try:
-                ll = command(*combination)
-            except Exception as e:
-                pp = ParamDict()
-
-    #
 
 # =============================================================================
 # main function
@@ -307,7 +363,7 @@ if True:
     if SKIP_DONE:
         raw_files = skip_done_raw_files(p, raw_files)
     # pre-process remaining files
-    pp_lls = trigger_preprocess(p, raw_files)
+    # pp_lls = trigger_preprocess(p, raw_files)
 
     # ----------------------------------------------------------------------
     # Load the recipe_control
@@ -318,9 +374,12 @@ if True:
     # ----------------------------------------------------------------------
     # Find index_files
     # ----------------------------------------------------------------------
-    raw_index_files = find_all_index_files(p)
-    gout = get_night_name(p['DRS_DATA_WORKING'], raw_index_files)
+    loc['RAW_INDEX_FILES'] = find_all_index_files(p)
+    gout = get_night_name(p['DRS_DATA_WORKING'], loc['RAW_INDEX_FILES'])
     loc['INDEX_NIGHTNAME'], loc['INDEX_FILES'] = gout
+    # set sources
+    keys = ['RAW_INDEX_FILES', 'INDEX_NIGHTNAME', 'INDEX_FILES']
+    loc.set_sources(keys, main_name)
 
     # ----------------------------------------------------------------------
     # Run triggers
@@ -328,17 +387,17 @@ if True:
     # 1. cal_BADPIX_spirou.py
     badpix_lls = trigger_main(p, loc, recipe='cal_BADPIX_spirou')
     # 2. cal_DARK_spirou.py
-    dark_lls = trigger_main(p, loc)
+    dark_lls = trigger_main(p, loc, recipe='cal_DARK_spirou')
     # 3. cal_loc_RAW_spirou.py
-    loc_lls = trigger_main(p, loc)
+    loc_lls = trigger_main(p, loc, recipe='cal_loc_RAW_spirou')
     # 4. cal_SLIT_spirou.py
-    slit_lls = trigger_main(p, loc)
+    slit_lls = trigger_main(p, loc, recipe='cal_SLIT_spirou')
     # 5. cal_SHAPE_spirou.py
-    shape_lls = trigger_main(p, loc)
+    shape_lls = trigger_main(p, loc, recipe='cal_SHAPE_spirou')
     # 6. cal_FF_RAW_spirou.py
-    flat_lls = trigger_main(p, loc)
+    flat_lls = trigger_main(p, loc, recipe='cal_FF_RAW_spirou')
     # 7. cal_extract_RAW_spirou.py
-    ext_lls = trigger_main(p, loc)
+    ext_lls = trigger_main(p, loc, recipe='cal_extract_RAW_spirou')
 
 
 
