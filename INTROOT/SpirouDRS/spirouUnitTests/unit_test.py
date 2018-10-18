73512093d70241129f04cf61e48db7b1de513849
Neil Cook <neil.james.cook@gmail.com>
2018-10-18 17:29:47 -0400

    unit_test.py - fix comment

--- INTROOT/SpirouDRS/spirouUnitTests/unit_test.py
+++ INTROOT/SpirouDRS/spirouUnitTests/unit_test.py
@@ -102,7 +102,7 @@ def main(runname=None, quiet=False):
     runs = spirouUnitTests.get_runs(p, rparams, rfile)
 
     # ----------------------------------------------------------------------
-    # Get runs
+    # Run runs
     # ----------------------------------------------------------------------
     # storage for times
     times = OrderedDict()
