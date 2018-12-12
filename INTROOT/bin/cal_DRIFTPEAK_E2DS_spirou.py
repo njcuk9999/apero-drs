be5cbc4e5bd03e71f8bb1aca6320118128d0ddfe
Neil Cook <neil.james.cook@gmail.com>
2018-12-12 13:36:30 -0500

    cal_DRIFTPEAK_E2DS_spirou.py - pep8 clean up of Francois branch

--- INTROOT/bin/cal_DRIFTPEAK_E2DS_spirou.py
+++ INTROOT/bin/cal_DRIFTPEAK_E2DS_spirou.py
@@ -337,10 +337,8 @@ def main(night_name=None, reffile=None):
             # work out mean drift across all orders
             loc = spirouRV.DriftAllOrders(p, loc, i_it, nomin, nomax)
             # log the mean drift
-            wmsg = ('Time from ref= {0:.2f} h '
-                    '- Flux Ratio= {1:.3f} '
-                    '- Drift mean= {2:.2f} +- '
-                    '{3:.2f} m/s')
+            wmsg = ('Time from ref= {0:.2f} h - Flux Ratio= {1:.3f} '
+                    '- Drift mean= {2:.2f} +- {3:.2f} m/s')
             wargs = [loc['DELTATIME'][i_it], loc['FLUXRATIO'][i_it],
                      loc['MEANRV'][i_it], loc['MERRDRIFT'][i_it]]
             WLOG(p, 'info', wmsg.format(*wargs))
