1a4f78a193602732be99f71d5b8db47c9fbd5541
Neil Cook <neil.james.cook@gmail.com>
2019-01-18 18:52:06 -0500

    add configuration.instruments.spirou files

---
+++ INTROOT2/drsmodule/configuration/instruments/spirou/default_constants.py
@@ -0,0 +1,15 @@
+"""
+
+
+Created on 2019-01-17
+
+@author: cook
+"""
+from drsmodule.constants.default.default_constants import *
+
+
+# -----------------------------------------------------------------------------
+# Spirou Constant definitions
+# -----------------------------------------------------------------------------
+
+
