1e8c3f45eb46afa919954b4f4ed0c612b847512c
Neil Cook <neil.james.cook@gmail.com>
2019-02-15 14:42:23 -0500

    tools.* - update paths to drs_setup (via config)

--- INTROOT2/drsmodule/tools/__init__.py
+++ INTROOT2/drsmodule/tools/__init__.py
@@ -5,18 +5,24 @@
 
 # CODE DESCRIPTION HERE
 
-Created on 2019-02-13 at 16:35
+Created on 2019-02-15 at 13:23
 
 @author: cook
 """
 
-from . import find_error
+from . import error
+from . import documentation
+from . import listing
+from . import setup
+from . import testing
+from . import visulisation
 
 # =============================================================================
 # Define functions
 # =============================================================================
 
-# Nothing to see here.
+# Tool used to locate error codes
+find_error = error.find_error
 
 # =============================================================================
 # End of code
