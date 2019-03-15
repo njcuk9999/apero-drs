96e549c93b7818fbda2b4c671559156ca9eeea62
Neil Cook <neil.james.cook@gmail.com>
2019-03-15 15:40:04 -0400

    spirouTelluric.py - fix list of col names for bigcube (only one bad file now)

--- INTROOT/SpirouDRS/spirouTelluric/spirouTelluric.py
+++ INTROOT/SpirouDRS/spirouTelluric/spirouTelluric.py
@@ -647,14 +647,14 @@ def construct_big_table(p, loc):
 
     colnames = ['RowNum', 'Filename', 'OBJNAME', 'OBJECT', 'BERV', 'WAVEFILE',
                 'SNR_{0}'.format(snr_order), 'DATE', 'VERSION', 'DARKFILE',
-                'BADFILE1', 'BADFILE2', 'LOCOFILE', 'BLAZEFILE', 'FLATFILE',
+                'BADFILE', 'LOCOFILE', 'BLAZEFILE', 'FLATFILE',
                 'SHAPEFILE', 'EXTRACT_FILE']
 
     columns = ['BASE_ROWNUM', 'BASE_FILELIST', 'BASE_OBJNAME', 'BASE_OBJECT',
                'BASE_BERVLIST', 'BASE_WAVELIST',
                'BASE_SNRLIST_{0}'.format(snr_order), 'BASE_DATELIST',
-               'BASE_VERSION', 'BASE_DARKFILE', 'BASE_BADFILE1',
-               'BASE_BADFILE2', 'BASE_LOCOFILE', 'BASE_BLAZFILE',
+               'BASE_VERSION', 'BASE_DARKFILE', 'BASE_BADFILE',
+               'BASE_LOCOFILE', 'BASE_BLAZFILE',
                'BASE_FLATFILE', 'BASE_SHAPEFILE', 'BASE_EXTRFILE']
     # get values from loc
     values = []
