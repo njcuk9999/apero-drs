d792c963e9ab2ae4461112f787623321843cece6
Neil Cook <neil.james.cook@gmail.com>
2019-03-21 19:31:33 -0400

    drsmodule.io - continue input_redo

---
+++ INTROOT2/drsmodule/io/drs_image.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+# CODE NAME HERE
+
+# CODE DESCRIPTION HERE
+
+Created on 2019-03-21 at 14:28
+
+@author: cook
+"""
+
+from __future__ import division
+import numpy as np
+from astropy.io import fits
+from astropy.table import Table
+
+from drsmodule import constants
+from drsmodule.config import drs_log
+from drsmodule import locale
+
+
+# =============================================================================
+# Define variables
+# =============================================================================
+__NAME__ = 'io.drs_files.py'
+__INSTRUMENT__ = None
+# Get constants
+Constants = constants.load(__INSTRUMENT__)
+# Get version and author
+__version__ = Constants['DRS_VERSION']
+__author__ = Constants['AUTHORS']
+__date__ = Constants['DRS_DATE']
+__release__ = Constants['DRS_RELEASE']
+# get param dict
+ParamDict = constants.ParamDict
+# Get Logging function
+WLOG = drs_log.wlog
+# Get the text types
+ErrorEntry = locale.drs_text.ErrorEntry
+
+
+# =============================================================================
+# Define functions
+# =============================================================================
+def rotate_image(image, rotation):
+    """
+    Rotates the image by rotation
+
+    :param image: numpy array (2D), the image to rotate
+    :param rotation: float, the rotational angle in degrees (counter-clockwise)
+                     must be a multiple of +/- 90 degrees
+
+    :return newimage:  numpy array (2D), the rotated image
+    """
+    rotation = int(rotation // 90)
+    newimage = np.rot90(image, rotation)
+    return newimage
+
+
+# =============================================================================
+# Start of code
+# =============================================================================
+# Main code here
+if __name__ == "__main__":
+    # ----------------------------------------------------------------------
+    # print 'Hello World!'
+    print("Hello World!")
+
+# =============================================================================
+# End of code
+# =============================================================================
