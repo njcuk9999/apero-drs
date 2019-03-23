dbfae3a31498a18f6324e3985500da0ca7286bf2
Neil Cook <neil.james.cook@gmail.com>
2019-03-23 13:39:50 -0400

    blank_spirou.py - update the blank example script

--- INTROOT2/recipes/test/blank_spirou.py
+++ INTROOT2/recipes/test/blank_spirou.py
@@ -48,7 +48,28 @@ def _main(recipe, params):
     return dict(locals())
 
 
+# All recipe code goes in _main
+#    Only change the following from here:
+#     1) function calls  (i.e. main(arg1, arg2, **kwargs)
+#     2) fkwargs         (i.e. fkwargs=dict(arg1=arg1, arg2=arg2, **kwargs)
+#     3) config_main  outputs value   (i.e. None, pp, reduced)
+# Everything else is controlled from recipe_definition
 def main(directory=None, files=None, **kwargs):
+    """
+    Main function for {RECIPE NAME HERE}.py
+
+    :param directory: string, the night name sub-directory
+    :param files: list of strings or string, the list of files to process
+    :param kwargs: any additional keywords
+
+    :type directory: str
+    :type files: list[str]
+
+    :keyword debug: int, debug level (0 for None)
+
+    :returns: dictionary of the local space
+    :rtype: dict
+    """
     # assign function calls (must add positional)
     fkwargs = dict(directory=directory, files=files, **kwargs)
     # ----------------------------------------------------------------------
@@ -56,19 +77,26 @@ def main(directory=None, files=None, **kwargs):
     recipe, params = config.setup(__NAME__, __INSTRUMENT__, fkwargs)
     # ----------------------------------------------------------------------
     # run main bulk of code (catching all errors)
-    try:
+    if params['DRS_DEBUG'] > 0:
         llmain = _main(recipe, params)
+        llmain['e'], llmain['tb'] = None, None
         success = True
-    except Exception as e:
-        string_trackback = traceback.format_exc()
-        success = False
-        emsg = ErrorEntry('01-010-00001', args=[type(e)])
-        emsg += '\n\n' + ErrorEntry(string_trackback)
-        WLOG(params, 'error', emsg, raise_exception=False)
-        llmain = dict()
-    except SystemExit as e:
-        success = False
-        llmain = dict()
+    else:
+        try:
+            llmain = _main(recipe, params)
+            llmain['e'], llmain['tb'] = None, None
+            success = True
+        except Exception as e:
+            string_trackback = traceback.format_exc()
+            success = False
+            emsg = ErrorEntry('01-010-00001', args=[type(e)])
+            emsg += '\n\n' + ErrorEntry(string_trackback)
+            WLOG(params, 'error', emsg, raise_exception=False, wrap=False)
+            llmain = dict(e=e, tb=string_trackback)
+        except SystemExit as e:
+            string_trackback = traceback.format_exc()
+            success = False
+            llmain = dict(e=e, tb=string_trackback)
     # ----------------------------------------------------------------------
     # End Message
     # ----------------------------------------------------------------------
