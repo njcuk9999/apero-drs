b157b9afe3f5696c3016a828a04e4b785526ef1b
Neil Cook <neil.james.cook@gmail.com>
2019-03-25 17:39:28 -0400

    cal_dark_spirou.py - flesh out functionality

--- INTROOT2/recipes/spirou/cal_dark_spirou.py
+++ INTROOT2/recipes/spirou/cal_dark_spirou.py
@@ -12,12 +12,15 @@ Created on 2019-03-23 at 13:01
 from __future__ import division
 import traceback
 import numpy as np
+import warnings
 
 from drsmodule import constants
 from drsmodule import config
 from drsmodule import locale
+from drsmodule.config.database import database
 from drsmodule.io import drs_fits
 from drsmodule.science.calib import dark
+from drsmodule.config.instruments.spirou import file_definitions
 
 # =============================================================================
 # Define variables
@@ -35,7 +38,9 @@ __release__ = Constants['DRS_RELEASE']
 WLOG = config.wlog
 # Get the text types
 ErrorEntry = locale.drs_text.ErrorEntry
-
+# Define the DARK file
+DARK_FILE = file_definitions.out_dark
+SKY_FILE = file_definitions.out_sky
 
 # =============================================================================
 # Define functions
@@ -47,12 +52,16 @@ def _main(recipe, params):
     mainname = __NAME__ + '._main()'
     # get files
     infiles = params['INPUTS']['FILES'][1]
+    # get list of filenames (for output)
+    rawfiles = []
+    for infile in infiles:
+        rawfiles.append(infile.basename)
     # combine input images if required
     if params['COMBINE_IMAGES']:
+        # get combined file
         infiles = [drs_fits.combine(params, infiles, math='average')]
     # get the number of infiles
     num_files = len(infiles)
-
     # ----------------------------------------------------------------------
     # Loop around input files
     # ----------------------------------------------------------------------
@@ -63,19 +72,17 @@ def _main(recipe, params):
         infile = infiles[it]
         # get data from file instance
         image = np.array(infile.data)
-
         # ------------------------------------------------------------------
         # Get basic image properties
         # ------------------------------------------------------------------
         # get image readout noise
-        params['SIGDET'] = infile.get_key('KW_RDNOISE')
+        sigdet = infile.get_key('KW_RDNOISE')
         # get expsoure time
-        params['EXPTIME'] = infile.get_key('KW_EXPTIME')
+        exptime = infile.get_key('KW_EXPTIME')
         # get gain
-        params['GAIN'] = infile.get_key('KW_GAIN')
-        # set sources
-        params.set_sources(['SIGDET', 'EXPTIME', 'GAIN'], mainname)
-
+        gain = infile.get_key('KW_GAIN')
+        # get data type
+        dprtype = infile.get_key('KW_DPRTYPE', dtype=str)
         # ------------------------------------------------------------------
         # Dark exposure time check
         # ------------------------------------------------------------------
@@ -85,28 +92,24 @@ def _main(recipe, params):
         if params['EXPTIME'] < params['QC_DARK_TIME']:
             emsg = 'Dark exposure time too short (< {0:.1f} s)'
             WLOG(params, 'error', emsg.format(params['QC_DARK_TIME']))
-
         # ------------------------------------------------------------------
         # Resize and rotate image
         # ------------------------------------------------------------------
         # convert NaNs to zeros
         nanmask = ~np.isfinite(image)
         image0 = np.where(nanmask, np.zeros_like(image), image)
-
         # resize blue image
         bkwargs = dict(xlow=params['IMAGE_X_BLUE_LOW'],
                        xhigh=params['IMAGE_X_BLUE_HIGH'],
                        ylow=params['IMAGE_Y_BLUE_LOW'],
                        yhigh=params['IMAGE_Y_BLUE_HIGH'])
         imageblue = drs_fits.resize(params, image0, **bkwargs)
-
         # resize red image
         bkwargs = dict(xlow=params['IMAGE_X_RED_LOW'],
                        xhigh=params['IMAGE_X_RED_HIGH'],
                        ylow=params['IMAGE_Y_RED_LOW'],
                        yhigh=params['IMAGE_Y_RED_HIGH'])
         imagered = drs_fits.resize(params, image0, **bkwargs)
-
         # ------------------------------------------------------------------
         # Dark Measurement
         # ------------------------------------------------------------------
@@ -121,12 +124,13 @@ def _main(recipe, params):
         # measure dark for rede part
         out3 = dark.measure_dark(params, imagered, '40-011-00005')
         hist_red, med_red, dadead_red = out3
-
         # ------------------------------------------------------------------
         # Identification of bad pixels
         # ------------------------------------------------------------------
-        # TODO: fill in section
-
+        # Question bad pixel detection is done in cal_BAD_pix
+        # Question:   shouldn't this be used instead of a separate badpix here?
+        out = dark.measure_dark_badpix(params, image, nanmask)
+        baddark, dadeadall = out
         # ------------------------------------------------------------------
         # Plots
         # ------------------------------------------------------------------
@@ -135,18 +139,112 @@ def _main(recipe, params):
         # ------------------------------------------------------------------
         # Quality control
         # ------------------------------------------------------------------
-        # TODO: fill in section
-
+        # set passed variable and fail message list
+        fail_msg, qc_values, qc_names, qc_logic, qc_pass = [], [], [], [], []
+        # ------------------------------------------------------------------
+        # check that med < qc_max_darklevel
+        if params['MED_FULL'] > params['QC_MAX_DARKLEVEL']:
+            # add failed message to fail message list
+            fargs = [params['MED_FULL'], params['QC_MAX_DARKLEVEL']]
+            fail_msg.append(ErrorEntry('40-011-00008', args=fargs))
+            qc_pass.append(0)
+        else:
+            qc_pass.append(1)
+        # add to qc header lists
+        qc_values.append(params['MED_FULL'])
+        qc_names.append('MED_FULL')
+        qc_logic.append('MED_FULL > {0:.2f}'.format(params['QC_MAX_DARKLEVEL']))
+        # ------------------------------------------------------------------
+        # check that fraction of dead pixels < qc_max_dead
+        if dadeadall > params['QC_MAX_DEAD']:
+            # add failed message to fail message list
+            fargs = [dadeadall, params['QC_MAX_DEAD']]
+            fail_msg.append(ErrorEntry('40-011-00009', args=fargs))
+            qc_pass.append(0)
+        else:
+            qc_pass.append(1)
+        # add to qc header lists
+        qc_values.append(dadeadall)
+        qc_names.append('DADEADALL')
+        qc_logic.append('DADEADALL > {0:.2f}'.format(params['QC_MAX_DEAD']))
+        # ----------------------------------------------------------------------
+        # checl that the precentage of dark pixels < qc_max_dark
+        if baddark > params['QC_MAX_DARK']:
+            fargs = [params['DARK_CUTLIMIT'], baddark, params['QC_MAX_DARK']]
+            fail_msg.append(ErrorEntry('40-011-00010', args=fargs))
+            qc_pass.append(0)
+        else:
+            qc_pass.append(1)
+        # add to qc header lists
+        qc_values.append(baddark)
+        qc_names.append('baddark')
+        qc_logic.append('baddark > {0:.2f}'.format(params['QC_MAX_DARK']))
+        # ------------------------------------------------------------------
+        # finally log the failed messages and set QC = 1 if we pass the
+        # quality control QC = 0 if we fail quality control
+        if np.sum(qc_pass) == len(qc_pass):
+            WLOG(params, 'info', ErrorEntry('40-005-00001'))
+            params['QC'] = 1
+            params.set_source('QC', __NAME__ + '/main()')
+        else:
+            for farg in fail_msg:
+                WLOG(params, 'warning', ErrorEntry('40-005-00001') + farg)
+            params['QC'] = 0
+            params.set_source('QC', __NAME__ + '/main()')
+            continue
+        # store in qc_params
+        qc_params = [qc_names, qc_values, qc_logic, qc_pass]
         # ------------------------------------------------------------------
         # Save dark to file
         # ------------------------------------------------------------------
-        # TODO: fill in section
-
-        # ----------------------------------------------------------------------
+        # define outfile
+        if dprtype == 'DARK_DARK':
+            outfile = DARK_FILE.copy()
+        elif dprtype == 'SKY_DARK':
+            outfile = SKY_FILE.copy()
+        # construct the filename from file instance
+        outfile.construct_filename(params, infile=infile)
+        # ------------------------------------------------------------------
+        # define header keys for output file
+        # copy keys from input file
+        outfile.copy_original_keys(infile)
+        # add version
+        outfile.add_hkey('KW_VERSION', value=params['DRS_VERSION'])
+        # add process id
+        outfile.add_hkey('KW_PID', value=params['PID'])
+        # add output tag
+        outfile.add_key('KW_OUTPUT', value=outfile.name)
+        # add input files
+        outfile.add_hkey_1d('KW_INFILE1', values=rawfiles)
+        # add qc parameters
+        outfile.add_qckeys(qc_params)
+        # add blue/red/full detector parameters
+        outfile.add_hkey('KW_DARK_DEAD', value=dadead_full)
+        outfile.add_hkey('KW_DARK_MED', value=med_full)
+        outfile.add_hkey('KW_DARK_B_DEAD', value=dadead_blue)
+        outfile.add_hkey('KW_DARK_B_MED', value=med_blue)
+        outfile.add_hkey('KW_DARK_R_DEAD', value=dadead_red)
+        outfile.add_hkey('KW_DARK_R_MED', value=med_red)
+        # add the cut limit
+        outfile.add_hkey('KW_DARK_CUT', value=params['DARK_CUTLIMIT'])
+        # ------------------------------------------------------------------
+        # Set to zero dark value > dark_cutlimit
+        cutmask = image0 > params['DARK_CUTLIMIT']
+        image0c = np.where(cutmask, np.zeros_like(image0), image0)
+        # copy data
+        outfile.data = image0c
+        # ------------------------------------------------------------------
+        # log that we are saving rotated image
+        wargs = [outfile.filename]
+        WLOG(params, '', ErrorEntry('40-010-00009', args=wargs))
+        # ------------------------------------------------------------------
+        # write image to file
+        outfile.write()
+        # ------------------------------------------------------------------
         # Move to calibDB and update calibDB
-        # ----------------------------------------------------------------------
-        # TODO: fill in section
-
+        # ------------------------------------------------------------------
+        if params['QC']:
+            database.add_file(params, outfile)
 
     # ----------------------------------------------------------------------
     # End of main code
