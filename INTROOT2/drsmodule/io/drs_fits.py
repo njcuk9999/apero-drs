059171abe40b3b0150ee9390b10f436065fe8d70
Neil Cook <neil.james.cook@gmail.com>
2019-03-25 13:19:56 -0400

    drs_fits.py - add combine and resize functions

--- INTROOT2/drsmodule/io/drs_fits.py
+++ INTROOT2/drsmodule/io/drs_fits.py
@@ -14,11 +14,12 @@ import traceback
 import os
 from astropy.io import fits
 from astropy.table import Table
+import numpy as np
 
 from drsmodule import constants
 from drsmodule.config import drs_log
 from drsmodule import locale
-
+from drsmodule.config import drs_file
 
 # =============================================================================
 # Define variables
@@ -34,6 +35,7 @@ __date__ = Constants['DRS_DATE']
 __release__ = Constants['DRS_RELEASE']
 # get param dict
 ParamDict = constants.ParamDict
+DrsFitsFile = drs_file.DrsFitsFile
 # Get Logging function
 WLOG = drs_log.wlog
 # Get the text types
@@ -179,20 +181,131 @@ def write(params, filename, header=None, comments=None):
 # =============================================================================
 # Define other functions
 # =============================================================================
-# TODO: write function
-def combine(params, infiles, math='average'):
+def combine(params, infiles, math='average', same_type=True):
+    """
+    Takes a list of infiles and combines them (infiles must be DrsFitsFiles)
+    combines using the math given.
 
+    Allowed math:
+        'sum', 'add', '+'
+        'average', 'mean'
+        'subtract', '-'
+        'divide', '/'
+        'multiply', 'times', '*'
+
+    Note 'infiles' must be all the same DrsFitsFile type to combine by default,
+    use 'same_type=False' to override this option
+
+    Note the header is copied from infiles[0]
+
+    :param params: ParamDict, parameter dictionary of constants
+    :param infiles: list of DrsFiles, list of DrsFitsFiles to combine
+    :param math: str, the math allowed (see above)
+    :param same_type: bool, if True all infiles must have the same DrsFitFile
+                      dtype
+
+    :type params: ParamDict
+    :type infiles: list[DrsFitsFile]
+    :type math: str
+    :type same_type: bool
+
+    :return: Returns the combined DrsFitFile (header same as infiles[0])
+    :rtype: DrsFitsFile
+    """
+    func_name = __NAME__ + '.combine()'
+    # if we have a string assume we have 1 file and skip combine
+    if type(infiles) is str:
+        return infiles
     # make sure infiles is a list
     if type(infiles) is not list:
-        WLOG(params, 'error', ErrorEntry(''))
-
+        WLOG(params, 'error', ErrorEntry('00-001-00020', args=[func_name]))
+    # if we have only one file (or none) skip combine
+    if len(infiles) < 2:
+        return infiles
     # check that all infiles are the same DrsFileType
-
+    if same_type:
+        for it, infile in enumerate(infiles):
+            if infile.name != infiles[0].name:
+                eargs = [infiles[0].name, it, infile.name, func_name]
+                WLOG(params, 'error', ErrorEntry('00-001-00021', args=eargs))
     # make new infile using math
-    # question: how do we deal with multiple headers?
+    outfile = infiles[0].combine(infiles[1:], math, same_type)
+    # return combined infile
+    return outfile
 
-    # return combined infile (as a single entry list)
-    return infiles
+
+def resize(params, image, x=None, y=None, xlow=0, xhigh=None,
+           ylow=0, yhigh=None):
+    """
+    Resize an image based on a pixel values
+
+    :param params: ParamDict, parameter dictionary of constants
+    :param image: numpy array (2D), the image
+    :param x: None or numpy array (1D), the list of x pixels
+    :param y: None or numpy array (1D), the list of y pixels
+    :param xlow: int, x pixel value (x, y) in the bottom left corner,
+                 default = 0
+    :param xhigh:  int, x pixel value (x, y) in the top right corner,
+                 if None default is image.shape(1)
+    :param ylow: int, y pixel value (x, y) in the bottom left corner,
+                 default = 0
+    :param yhigh: int, y pixel value (x, y) in the top right corner,
+                 if None default is image.shape(0)
+    :param getshape: bool, if True returns shape of newimage with newimage
+
+    if getshape = True
+    :return newimage: numpy array (2D), the new resized image
+    :return nx: int, the shape in the first dimension, i.e. data.shape[0]
+    :return ny: int, the shape in the second dimension, i.e. data.shape[1]
+
+    if getshape = False
+    :return newimage: numpy array (2D), the new resized image
+    """
+    func_name = __NAME__ + '.resize()'
+    # Deal with no low/high values
+    if xhigh is None:
+        xhigh = image.shape(1)
+    if yhigh is None:
+        yhigh = image.shape(0)
+    # if our x pixels and y pixels to keep are defined then use them to
+    # construct the new image
+    if x is not None and y is not None:
+        pass
+    # else define them from low/high values
+    else:
+        # deal with xlow > xhigh
+        if xlow > xhigh:
+            x = np.arange(xhigh + 1, xlow + 1)[::-1]
+        elif xlow == xhigh:
+            eargs = ['xlow', 'xhigh', xlow, func_name]
+            WLOG(params, 'error', ErrorEntry('00-001-00023', args=eargs))
+        else:
+            x = np.arange(xlow, xhigh)
+        # deal with ylow > yhigh
+        if ylow > yhigh:
+            y = np.arange(yhigh + 1, ylow + 1)[::-1]
+        elif ylow == yhigh:
+            eargs = ['ylow', 'yhigh', xlow, func_name]
+            WLOG(params, 'error', ErrorEntry('00-001-00023', args=eargs))
+        else:
+            y = np.arange(ylow, yhigh)
+    # construct the new image (if one can't raise error)
+    try:
+        newimage = np.take(np.take(image, x, axis=1), y, axis=0)
+    except Exception as e:
+        eargs = [xlow, xhigh, ylow, yhigh, type(e), e, func_name]
+        WLOG(params, 'error', ErrorEntry('00-001-00024', args=eargs))
+        newimage = None
+    # return error if we removed all pixels
+    if newimage.shape[0] == 0 or newimage.shape[1] == 0:
+        eargs = [xlow, xhigh, ylow, yhigh, func_name]
+        WLOG(params, 'error', ErrorEntry('00-001-00025', args=eargs))
+        newimage = None
+
+    # return new image
+    return newimage
+
+
 
 
 # =============================================================================
