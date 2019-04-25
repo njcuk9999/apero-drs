e8007756760e535b7ce1b8727fc8482c8bc340a2
Neil Cook <neil.james.cook@gmail.com>
2019-04-25 12:36:20 -0400

    cal_CCF_E2DS_FP_spirou.py - looking for NaN warnings

--- INTROOT/bin/cal_CCF_E2DS_FP_spirou.py
+++ INTROOT/bin/cal_CCF_E2DS_FP_spirou.py
@@ -266,9 +266,7 @@ def main(night_name=None, e2dsfile=None, mask=None, rv=None, width=None,
     # Do correlation
     # ----------------------------------------------------------------------
     # calculate and fit the CCF
-    print('Start Coravelation (p, loc)')
     loc = spirouRV.Coravelation(p, loc)
-    print('End Coravelation (p, loc)')
 
     # ----------------------------------------------------------------------
     # Correlation stats
