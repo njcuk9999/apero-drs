e702eabeb37e4f18eb25a8092e4d3210024fdbff
Neil Cook <neil.james.cook@gmail.com>
2019-05-13 12:53:57 -0400

    background.py - first commit space for background functions

---
+++ INTROOT2/terrapipe/science/calib/background.py
@@ -0,0 +1,119 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+# CODE NAME HERE
+
+# CODE DESCRIPTION HERE
+
+Created on 2019-05-13 at 12:40
+
+@author: cook
+"""
+from __future__ import division
+import numpy as np
+import warnings
+from scipy.signal import convolve2d
+
+from terrapipe import constants
+from terrapipe.config import drs_log
+from terrapipe import locale
+from terrapipe.config import drs_file
+
+
+# =============================================================================
+# Define variables
+# =============================================================================
+__NAME__ = 'science.calib.background.py'
+__INSTRUMENT__ = None
+# Get constants
+Constants = constants.load(__INSTRUMENT__)
+# Get version and author
+__version__ = Constants['DRS_VERSION']
+__author__ = Constants['AUTHORS']
+__date__ = Constants['DRS_DATE']
+__release__ = Constants['DRS_RELEASE']
+# get param dict
+ParamDict = constants.ParamDict
+DrsFitsFile = drs_file.DrsFitsFile
+# Get Logging function
+WLOG = drs_log.wlog
+# Get the text types
+TextEntry = locale.drs_text.TextEntry
+TextDict = locale.drs_text.TextDict
+
+# =============================================================================
+# Define variables
+# =============================================================================
+
+# -----------------------------------------------------------------------------
+
+# =============================================================================
+# Define functions
+# =============================================================================
+def create_background_map(params, image, badpixmask):
+
+    # get constants
+    width = params['BKGR_BOXSIZE']
+    percent = params['BKGR_PERCENTAGE']
+    csize = params['BKGR_MASK_CONVOLVE_SIZE']
+    nbad = params['BKGR_N_BAD_NEIGHBOURS']
+
+    # set image bad pixels to NaN
+    image0 = np.array(image)
+    badmask = np.array(badpixmask, dtype=bool)
+    image0[badmask] = np.nan
+
+    # image that will contain the background estimate
+    backest = np.zeros_like(image0)
+
+    # we slice the image in ribbons of width "width".
+    # The slicing is done in the cross-dispersion direction, so we
+    # can simply take a median along the "fast" dispersion to find the
+    # order profile. We pick width to be small enough for the orders not
+    # to show a significant curvature within w pixels
+    for x_it in range(0, image0.shape[1], width):
+        # ribbon to find the order profile
+        ribbon = np.nanmedian(image0[:, x_it:x_it + width], axis=1)
+
+        for y_it in range(image0.shape[0]):
+            # we perform a running Nth percentile filter along the
+            # order profile. The box of the filter is w. Note that it could
+            # differ in princile from w, its just the same for the sake of
+            # simplicity.
+            ystart = y_it - width // 2
+            yend = y_it + width // 2
+            if ystart < 0:
+                ystart = 0
+            if yend > image0.shape[0] - 1:
+                yend = image0.shape[0] - 1
+            # background estimate
+            backest_pix = np.nanpercentile(ribbon[ystart:yend], percent)
+            backest[y_it, x_it: x_it + width] = backest_pix
+    # the mask is the area that is below then Nth percentile threshold
+    with warnings.catch_warnings(record=True) as _:
+        backmask = np.array(image0 < backest, dtype=float)
+    # we take advantage of the order geometry and for that the "dark"
+    # region of the array be continuous in the "fast" dispersion axis
+    nribbon = convolve2d(backmask, np.ones([1, csize]), mode='same')
+    # we remove from the binary mask all isolated (within 1x7 ribbon)
+    # dark pixels
+    backmask[nribbon == 1] = 0
+    # If a pixel has 3 or more "dark" neighbours, we consider it dark
+    # regardless of its initial value
+    backmask[nribbon >= nbad] = 1
+    # return the background mask
+    return backmask
+
+
+# =============================================================================
+# Start of code
+# =============================================================================
+# Main code here
+if __name__ == "__main__":
+    # ----------------------------------------------------------------------
+    # print 'Hello World!'
+    print("Hello World!")
+
+# =============================================================================
+# End of code
+# =============================================================================
