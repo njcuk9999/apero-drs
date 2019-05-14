c26853edd24a77346a5a4548819136b3007cf757
Neil Cook <neil.james.cook@gmail.com>
2019-05-14 18:21:31 -0400

    drs_lock.py - generalise lock functions

--- INTROOT2/terrapipe/io/drs_lock.py
+++ INTROOT2/terrapipe/io/drs_lock.py
@@ -48,7 +48,7 @@ HelpText = drs_text.HelpDict
 # =============================================================================
 # Define functions
 # =============================================================================
-def check_fits_lock_file(p, filename):
+def check_lock_file(p, filename):
     """
     Check whether lock file exists - wait if it does - else create a lock
     file and return the lock file and lock filename
@@ -69,10 +69,7 @@ def check_fits_lock_file(p, filename):
     # construct lock file name
     max_wait_time = p['DB_MAX_WAIT']
     # get lock file name
-    if '.fits' in filename:
-        lock_file = filename.replace('.fits', '.lock')
-    else:
-        lock_file = filename + '.lock'
+    lock_file = filename + '.lock'
     # check if lock file already exists
     if os.path.exists(lock_file):
         WLOG(p, 'warning', TextEntry('10-001-00002', args=[filename]))
@@ -86,12 +83,12 @@ def check_fits_lock_file(p, filename):
         WLOG(p, 'error', TextEntry('01-001-00002', args=eargs))
     # try to open the lock file
     # wait until lock_file does not exist or we have exceeded max wait time
-    lock = open_fits_lock_file(p, lock_file, filename)
+    lock = open_lock_file(p, lock_file, filename)
     # return lock file and name
     return lock, lock_file
 
 
-def open_fits_lock_file(p, lock_file, filename):
+def open_lock_file(p, lock_file, filename):
     """
     Opens the lock file (or waits if file is already being opened)
 
@@ -114,23 +111,23 @@ def open_fits_lock_file(p, lock_file, filename):
     wait_time = 0
     open_file = True
     lock = None
-    while open_file and wait_time < p['FITSOPEN_MAX_WAIT']:
+    while open_file and wait_time < p['LOCKOPEN_MAX_WAIT']:
         # noinspection PyBroadException
         try:
             lock = open(lock_file, 'w')
             open_file = False
         except Exception as e:
             if wait_time == 0:
-                WLOG(p, 'warning', TextEntry('10-001-00003'))
+                WLOG(p, 'warning', TextEntry('10-001-00003', args=[lock_file]))
             time.sleep(1)
             wait_time += 1
-    if wait_time > p['FITSOPEN_MAX_WAIT']:
+    if wait_time > p['LOCKOPEN_MAX_WAIT']:
         eargs = [filename, lock_file]
         WLOG(p, 'error', TextEntry('01-001-00002', args=eargs))
     return lock
 
 
-def close_fits_lock_file(p, lock, lock_file, filename):
+def close_lock_file(p, lock, lock_file, filename):
     """
     Opens the lock file (or waits if file is already being opened)
 
@@ -153,7 +150,7 @@ def close_fits_lock_file(p, lock, lock_file, filename):
     # wait until lock_file does not exist or we have exceeded max wait time
     wait_time = 0
     close_file = True
-    while close_file and wait_time < p['FITSOPEN_MAX_WAIT']:
+    while close_file and wait_time < p['LOCKOPEN_MAX_WAIT']:
         # noinspection PyBroadException
         try:
             lock.close()
@@ -162,10 +159,10 @@ def close_fits_lock_file(p, lock, lock_file, filename):
             close_file = False
         except Exception as e:
             if wait_time == 0:
-                WLOG(p, 'warning', TextEntry('10-001-00004'))
+                WLOG(p, 'warning', TextEntry('10-001-00004', args=[lock_file]))
             time.sleep(1)
             wait_time += 1
-    if wait_time > p['FITSOPEN_MAX_WAIT']:
+    if wait_time > p['LOCKOPEN_MAX_WAIT']:
         eargs = [filename, lock_file]
         WLOG(p, 'error', TextEntry('01-001-00002', args=eargs))
 
