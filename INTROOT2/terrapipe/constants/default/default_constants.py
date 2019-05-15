c60ced7e530df729154b74d49a556e286c21e108
Neil Cook <neil.james.cook@gmail.com>
2019-05-15 18:44:57 -0400

    constants.defaults.*.py - add cal_loc constants

--- INTROOT2/terrapipe/constants/default/default_constants.py
+++ INTROOT2/terrapipe/constants/default/default_constants.py
@@ -5,7 +5,8 @@ from terrapipe.constants.core import constant_functions
 # Define variables
 # =============================================================================
 # all definition
-__all__ = ['PP_CORRUPT_MED_SIZE', 'PP_CORRUPT_HOT_THRES', 'PP_NUM_DARK_AMP',
+__all__ = [# preprocessing constants
+           'PP_CORRUPT_MED_SIZE', 'PP_CORRUPT_HOT_THRES', 'PP_NUM_DARK_AMP',
            'PP_FULL_FLAT', 'PP_TOTAL_AMP_NUM', 'DATA_ENGINEERING',
            'PP_NUM_REF_TOP', 'PP_NUM_REF_BOTTOM', 'PP_RMS_PERCENTILE',
            'PP_LOWEST_RMS_PERCENTILE', 'PP_CORRUPT_SNR_HOTPIX',
@@ -24,13 +25,16 @@ __all__ = ['PP_CORRUPT_MED_SIZE', 'PP_CORRUPT_HOT_THRES', 'PP_NUM_DARK_AMP',
            'IMAGE_Y_RED_HIGH', 'DARK_CUTLIMIT', 'QC_MAX_DARKLEVEL',
            'HISTO_BINS', 'HISTO_RANGE_LOW', 'HISTO_RANGE_HIGH',
            'USE_SKYDARK_CORRECTION', 'USE_SKYDARK_ONLY',
-            # badpix constants
+           # badpix constants
            'BADPIX_FULL_FLAT', 'BADPIX_FLAT_MED_WID', 'BADPIX_FLAT_CUT_RATIO',
            'BADPIX_ILLUM_CUT', 'BADPIX_MAX_HOTPIX', 'BADPIX_FULL_THRESHOLD',
            'BADPIX_NORM_PERCENTILE',
            # bkgr constants
            'BKGR_BOXSIZE', 'BKGR_PERCENTAGE', 'BKGR_MASK_CONVOLVE_SIZE',
-           'BKGR_N_BAD_NEIGHBOURS']
+           'BKGR_N_BAD_NEIGHBOURS', 'BKGR_NO_SUBTRACTION', 'BKGR_KER_AMP',
+           'BKGR_KER_WX', 'BKGR_KER_WY', 'BKGR_KER_SIG',
+           # localisation constants
+           'LOC_ORDERP_BOX_SIZE']
 
 # set name
 __NAME__ = 'terrapipe.constants.default.default_constants'
@@ -157,13 +161,13 @@ IMAGE_Y_BLUE_HIGH = Const('IMAGE_Y_BLUE_HIGH', value=None, dtype=int, minimum=0,
 
 # Defines the resized red image
 IMAGE_X_RED_LOW = Const('IMAGE_X_RED_LOW', value=None, dtype=int, minimum=0,
-                         source=__NAME__)
+                        source=__NAME__)
 IMAGE_X_RED_HIGH = Const('IMAGE_X_RED_HIGH', value=None, dtype=int, minimum=0,
-                          source=__NAME__)
-IMAGE_Y_RED_LOW = Const('IMAGE_Y_RED_LOW', value=None, dtype=int, minimum=0,
                          source=__NAME__)
+IMAGE_Y_RED_LOW = Const('IMAGE_Y_RED_LOW', value=None, dtype=int, minimum=0,
+                        source=__NAME__)
 IMAGE_Y_RED_HIGH = Const('IMAGE_Y_RED_HIGH', value=None, dtype=int, minimum=0,
-                          source=__NAME__)
+                         source=__NAME__)
 
 #   Define a bad pixel cut limit (in ADU/s)
 DARK_CUTLIMIT = Const('DARK_CUTLIMIT', value=None, dtype=float, source=__NAME__)
@@ -182,7 +186,7 @@ HISTO_BINS = Const('HISTO_BINS', value=None, dtype=int, source=__NAME__,
 HISTO_RANGE_LOW = Const('HISTO_RANGE_LOW', value=None, dtype=int,
                         source=__NAME__)
 HISTO_RANGE_HIGH = Const('HISTO_RANGE_LOW', value=None, dtype=int,
-                        source=__NAME__)
+                         source=__NAME__)
 
 #    Define whether to use SKYDARK for dark corrections
 USE_SKYDARK_CORRECTION = Const('USE_SKYDARK_CORRECTION', value=None,
@@ -194,12 +198,11 @@ USE_SKYDARK_ONLY = Const('USE_SKYDARK_ONLY', value=None, dtype=bool,
                          source=__NAME__)
 
 # =============================================================================
-# CALIBRATION: BAD PIXEL SETTINGS
+# CALIBRATION: BAD PIXEL MAP SETTINGS
 # =============================================================================
 #   Defines the full detector flat file (located in the data folder)
 BADPIX_FULL_FLAT = Const('BADPIX_FULL_FLAT', value=None, dtype=str,
                          source=__NAME__)
-
 
 #   Percentile to normalise to when normalising and median filtering
 #      image [percentage]
@@ -227,6 +230,9 @@ BADPIX_MAX_HOTPIX = Const('BADPIX_MAX_HOTPIX', value=None, dtype=float,
 BADPIX_FULL_THRESHOLD = Const('BADPIX_FULL_THRESHOLD', value=None, dtype=float,
                               source=__NAME__, minimum=0.0)
 
+# =============================================================================
+# CALIBRATION: BACKGROUND CORRECTION SETTINGS
+# =============================================================================
 #    Width of the box to produce the background mask
 BKGR_BOXSIZE = Const('BKGR_BOXSIZE', value=None, dtype=int,
                      source=__NAME__, minimum=0)
@@ -243,3 +249,32 @@ BKGR_MASK_CONVOLVE_SIZE = Const('BKGR_MASK_CONVOLVE_SIZE', value=None,
 #        regardless of its initial value
 BKGR_N_BAD_NEIGHBOURS = Const('BKGR_N_BAD_NEIGHBOURS', value=None, dtype=int,
                               source=__NAME__, minimum=0)
+
+#    Do not correct for background measurement (True or False)
+BKGR_NO_SUBTRACTION = Const('BKGR_NO_SUBTRACTION', value=None, dtype=bool,
+                            source=__NAME__)
+
+#    Kernel amplitude determined from drs_local_scatter.py
+BKGR_KER_AMP = Const('BKGR_KER_AMP', value=None, dtype=float, source=__NAME__)
+
+#    Background kernel width in in x and y [pixels]
+BKGR_KER_WX = Const('BKGR_KER_WX', value=None, dtype=int, source=__NAME__)
+BKGR_KER_WY = Const('BKGR_KER_WY', value=None, dtype=int, source=__NAME__)
+
+#    construct a convolution kernel. We go from -IC_BKGR_KER_SIG to
+#        +IC_BKGR_KER_SIG sigma in each direction. Its important no to
+#        make the kernel too big as this slows-down the 2D convolution.
+#        Do NOT make it a -10 to +10 sigma gaussian!
+BKGR_KER_SIG = Const('BKGR_KER_SIG', value=None, dtype=float, source=__NAME__)
+
+
+# =============================================================================
+# CALIBRATION: LOCALISATION SETTINGS
+# =============================================================================
+#   Size of the order_profile smoothed box
+#     (from pixel - size to pixel + size)
+LOC_ORDERP_BOX_SIZE = Const('LOC_ORDERP_BOX_SIZE', value=None, dtype=int,
+                            source=__NAME__)
+
+
+
