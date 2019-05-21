f9b5d88f1494395866cb3daada752e6552a3af0b
Neil Cook <neil.james.cook@gmail.com>
2019-05-21 12:27:45 -0400

    cal_loc_spirou.py - continue work on adapting recipe for terrapipe

--- INTROOT2/recipes/spirou/cal_loc_spirou.py
+++ INTROOT2/recipes/spirou/cal_loc_spirou.py
@@ -15,13 +15,16 @@ import numpy as np
 from terrapipe import constants
 from terrapipe import config
 from terrapipe import locale
+from terrapipe.config import math
 from terrapipe.config.core import drs_database
 from terrapipe.config.instruments.spirou import file_definitions
 from terrapipe.io import drs_fits
+from terrapipe.io import drs_image
 from terrapipe.science.calib import dark
 from terrapipe.science.calib import badpix
 from terrapipe.science.calib import background
 from terrapipe.science.calib import localisation
+
 
 # =============================================================================
 # Define variables
@@ -44,6 +47,14 @@ TextDict = locale.drs_text.TextDict
 # Define the output files
 ORDERP_AB = file_definitions.out_loc_orderp_ab
 ORDERP_C = file_definitions.out_loc_orderp_c
+
+LOCO1_AB = file_definitions.out_loc_loco_ab
+LOCO1_C =file_definitions.out_loc_loco_c
+LOCO2_AB = file_definitions.out_loc_loco_2_ab
+LOCO2_C = file_definitions.out_loc_loco_2c
+LOCO3_AB = file_definitions.out_loc_loco_3_ab
+LOCO3_C = file_definitions.out_loc_loco_3_c
+
 # alias pcheck
 pcheck = config.pcheck
 
@@ -174,9 +185,14 @@ def __main__(recipe, params):
         # image 2 is flipped (if required)
         if params['INPUT_FLIP_IMAGE']:
             # flip flat
-            image2 = drs_fits.flip_image(params, image1)
+            image2 = drs_image.flip_image(params, image1)
         else:
             image2 = np.array(image1)
+
+        # ------------------------------------------------------------------
+        #  Convert ADU/s to electrons
+        # ------------------------------------------------------------------
+        image2 = drs_image.convert_to_e(params, image2)
 
         # ------------------------------------------------------------------
         # Resize image
@@ -189,7 +205,7 @@ def __main__(recipe, params):
                          ylow=params['IMAGE_Y_LOW'],
                          yhigh=params['IMAGE_Y_HIGH'])
             # resize flat
-            image2 = drs_fits.resize(params, image2, **sargs)
+            image2 = drs_image.resize(params, image2, **sargs)
 
         # ------------------------------------------------------------------
         # Correct for the BADPIX mask (set all bad pixels to NaNs)
@@ -218,7 +234,14 @@ def __main__(recipe, params):
         cent_0, cent_coeffs, cent_rms, cent_max_ptp = lout[:4]
         cent_frac_ptp, cent_max_rmpts = lout[4:6]
         wid_0, wid_coeffs, wid_rms, wid_max_ptp = lout[6:10]
-        wid_frac_ptp, wid_max_rmpts, xplot, yplot = lout[10:]
+        wid_frac_ptp, wid_max_rmpts, xplot, yplot = lout[10:14]
+        rorder_num, mean_rms_cent, mean_rms_wid = lout[14:]
+
+        # ------------------------------------------------------------------
+        # Use the fits the calculate pixel fit values
+        # ------------------------------------------------------------------
+        center_fits = math.calculate_polyvals(cent_coeffs, image4.shape[1])
+        width_fits = math.calculate_polyvals(wid_coeffs, image4.shape[1])
 
         # ------------------------------------------------------------------
         # Plot the image (ready for fit points to be overplotted later)
@@ -226,7 +249,8 @@ def __main__(recipe, params):
         # Plot the image (ready for fit points to be overplotted later)
         if params['DRS_PLOT'] > 0:
             # get saturation threshold
-            sat_thres = pcheck(params, 'LOC_SAT_THRES') * gain * num_files
+            loc_sat_thres = pcheck(params, 'LOC_SAT_THRES', func=mainname)
+            sat_thres = loc_sat_thres * gain * num_files
             # plot image above saturation threshold
             # TODO: Add sPlt.locplot_im_sat_threshold(p, loc, data2, sat_thres)
 
@@ -243,9 +267,79 @@ def __main__(recipe, params):
         # set passed variable and fail message list
         fail_msg, qc_values, qc_names, qc_logic, qc_pass = [], [], [], [], []
         textdict = TextDict(params['INSTRUMENT'], params['LANGUAGE'])
-
-        # TODO: complete
-
+        # get qc parameters
+        max_removed_cent = pcheck(params, 'QC_LOC_MAXFIT_REMOVED_CTR',
+                                  func=mainname)
+        max_removed_wid = pcheck(params, 'QC_LOC_MAXFIT_REMOVED_WID',
+                                 func=mainname)
+        rmsmax_cent = pcheck(params, 'QC_LOC_RMSMAX_CTR', func=mainname)
+        rmsmax_wid = pcheck(params, 'QC_LOC_RMSMAX_WID', func=mainname)
+        required_norders = pcheck(params, 'FIBER_MAX_NUM_ORDERS', func=mainname)
+        # ----------------------------------------------------------------------
+        # check that max number of points rejected in center fit is below
+        #    threshold
+        if np.nansum(cent_max_rmpts) > max_removed_cent:
+            # add failed message to fail message list
+            fargs = [np.nansum(cent_max_rmpts), max_removed_cent]
+            fail_msg.append(textdict['40-013-00014'].format(*fargs))
+            qc_pass.append(0)
+        else:
+            qc_pass.append(1)
+        # add to qc header lists
+        qc_values.append(np.nansum(cent_max_rmpts))
+        qc_names.append('sum(MAX_RMPTS_POS')
+        qc_logic.append('sum(MAX_RMPTS_POS) < {0:.2f}'.format(cent_max_rmpts))
+        # ----------------------------------------------------------------------
+        # check that  max number of points rejected in width fit is below
+        #   threshold
+        if np.nansum(wid_max_rmpts) > max_removed_wid:
+            # add failed message to fail message list
+            fargs = [np.nansum(cent_max_rmpts), max_removed_wid]
+            fail_msg.append(textdict['40-013-00015'].format(*fargs))
+            qc_pass.append(0)
+        else:
+            qc_pass.append(1)
+        # add to qc header lists
+        qc_values.append(np.nansum(wid_max_rmpts))
+        qc_names.append('sum(MAX_RMPTS_WID)')
+        qc_logic.append('sum(MAX_RMPTS_WID) < {0:.2f}'.format(max_removed_wid))
+        # ------------------------------------------------------------------
+        if mean_rms_cent > rmsmax_cent:
+            # add failed message to fail message list
+            fargs = [mean_rms_cent, rmsmax_cent]
+            fail_msg.append(textdict['40-013-00016'].format(*fargs))
+            qc_pass.append(0)
+        else:
+            qc_pass.append(1)
+        # add to qc header lists
+        qc_values.append(mean_rms_cent)
+        qc_names.append('mean_rms_center')
+        qc_logic.append('mean_rms_center < {0:.2f}'.format(rmsmax_cent))
+        # ------------------------------------------------------------------
+        if mean_rms_wid > rmsmax_wid:
+            # add failed message to fail message list
+            fargs = [mean_rms_wid, rmsmax_wid]
+            fail_msg.append(textdict['40-013-00017'].format(*fargs))
+            qc_pass.append(0)
+        else:
+            qc_pass.append(1)
+        # add to qc header lists
+        qc_values.append(mean_rms_wid)
+        qc_names.append('mean_rms_wid')
+        qc_logic.append('mean_rms_wid < {0:.2f}'.format(rmsmax_wid))
+        # ------------------------------------------------------------------
+        # check for abnormal number of identified orders
+        if rorder_num != required_norders:
+            # add failed message to fail message list
+            fargs = [rorder_num, required_norders]
+            fail_msg.append(textdict['40-013-00018'].format(*fargs))
+            qc_pass.append(0)
+        else:
+            qc_pass.append(1)
+        # add to qc header lists
+        qc_values.append(rorder_num)
+        qc_names.append('rorder_num')
+        qc_logic.append('rorder_num != {0}'.format(required_norders))
         # ------------------------------------------------------------------
         # finally log the failed messages and set QC = 1 if we pass the
         # quality control QC = 0 if we fail quality control
@@ -298,7 +392,40 @@ def __main__(recipe, params):
         # Save and record of image of localization with order center
         #     and keywords
         # ------------------------------------------------------------------
+        loco1file = LOCO1_AB.newcopy(recipe=recipe)
+        # construct the filename from file instance
+        loco1file.construct_filename(params, infile=infile)
+        # ------------------------------------------------------------------
+        # define header keys for output file
+        # copy keys from input file
+        loco1file.copy_original_keys(infile)
+        # add version
+        loco1file.add_hkey('KW_VERSION', value=params['DRS_VERSION'])
+        # add output tag
+        loco1file.add_hkey('KW_OUTPUT', value=loco1file.name)
+        # add input files (and deal with combining or not combining)
+        if combine:
+            hfiles = rawfiles
+        else:
+            hfiles = [infile.basename]
+        loco1file.add_hkey_1d('KW_INFILE1', values=hfiles, dim1name='file')
+        # add the calibration files use
+        loco1file.add_hkey('KW_CDBDARK', value=params['DARKFILE'])
+        loco1file.add_hkey('KW_CDBBAD', value=params['BADPFILE'])
+        loco1file.add_hkey('KW_CDBBACK', value=params['BACKFILE'])
+        # add localisation parameters
+
         # TODO: complete
+
+        # add qc parameters
+        loco1file.add_qckeys(qc_params)
+        # copy data
+        loco1file.data = center_fits
+        # ------------------------------------------------------------------
+        # log that we are saving rotated image
+        WLOG(params, '', TextEntry('40-013-00019', args=[loco1file.filename]))
+        # write image to file
+        loco1file.write()
 
         # ------------------------------------------------------------------
         # Save and record of image of sigma
