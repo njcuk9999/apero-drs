9b30b34ceee241e8a31eb22a9f79df64eaa28115
Neil Cook <neil.james.cook@gmail.com>
2019-06-12 17:29:46 -0400

    multiprocess_test.py - add an event (to terminate all current and future jobs on crash)

--- INTROOT/misc/multiprocess_test.py
+++ INTROOT/misc/multiprocess_test.py
@@ -10,8 +10,8 @@ Created on 2019-06-11 at 13:40
 @author: cook
 """
 
-from multiprocessing import Process, Manager
-
+from multiprocessing import Process, Manager, Event
+import sys
 
 # =============================================================================
 # Define variables
@@ -22,9 +22,17 @@ from multiprocessing import Process, Manager
 # =============================================================================
 # Define functions
 # =============================================================================
-def myfunc(name, return_dict):
+def myfunc(name, return_dict, event):
     print('Running {0}'.format(name))
+    if name == 'Run3':
+        event.set()
     return_dict[name] = 'OUT' + name
+
+
+def terminate_jobs(jobs):
+    for job in jobs:
+        job.terminate()
+
 
 # =============================================================================
 # Start of code
@@ -37,22 +45,30 @@ if __name__ == "__main__":
 
     # start process manager
     manager = Manager()
+    event = Event()
     return_dict = manager.dict()
     # loop around groups
     for g_it, group in enumerate(grouplist):
+        # skip if event is set
+        if event.is_set():
+            continue
         # process storage
         jobs = []
         # loop around sub groups (to be run at same time)
         for runlist_group in group:
             # get args
-            args = [runlist_group, return_dict]
+            args = [runlist_group, return_dict, event]
             # get parallel process
             process = Process(target=myfunc, args=args)
             process.start()
             jobs.append(process)
-        # do not continue until
+        # do not continue until finished
         for proc in jobs:
             proc.join()
+            if event.is_set():
+                terminate_jobs(jobs)
+
+
 
 
 
