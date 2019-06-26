33edbe1f3b5ced8c71b2a15970bb0793e044e770
Neil Cook <neil.james.cook@gmail.com>
2019-06-26 13:58:42 -0400

    reorganize config and constants (now all in core sub-module) - update module order

--- INTROOT2/recipes/spirou/cal_badpix_spirou.py
+++ INTROOT2/recipes/spirou/cal_badpix_spirou.py
@@ -12,9 +12,9 @@ Created on 2019-05-13 at 11:04
 from __future__ import division
 import numpy as np
 
-from terrapipe.core import constants
 from terrapipe import core
 from terrapipe import locale
+from terrapipe.core import constants
 from terrapipe.core.core import drs_database
 from terrapipe.core.instruments.spirou import file_definitions
 from terrapipe.io import drs_fits
