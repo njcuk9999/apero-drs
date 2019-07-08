3796ee96e6ea95cfb86d3a13f821d63d78f54d2a
Neil Cook <neil.james.cook@gmail.com>
2019-07-08 12:35:09 -0400

    output_filenames.py - change how getting filenames work (now uses prefix/suffix/filetype and deal with having a fiber defined)

--- INTROOT2/terrapipe/core/instruments/default/output_filenames.py
+++ INTROOT2/terrapipe/core/instruments/default/output_filenames.py
@@ -35,55 +35,98 @@ TextEntry = locale.drs_text.TextEntry
 
 
 # =============================================================================
-# Define functions
+# Define file functions
 # =============================================================================
 def general_file(params, **kwargs):
-    func_name = __NAME__ + '.general_file()'
+    func_name = kwargs.get('func', __NAME__ + '.general_file()')
     # get parameters from keyword arguments
     infile = kwargs.get('infile', None)
     outfile = kwargs.get('outfile', None)
+    fiber = kwargs.get('fiber', None)
+    path = kwargs.get('path', None)
     # deal with kwargs that are required
     if infile is None:
         WLOG(params, 'error', TextEntry('00-001-00017', args=[func_name]))
     if outfile is None:
         WLOG(params, 'error', TextEntry('00-001-00018', args=[func_name]))
+    # try to get fiber from outfile
+    if fiber is None:
+        fiber = outfile.fiber
+    # deal with fiber being required but still unset
+    if outfile.fibers is not None and fiber is None:
+        eargs = [outfile, func_name]
+        WLOG(params, 'error', TextEntry('00-001-00032', args=eargs))
+    # get kwargs where default dependent on required arguments
+    prefix = kwargs.get('prefix', outfile.prefix)
+    suffix = kwargs.get('suffix', outfile.suffix)
     # construct out filename
-    outfilename = infile.basename.replace(outfile.inext, outfile.ext)
-    # get output path from params
-    outpath = params['OUTPATH']
-    # get output night name from params
-    outdirectory = params['NIGHTNAME']
-    # construct absolute path
-    abspath = os.path.join(outpath, outdirectory, outfilename)
+    inext = outfile.filetype
+    outfilename = get_outfilename(params, infile.basename, prefix=prefix,
+                                  suffix=suffix, inext=inext, fiber=fiber)
+    # deal with no given path (default)
+    if path is None:
+        # get output path from params
+        outpath = params['OUTPATH']
+        # get output night name from params
+        outdirectory = params['NIGHTNAME']
+        # construct absolute path
+        abspath = os.path.join(outpath, outdirectory, outfilename)
+    else:
+        abspath = os.path.join(path, outfilename)
     # return absolute path
     return abspath
 
 
 def debug_file(params, **kwargs):
     func_name = __NAME__ + '.debug_back()'
+    func = kwargs.get('func', None)
+    if func is None:
+        func = func_name
+    else:
+        func = '{0} and {1}'.format(func, func_name)
+    # update keywords func name
+    kwargs['func'] = func
     # get parameters from keyword arguments
-    infile = kwargs.get('infile', None)
     outfile = kwargs.get('outfile', None)
-    fiber = kwargs.get('fiber', None)
-    # deal with kwargs that are required
-    if infile is None:
-        WLOG(params, 'error', TextEntry('00-001-00017', args=[func_name]))
-    if outfile is None:
-        WLOG(params, 'error', TextEntry('00-001-00018', args=[func_name]))
-    # construct out filename
-    outext = outfile.ext.format(fiber)
-    outfilename = infile.basename.replace(outfile.inext, outext)
-    outfilename = 'DEBUG_' + outfilename
-    # get output path from params
-    outpath = params['OUTPATH']
-    # get output night name from params
-    outdirectory = params['NIGHTNAME']
-    # construct absolute path
-    abspath = os.path.join(outpath, outdirectory, outfilename)
+    if outfile is not None:
+        prefix = 'DEBUG_' + kwargs.get('prefix', outfile.prefix)
+    else:
+        prefix = 'DEBUG_'
     # return absolute path
-    return abspath
+    return general_file(params, prefix=prefix, **kwargs)
 
 
+# =============================================================================
+# Define user functions
+# =============================================================================
+def get_outfilename(params, infilename, prefix=None, suffix=None,
+                    inext=None, outext=None, fiber=None):
+    func_name = __NAME__ + '.get_outfilename()'
+    # deal with fiber
+    if fiber is not None:
+        suffix = '{0}_{1}'.format(suffix, fiber.upper())
+    # remove extension
+    if inext is None:
+        inext = infilename.split('.')[-1]
+    # strip filename of extenstion
+    if infilename.endswith(inext):
+        outfilename = str(infilename[:-len(inext)])
+    else:
+        eargs = [infilename, inext, func_name]
+        WLOG(params, 'error', TextEntry('00-001-00031', args=eargs))
+        outfilename = ''
+    # add prefix and suffix
+    if prefix is not None:
+        outfilename = '{0}{1}'.format(prefix, outfilename)
+    if suffix is not None:
+        outfilename = '{0}{1}'.format(outfilename, suffix)
+    # add back extention or add new one
+    if outext is None:
+        outfilename = '{0}{1}'.format(outfilename, inext)
+    else:
+        outfilename = '{0}{1}'.format(outfilename, outext)
+    # return filename
+    return outfilename
 
 
 # =============================================================================
