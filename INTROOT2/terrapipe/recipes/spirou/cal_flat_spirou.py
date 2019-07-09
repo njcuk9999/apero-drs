e4f72d5161b1343e726034e3b8d80c0aa47f912b
Neil Cook <neil.james.cook@gmail.com>
2019-07-09 17:34:12 -0400

    cal_flat_spirou.py - continue porting over code from spiroudrs

--- INTROOT2/terrapipe/recipes/spirou/cal_flat_spirou.py
+++ INTROOT2/terrapipe/recipes/spirou/cal_flat_spirou.py
@@ -10,6 +10,8 @@ Created on 2019-07-05 at 16:45
 @author: cook
 """
 from __future__ import division
+import numpy as np
+import warnings
 
 from terrapipe import core
 from terrapipe import locale
@@ -44,6 +46,7 @@ TextDict = locale.drs_text.TextDict
 # Define the output files
 FLAT_FILE = file_definitions.out_ff_flat
 BLAZE_FILE = file_definitions.out_ff_blaze
+E2DSLL_FILE = file_definitions.out_ext_e2dsll
 ORDERP_SFILE = file_definitions.out_orderp_straight
 
 
@@ -148,55 +151,215 @@ def __main__(recipe, params):
         # ------------------------------------------------------------------
         # Load and straighten order profiles
         # ------------------------------------------------------------------
-        # storage for order profiles
-        orderprofiles = dict()
-        # loop around fibers
-        for fiber in fibertypes:
-            # construct order profile file
-            orderpsfile = ORDERP_SFILE.newcopy(recipe=recipe, fiber=fiber)
-            orderpsfile.construct_filename(params, infile=infile)
-            # check if temporary file exists
-            if orderpsfile.file_exists():
-                # load the numpy temporary file
-                orderpsfile.read(params)
-                # push data into orderp
-                orderp = orderpsfile.data
-            # load the order profile
-            else:
-                # load using localisation load order profile function
-                orderp = localisation.load_orderp(params, header, fiber=fiber)
-                # push into orderpsfile
-                orderpsfile.data = orderp
-                # save for use later
-                orderpsfile.write(params)
-            # straighten orders
-            orderp = shape.ea_transform(params, orderp, shapelocal,
-                                        dxmap=shapex, dymap=shapey)
-            # store in storage dictionary
-            orderprofiles[fiber] = orderp
+        sargs = [infile, fibertypes, shapelocal, shapex, shapey, ORDERP_SFILE]
+        orderprofiles = extract.order_profiles(params, recipe, *sargs)
 
         # ------------------------------------------------------------------
         # Apply shape transformations
         # ------------------------------------------------------------------
+        # log progress (straightening orderp)
+        WLOG(params, 'info', TextEntry('40-016-00004'))
+        # straighten image
         image2 = shape.ea_transform(params, image, shapelocal, dxmap=shapex,
                                     dymap=shapey)
 
         # ------------------------------------------------------------------
         # Fiber loop
         # ------------------------------------------------------------------
-        fibertypes = drs_image.get_fiber_types(params)
-
         # loop around fiber types
         for fiber in fibertypes:
-            # load wavelength solution
+            # push fiber into params
+            params['FIBER'] = fiber
+            params.set_source('FIBER', mainname)
+            # load wavelength solution for this fiber
             wprops = wave.get_wavesolution(params, recipe, header, fiber=fiber)
-
+            # load the localisation properties for this fiber
+            lprops = localisation.get_coefficients(params, recipe, header,
+                                                   fiber=fiber, merge=True)
+            # get the localisation center coefficients for this fiber
+            lcoeffs = lprops['CENT_COEFFS']
+            # get the number of frames used
+            nframes = infile.numfiles
+            # get the order profile for this fiber
+            orderp = orderprofiles[fiber]
             # extract spectrum
+            eprops = extract.extract2d(params, image2, orderp, lcoeffs, nframes,
+                                       props, kind='flat')
 
-            eprops = extract.
+            # --------------------------------------------------------------
+            # Plots
+            # --------------------------------------------------------------
+            if params['DRS_PLOT'] > 0:
+                # TODO fill in
+                # # start interactive session if needed
+                # sPlt.start_interactive_session(p)
+                # # plot all orders or one order
+                # if p['IC_FF_PLOT_ALL_ORDERS']:
+                #     # plot image with all order fits (slower)
+                #     sPlt.ff_aorder_fit_edges(p, loc, data1)
+                # else:
+                #     # plot image with selected order fit and edge fit (faster)
+                #     sPlt.ff_sorder_fit_edges(p, loc, data1)
+                # # plot tilt adjusted e2ds and blaze for selected order
+                # sPlt.ff_sorder_tiltadj_e2ds_blaze(p, loc)
+                # # plot flat for selected order
+                # sPlt.ff_sorder_flat(p, loc)
+                # # plot the RMS for all but skipped orders
+                # # sPlt.ff_rms_plot(p, loc)
+                #
+                # if p['IC_FF_EXTRACT_TYPE'] in EXTRACT_SHAPE_TYPES:
+                #     sPlt.ff_debanana_plot(p, loc, data2)
+                pass
 
+            # --------------------------------------------------------------
+            # Quality control
+            # --------------------------------------------------------------
+            # set passed variable and fail message list
+            fail_msg, qc_values, qc_names = [], [], [],
+            qc_logic, qc_pass = [], []
+            textdict = TextDict(params['INSTRUMENT'], params['LANGUAGE'])
+            # --------------------------------------------------------------
+            # check that rms values in required orders are below threshold
 
-        # TODO: Need to finish this code
+            # get mask for removing certain values
+            remove_orders = params.listp('FF_RMS_SKIP_ORDERS', dtype=int)
+            remove_orders = np.array(remove_orders)
+            remove_mask = np.in1d(np.arange(len(eprops['RMS'])), remove_orders)
+            # apply max and calculate the maximum of the rms values
+            max_rms = np.nanmax(eprops['RMS'][~remove_mask])
+            # apply the quality control based on the maximum rms
+            if max_rms > params['QC_FF_MAX_RMS']:
+                # add failed message to fail message list
+                fargs = [fiber, max_rms, params['QC_FF_MAX_RMS']]
+                fail_msg.append(textdict[''].format(*fargs))
+                qc_pass.append(0)
+            else:
+                qc_pass.append(1)
+            # add to qc header lists
+            qc_values.append(max_rms)
+            qc_names.append('max_rms')
+            qc_logic.append('max_rms < {0:.2f}'.format(params['QC_FF_MAX_RMS']))
+            # --------------------------------------------------------------
+            # finally log the failed messages and set QC = 1 if we pass the
+            # quality control QC = 0 if we fail quality control
+            if np.sum(qc_pass) == len(qc_pass):
+                WLOG(params, 'info', TextEntry('40-005-10001'))
+                params['QC'] = 1
+                params.set_source('QC', __NAME__ + '/main()')
+            else:
+                for farg in fail_msg:
+                    WLOG(params, 'warning', TextEntry('40-005-10002') + farg)
+                params['QC'] = 0
+                params.set_source('QC', __NAME__ + '/main()')
+            # store in qc_params
+            qc_params = [qc_names, qc_values, qc_logic, qc_pass]
+
+            # --------------------------------------------------------------
+            # Store Blaze in file
+            # --------------------------------------------------------------
+            # get a new copy of the blaze file
+            blazefile = BLAZE_FILE.newcopy(recipe=recipe, fiber=fiber)
+            # construct the filename from file instance
+            blazefile.construct_filename(params, infile=infile)
+            # define header keys for output file
+            # copy keys from input file
+            blazefile.copy_original_keys(infile)
+            # add version
+            blazefile.add_hkey('KW_VERSION', value=params['DRS_VERSION'])
+            # add dates
+            blazefile.add_hkey('KW_DRS_DATE', value=params['DRS_DATE'])
+            blazefile.add_hkey('KW_DRS_DATE_NOW', value=params['DATE_NOW'])
+            # add process id
+            blazefile.add_hkey('KW_PID', value=params['PID'])
+            # add output tag
+            blazefile.add_hkey('KW_OUTPUT', value=blazefile.name)
+            # add input files (and deal with combining or not combining)
+            if combine:
+                hfiles = rawfiles
+            else:
+                hfiles = [infile.basename]
+            blazefile.add_hkey_1d('KW_INFILE1', values=hfiles,
+                                  dim1name='file')
+            # add qc parameters
+            blazefile.add_qckeys(qc_params)
+            # --------------------------------------------------------------
+            # add SNR parameters to header
+            blazefile.add_hkey_1d('KW_EXT_SNR', values=eprops['SNR'],
+                                  dim1name='order')
+            # add start and end extraction order used
+            blazefile.add_hkey('KW_EXT_START', value=eprops['START_ORDER'])
+            blazefile.add_hkey('KW_EXT_END', value=eprops['END_ORDER'])
+            # add extraction ranges used
+            blazefile.add_hkey('KW_EXT_RANGE1', value=eprops['RANGE1'])
+            blazefile.add_hkey('KW_EXT_RANGE2', value=eprops['RANGE2'])
+            # add cosmic parameters used
+            blazefile.add_hkey('KW_COSMIC', value=eprops['COSMIC'])
+            blazefile.add_hkey('KW_COSMIC_CUT', value=eprops['COSMIC_SIGCUT'])
+            blazefile.add_hkey('KW_COSMIC_THRES',
+                               value=eprops['COSMIC_THRESHOLD'])
+            # add blaze parameter used
+            blazefile.add_hkey('KW_BLAZE_WID', value=eprops['BLAZE_SIZE'])
+            blazefile.add_hkey('KW_BLAZE_CUT', value=eprops['BLAZE_CUT'])
+            blazefile.add_hkey('KW_BLAZE_DEG', value=eprops['BLAZE_DEG'])
+            # add saturation parameters used
+            blazefile.add_hkey('KW_SAT_QC', value=eprops['SAT_LEVEL'])
+            with warnings.catch_warnings(record=True) as _:
+                max_sat_level = np.nanmax(eprops['FLUX_VAL'])
+            blazefile.add_hkey('KW_SAT_LEVEL', value=max_sat_level)
+            # --------------------------------------------------------------
+            # copy data
+            blazefile.data = eprops['BLAZE']
+            # --------------------------------------------------------------
+            # log that we are saving rotated image
+            WLOG(params, '',
+                 TextEntry('40-015-00003', args=[blazefile.filename]))
+            # write image to file
+            blazefile.write()
+
+            # --------------------------------------------------------------
+            # Store Flat-field in file
+            # --------------------------------------------------------------
+            # get a new copy of the blaze file
+            flatfile = FLAT_FILE.newcopy(recipe=recipe, fiber=fiber)
+            # construct the filename from file instance
+            flatfile.construct_filename(params, infile=infile)
+            # copy header from blaze file
+            flatfile.copy_hdict(blazefile)
+            # copy data
+            flatfile.data = eprops['FLAT']
+            # --------------------------------------------------------------
+            # log that we are saving rotated image
+            WLOG(params, '',
+                 TextEntry('40-015-00004', args=[flatfile.filename]))
+            # write image to file
+            flatfile.write()
+
+            # --------------------------------------------------------------
+            # Store E2DSLL in file
+            # --------------------------------------------------------------
+            # get a new copy of the blaze file
+            e2dsllfile = E2DSLL_FILE.newcopy(recipe=recipe, fiber=fiber)
+            # construct the filename from file instance
+            e2dsllfile.construct_filename(params, infile=infile)
+            # copy header from blaze file
+            e2dsllfile.copy_hdict(blazefile)
+            # copy data
+            e2dsllfile.data = eprops['E2DSLL']
+            # --------------------------------------------------------------
+            # log that we are saving rotated image
+            WLOG(params, '',
+                 TextEntry('40-015-00005', args=[e2dsllfile.filename]))
+            # write image to file
+            e2dsllfile.write()
+
+            # --------------------------------------------------------------
+            # Update the calibration database
+            # --------------------------------------------------------------
+            if params['QC']:
+                # copy the blaze file to the calibDB
+                drs_database.add_file(params, blazefile)
+                # copy the flat file to the calibDB
+                drs_database.add_file(params, flatfile)
 
     # ----------------------------------------------------------------------
     # End of main code
