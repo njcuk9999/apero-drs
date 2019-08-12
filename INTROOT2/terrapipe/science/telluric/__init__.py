fac2e2bf8467b7682db39109177d99fcb45cec48
Neil Cook <neil.james.cook@gmail.com>
2019-08-12 18:15:14 -0400

    science.telluric.general.py - first commit - add get_whitelist and get_blacklist functions

--- INTROOT2/terrapipe/science/telluric/__init__.py
+++ INTROOT2/terrapipe/science/telluric/__init__.py
@@ -9,13 +9,16 @@ Created on 2019-01-17 at 14:31
 
 @author: cook
 """
+from . import general
 
-__all__ = []
+__all__ = ['get_blacklist', 'get_whitelist']
 
 # =============================================================================
 # Define functions
 # =============================================================================
+get_blacklist = general.get_blacklist
 
+get_whitelist = general.get_whitelist
 
 # =============================================================================
 # End of code
