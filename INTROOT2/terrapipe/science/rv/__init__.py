02d3966424738b82afb3485677954ff50e88fdf1
Neil Cook <neil.james.cook@gmail.com>
2019-08-21 15:27:29 -0400

    science.rv.general.py - add measure_fp_peaks (create_drift_file) and remove_wide_peaks

--- INTROOT2/terrapipe/science/rv/__init__.py
+++ INTROOT2/terrapipe/science/rv/__init__.py
@@ -9,12 +9,17 @@ Created on 2019-01-17 at 14:31
 
 @author: cook
 """
+from . import general
 
 __all__ = []
 
 # =============================================================================
 # Define functions
 # =============================================================================
+measure_fp_peaks = general.measure_fp_peaks
+
+remove_wide_peaks = general.remove_wide_peaks
+
 
 
 # =============================================================================
