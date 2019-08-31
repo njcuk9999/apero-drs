565c0e44dbb7129e22aec88693e1aeea9ca42d44
njcuk9999 <neil.james.cook@gmail.com>
2019-08-31 05:51:29 -0400

    tools.dev.* - change call to core.end_main

--- INTROOT2/terrapipe/tools/dev/changelog.py
+++ INTROOT2/terrapipe/tools/dev/changelog.py
@@ -88,7 +88,7 @@ def main(preview=1, **kwargs):
     # ----------------------------------------------------------------------
     # End Message
     # ----------------------------------------------------------------------
-    params = core.end_main(llmain['params'], recipe, success, outputs=None)
+    params = core.end_main(params, llmain, recipe, success, outputs=None)
     # return a copy of locally defined variables in the memory
     return core.get_locals(params, dict(locals()), llmain)
 
