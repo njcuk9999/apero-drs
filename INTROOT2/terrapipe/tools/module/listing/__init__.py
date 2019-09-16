bd11ca0b42062fdf0882d3307962ba7e11cc026e
Neil Cook <neil.james.cook@gmail.com>
2019-09-16 18:40:57 -0400

    tools.module.listing.general.py - functions for listing.py

--- INTROOT2/terrapipe/tools/module/listing/__init__.py
+++ INTROOT2/terrapipe/tools/module/listing/__init__.py
@@ -9,12 +9,20 @@ Created on 2019-02-13 at 16:37
 
 @author: cook
 """
+from . import general
+
 
 # =============================================================================
 # Define functions
 # =============================================================================
+get_all_files = general.get_all_files
 
-# Nothing to see here.
+get_nightnames = general.get_nightnames
+
+get_outputs = general.get_outputs
+
+remove_index_files = general.remove_index_files
+
 
 # =============================================================================
 # End of code
