ca9b3ccf6a0a06e51c0387b2fc5733ac16430971
Neil Cook <neil.james.cook@gmail.com>
2019-09-18 10:18:49 -0400

    science.calib.shape.py - add length of cube for printout

--- INTROOT2/terrapipe/science/calib/shape.py
+++ INTROOT2/terrapipe/science/calib/shape.py
@@ -162,7 +162,7 @@ def construct_master_fp(params, recipe, dprtype, fp_table, image_ref, **kwargs):
             # log process
             WLOG(params, '', TextEntry('40-014-00008', args=[len(fp_ids)]))
             # median fp cube
-            print('start {0}'.format(Time.now().iso))
+            print('start {0} len(cube)={1}'.format(Time.now().iso), len(cube))
             with warnings.catch_warnings(record=True) as _:
                 groupfp = np.nanmedian(cube, axis=0)
             print('end 1 {0}'.format(Time.now().iso))
