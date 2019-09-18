bf3b6b989b9c42ab5045536f150587572e359192
Neil Cook <neil.james.cook@gmail.com>
2019-09-18 14:04:50 -0400

    update date/version/changelog

--- INTROOT2/terrapipe/core/instruments/default/default_config.py
+++ INTROOT2/terrapipe/core/instruments/default/default_config.py
@@ -105,7 +105,7 @@ DRS_DATA_RUN = Const('DRS_DATA_RUN', dtype='path', source=__NAME__)
 # General properites
 # =============================================================================
 # Version
-DRS_VERSION = Const('DRS_VERSION', value='0.5.072', dtype=str,
+DRS_VERSION = Const('DRS_VERSION', value='0.5.073', dtype=str,
                     source=__NAME__)
 
 # Authors
@@ -119,7 +119,7 @@ DRS_RELEASE = Const('RELEASE', value='alpha pre-release', dtype=str,
                     source=__NAME__)
 
 # Date
-DRS_DATE = Const('DATE', value='2019-09-17', dtype=str, source=__NAME__)
+DRS_DATE = Const('DATE', value='2019-09-18', dtype=str, source=__NAME__)
 
 # Language
 LANGUAGE = Const('LANGUAGE', value='ENG', dtype=str, options=['ENG', 'FR'],
