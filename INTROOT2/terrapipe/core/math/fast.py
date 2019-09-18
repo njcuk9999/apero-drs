0d108b39f929593a03c860877ad383cce44f417a
Neil Cook <neil.james.cook@gmail.com>
2019-09-18 12:15:03 -0400

    core.math.fast.py - first commit numpy nan functions from bottleneck if available

---
+++ INTROOT2/terrapipe/core/math/fast.py
@@ -0,0 +1,269 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+# CODE NAME HERE
+
+# CODE DESCRIPTION HERE
+
+Created on 2019-09-18 at 10:53
+
+@author: cook
+"""
+try:
+    import bottleneck as bn
+    import numpy as np
+    HAS_BOTTLENECK = True
+except Exception as e:
+    import numpy as np
+    HAS_BOTTLENECK = False
+
+from terrapipe.core import constants
+
+# =============================================================================
+# Define variables
+# =============================================================================
+__NAME__ = 'core.math.fast.py'
+__INSTRUMENT__ = None
+# Get constants
+Constants = constants.load(__INSTRUMENT__)
+PConstants = constants.pload(__INSTRUMENT__)
+# Get version and author
+__version__ = Constants['DRS_VERSION']
+__author__ = Constants['AUTHORS']
+__date__ = Constants['DRS_DATE']
+__release__ = Constants['DRS_RELEASE']
+
+
+# =============================================================================
+# Define functions
+# =============================================================================
+def nanargmax(a, axis=None):
+    """
+    Bottleneck or numpy implementation of nanargmax depending on imports
+
+    :param a: numpy array, Input array. If `a` is not an array, a conversion
+              is attempted.
+    :param axis: {int, None}, optional, Axis along which the median is computed.
+                 The default (axis=None) is to compute the median of the
+                 flattened array.
+
+    :type a: np.ndarray
+    :type axis: int
+
+    :return:
+    """
+    if HAS_BOTTLENECK:
+        # return bottleneck function
+        return bn.nanargmax(a, axis=axis)
+    else:
+        # return numpy function
+        return np.nanargmax(a, axis=axis)
+
+
+def nanargmin(a, axis=None):
+    """
+    Bottleneck or numpy implementation of nanargmin depending on imports
+
+    :param a: numpy array, Input array. If `a` is not an array, a conversion
+              is attempted.
+    :param axis: {int, None}, optional, Axis along which the median is computed.
+                 The default (axis=None) is to compute the median of the
+                 flattened array.
+
+    :type a: np.ndarray
+    :type axis: int
+
+    :return:
+    """
+    if HAS_BOTTLENECK:
+        # return bottleneck function
+        return bn.nanargmin(a, axis=axis)
+    else:
+        # return numpy function
+        return np.nanargmin(a, axis=axis)
+
+
+def nanmax(a, axis=None, **kwargs):
+    """
+    Bottleneck or numpy implementation of nanmax depending on imports
+
+    :param a: numpy array, Input array. If `a` is not an array, a conversion
+              is attempted.
+    :param axis: {int, None}, optional, Axis along which the median is computed.
+                 The default (axis=None) is to compute the median of the
+                 flattened array.
+    :param kwargs: keyword arguments passed to numpy function only
+
+    :type a: np.ndarray
+    :type axis: int
+
+    :return:
+    """
+    if HAS_BOTTLENECK and len(kwargs) == 0:
+        # return bottleneck function
+        return bn.nanmax(a, axis=axis)
+    else:
+        # return numpy function
+        return np.nanmax(a, axis=axis, **kwargs)
+
+
+def nanmin(a, axis=None, **kwargs):
+    """
+    Bottleneck or numpy implementation of nanmin depending on imports
+
+    :param a: numpy array, Input array. If `a` is not an array, a conversion
+              is attempted.
+    :param axis: {int, None}, optional, Axis along which the median is computed.
+                 The default (axis=None) is to compute the median of the
+                 flattened array.
+    :param kwargs: keyword arguments passed to numpy function only
+
+    :type a: np.ndarray
+    :type axis: int
+
+    :return:
+    """
+    if HAS_BOTTLENECK and len(kwargs) == 0:
+        # return bottleneck function
+        return bn.nanmin(a, axis=axis)
+    else:
+        # return numpy function
+        return np.nanmin(a, axis=axis, **kwargs)
+
+
+def nanmean(a, axis=None, **kwargs):
+    """
+    Bottleneck or numpy implementation of nanmean depending on imports
+
+    :param a: numpy array, Input array. If `a` is not an array, a conversion
+              is attempted.
+    :param axis: {int, None}, optional, Axis along which the median is computed.
+                 The default (axis=None) is to compute the median of the
+                 flattened array.
+    :param kwargs: keyword arguments passed to numpy function only
+
+    :type a: np.ndarray
+    :type axis: int
+
+    :return:
+    """
+    if HAS_BOTTLENECK and len(kwargs) == 0:
+        # return bottleneck function
+        return bn.nanmean(a, axis=axis)
+    else:
+        # return numpy function
+        return np.nanmean(a, axis=axis, **kwargs)
+
+
+def nanmedian(a, axis=None, **kwargs):
+    """
+    Bottleneck or numpy implementation of nanmedian depending on imports
+
+    :param a: numpy array, Input array. If `a` is not an array, a conversion
+              is attempted.
+    :param axis: {int, None}, optional, Axis along which the median is computed.
+                 The default (axis=None) is to compute the median of the
+                 flattened array.
+    :param kwargs: keyword arguments passed to numpy function only
+
+    :type a: np.ndarray
+    :type axis: int
+
+    :return:
+    """
+    if HAS_BOTTLENECK and len(kwargs) == 0:
+        # return bottleneck function
+        return bn.nanmedian(a, axis=axis)
+    else:
+        # return numpy function
+        return np.nanmedian(a, axis=axis, **kwargs)
+
+
+def nanstd(a, axis=None, ddof=0, **kwargs):
+    """
+    Bottleneck or numpy implementation of nanstd depending on imports
+
+    :param a: numpy array, Input array. If `a` is not an array, a conversion
+              is attempted.
+    :param axis: {int, None}, optional, Axis along which the median is computed.
+                 The default (axis=None) is to compute the median of the
+                 flattened array.
+    :param ddof: int, optional. Means Delta Degrees of Freedom. The divisor
+                 used in calculations is ``N - ddof``, where ``N`` represents
+                 the number of non-NaN elements. By default `ddof` is zero.
+    :param kwargs: keyword arguments passed to numpy function only
+
+    :type a: np.ndarray
+    :type axis: int
+    :type ddof: int
+
+    :return:
+    """
+    if HAS_BOTTLENECK and len(kwargs) == 0:
+        # return bottleneck function
+        return bn.nanstd(a, axis=axis, ddof=ddof)
+    else:
+        # return numpy function
+        return np.nanstd(a, axis=axis, ddof=ddof, **kwargs)
+
+
+def nansum(a, axis=None, **kwargs):
+    """
+    Bottleneck or numpy implementation of nansum depending on imports
+
+    :param a: numpy array, Input array. If `a` is not an array, a conversion
+              is attempted.
+    :param axis: {int, None}, optional, Axis along which the median is computed.
+                 The default (axis=None) is to compute the median of the
+                 flattened array.
+    :param kwargs: keyword arguments passed to numpy function only
+
+    :type a: np.ndarray
+    :type axis: int
+
+    :return:
+    """
+    if HAS_BOTTLENECK and len(kwargs) == 0:
+        # return bottleneck function
+        return bn.nansum(a, axis=axis)
+    else:
+        # return numpy function
+        return np.nansum(a, axis=axis, **kwargs)
+
+
+def median(a, axis, **kwargs):
+    """
+    Bottleneck or numpy implementation of median depending on imports
+
+    :param a: numpy array, Input array. If `a` is not an array, a conversion
+              is attempted.
+    :param axis: {int, None}, optional, Axis along which the median is computed.
+                 The default (axis=None) is to compute the median of the
+                 flattened array.
+    :param kwargs: keyword arguments passed to numpy function only
+
+    :type a: np.ndarray
+    :type axis: int
+
+    :return:
+    """
+    if HAS_BOTTLENECK and len(kwargs) == 0:
+        # return bottleneck function
+        return bn.median(a, axis=axis)
+    else:
+        # return numpy function
+        return np.median(a, axis=axis, **kwargs)
+
+
+# =============================================================================
+# Start of code
+# =============================================================================
+# Main code here
+if __name__ == "__main__":
+    # ----------------------------------------------------------------------
+    # print 'Hello World!'
+    print("Hello World!")
+
+# =============================================================================
+# End of code
+# =============================================================================
