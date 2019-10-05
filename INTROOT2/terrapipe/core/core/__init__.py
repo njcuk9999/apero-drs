1e1263618e6f8a36324cfda96ba30f14ff42c524
njcuk9999 <neil.james.cook@gmail.com>
2019-10-05 17:19:22 -0400

    core.core.__init__.py - remove imports

--- INTROOT2/terrapipe/core/core/__init__.py
+++ INTROOT2/terrapipe/core/core/__init__.py
@@ -10,13 +10,6 @@ Created on 2019-01-18 at 13:46
 @author: cook
 """
 
-from . import drs_argument
-from . import drs_database
-from . import drs_file
-from . import drs_log
-from . import drs_recipe
-from . import drs_startup
-
 # =============================================================================
 # Define functions
 # =============================================================================
