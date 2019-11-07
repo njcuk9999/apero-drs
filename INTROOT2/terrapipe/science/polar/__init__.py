6e554e50ea25635a81e6820fc9b9d53caf75b0aa
Neil Cook <neil.james.cook@gmail.com>
2019-11-07 18:49:53 -0500

    science.polar.* - add whole lsd module

--- INTROOT2/terrapipe/science/polar/__init__.py
+++ INTROOT2/terrapipe/science/polar/__init__.py
@@ -10,6 +10,7 @@ Created on 2019-01-17 at 14:31
 @author: cook
 """
 from terrapipe.science.polar import general
+from terrapipe.science.polar import lsd
 
 __all__ = ['validate_polar_files']
 
@@ -21,6 +22,8 @@ calculate_polarimetry = general.calculate_polarimetry
 calculate_stokes_i = general.calculate_stokes_i
 
 calculate_continuum = general.calculate_continuum
+
+lsd_analysis_wrapper = lsd.lsd_analysis_wrapper
 
 validate_polar_files = general.validate_polar_files
 
