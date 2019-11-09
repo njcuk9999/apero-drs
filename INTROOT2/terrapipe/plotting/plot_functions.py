fc3c3039c18cc2faf8eeb25614a42a0defab2584
Neil Cook <neil.james.cook@gmail.com>
2019-11-08 19:56:35 -0500

    plotting.plot_functions.py - add polar plots

--- INTROOT2/terrapipe/plotting/plot_functions.py
+++ INTROOT2/terrapipe/plotting/plot_functions.py
@@ -924,6 +924,92 @@ def plot_shape_dx(plotter, graph, kwargs):
     plotter.plotend(graph)
 
 
+def plot_shape_linear_tparams(plotter, graph, kwargs):
+    # ------------------------------------------------------------------
+    # start the plotting process
+    if not plotter.plotstart(graph):
+        return
+    plt = plotter.plt
+    # ------------------------------------------------------------------
+    # get the arguments from kwargs
+    image = kwargs['image']
+    x1 = kwargs['x1']
+    x2 = kwargs['x2']
+    y1 = kwargs['y1']
+    y2 = kwargs['y2']
+    # ------------------------------------------------------------------
+    # get image shape
+    dim1, dim2 = image.shape
+    # get calculated parameters
+    diffx = x1 - x2
+    diffy = y1 - y2
+    xrange1 = [0, dim2]
+    xrange2 = [0, dim1]
+    yrange = [-0.05, 0.05]
+    nbins = 50
+    pstep = 100
+    # ------------------------------------------------------------------
+    # set up plot
+    fig, frames = graph.set_figure(plotter, ncols=2, nrows=2)
+    # set up mean points plot
+    mkwargs = dict(color='w', linestyle='None', marker='.')
+    # ----------------------------------------------------------------------
+    # plot[0,0] x1 vs x1 - x2
+    # ----------------------------------------------------------------------
+    frames[0, 0].hist2d(x1, diffx, bins=nbins, range=[xrange1, yrange])
+    frames[0, 0].set(xlabel='x1', ylabel='x1 - x2')
+    # calculate bin mean
+    for pbin in range(0, dim2, pstep):
+        with warnings.catch_warnings(record=True) as _:
+            keep = np.abs(x1 - pbin < nbins)
+            nanmed = mp.nanmedian(diffx[keep])
+        if np.sum(keep) > 100:
+            frames[0, 0].plot([pbin], nanmed, **mkwargs)
+    # ----------------------------------------------------------------------
+    # plot[0,1] y1 vs x1 - x2
+    # ----------------------------------------------------------------------
+    frames[0, 1].hist2d(y1, diffx, bins=nbins, range=[xrange2, yrange])
+    frames[0, 1].set(xlabel='y1', ylabel='x1 - x2')
+    # calculate bin mean
+    for pbin in range(0, dim2, pstep):
+        with warnings.catch_warnings(record=True) as _:
+            keep = np.abs(y1 - pbin < nbins)
+            nanmed = mp.nanmedian(diffx[keep])
+        if np.sum(keep) > 100:
+            frames[0, 0].plot([pbin], nanmed, **mkwargs)
+    # ----------------------------------------------------------------------
+    # plot[1,0] x1 vs y1 - y2
+    # ----------------------------------------------------------------------
+    frames[1, 0].hist2d(x1, diffy, bins=nbins, range=[xrange1, yrange])
+    frames[1, 0].set(xlabel='x1', ylabel='y1 - y2')
+    # calculate bin mean
+    for pbin in range(0, dim2, pstep):
+        with warnings.catch_warnings(record=True) as _:
+            keep = np.abs(x1 - pbin < nbins)
+            nanmed = mp.nanmedian(diffy[keep])
+        if np.sum(keep) > 100:
+            frames[0, 0].plot([pbin], nanmed, **mkwargs)
+    # ----------------------------------------------------------------------
+    # plot[1,1] y1 vs y1 - y2
+    # ----------------------------------------------------------------------
+    frames[1, 1].hist2d(y1, diffy, bins=nbins, range=[xrange2, yrange])
+    frames[1, 1].set(xlabel='y1', ylabel='y1 - y2')
+    # calculate bin mean
+    for pbin in range(0, dim2, pstep):
+        with warnings.catch_warnings(record=True) as _:
+            keep = np.abs(y1 - pbin < nbins)
+            nanmed = mp.nanmedian(diffy[keep])
+        if np.sum(keep) > 100:
+            frames[0, 0].plot([pbin], nanmed, **mkwargs)
+    # ----------------------------------------------------------------------
+    # title
+    # ----------------------------------------------------------------------
+    plt.suptitle('Linear transform parameters')
+    # ------------------------------------------------------------------
+    # wrap up using plotter
+    plotter.plotend(graph)
+
+
 def plot_shape_angle_offset(plotter, graph, kwargs):
     # ------------------------------------------------------------------
     # start the plotting process
@@ -1103,6 +1189,8 @@ def plot_shape_local_zoom_shift(plotter, graph, kwargs):
 
 
 shape_dx = Graph('SHAPE_DX', kind='debug', func=plot_shape_dx)
+shape_linear_tparams = Graph('SHAPE_LINEAR_TPARAMS', kind='debug',
+                             func=plot_shape_linear_tparams)
 shape_angle_offset_all = Graph('SHAPE_ANGLE_OFFSET_ALL', kind='debug',
                                func=plot_shape_angle_offset)
 shape_angle_offset = Graph('SHAPE_ANGLE_OFFSET', kind='debug',
@@ -1119,9 +1207,9 @@ sum_shape_local_zoom_shift = Graph('SUM_SHAPEL_ZOOM_SHIFT', kind='summary',
                                    figsize=(16, 10), dpi=150,
                                    description=sum_desc)
 # add to definitions
-definitions += [shape_dx, shape_angle_offset_all, shape_angle_offset,
-                sum_shape_angle_offset, shape_local_zoom_shift,
-                sum_shape_local_zoom_shift]
+definitions += [shape_dx, shape_linear_tparams, shape_angle_offset_all,
+                shape_angle_offset, sum_shape_angle_offset,
+                shape_local_zoom_shift, sum_shape_local_zoom_shift]
 
 
 # =============================================================================
@@ -2878,6 +2966,190 @@ definitions += [ccf_rv_fit, ccf_rv_fit_loop]
 # =============================================================================
 # Define polarisation plotting functions
 # =============================================================================
+def plot_polar_continuum(plotter, graph, kwargs):
+    # ------------------------------------------------------------------
+    # start the plotting process
+    if not plotter.plotstart(graph):
+        return
+    # ------------------------------------------------------------------
+    # get the arguments from kwargs
+    props = kwargs['props']
+    # get data from props
+    wl = props['FLAT_X']
+    pol = 100 * props['FLAT_POL']
+    contpol = 100.0 * props['CONT_POL']
+    contxbin = np.array(props['CONT_XBIN'])
+    contybin = 100. * np.array(props['CONT_YBIN'])
+    stokes = props['STOKES']
+    method = props['METHOD']
+    nexp = props['NEXPOSURES']
+    # ------------------------------------------------------------------
+    # set up plot
+    fig, frame = graph.set_figure(plotter, nrows=1, ncols=1)
+    # ------------------------------------------------------------------
+    # set up title
+    title = 'Polarimetry: Stokes {0}, Method={1}, for {2} exposures'
+    titleargs = [stokes, method, nexp]
+    # ------------------------------------------------------------------
+    # plot polarimetry data
+    frame.plot(wl, pol, linestyle='None', marker='.',
+               label='Degree of Polarization')
+    # plot continuum sample points
+    frame.plot(contxbin, contybin, linestyle='None', marker='o',
+               label='Continuum Sampling')
+    # plot continuum fit
+    frame.plot(wl, contpol, label='Continuum Polarization')
+    # ---------------------------------------------------------------------
+    # set title and labels
+    xlabel = 'wavelength [nm]'
+    ylabel = 'Degree of polarization for Stokes {0} [%]'.format(stokes)
+    frame.set(title=title.format(*titleargs), xlabel=xlabel, ylabel=ylabel)
+    # ---------------------------------------------------------------------
+    # plot legend
+    frame.legend(loc=0)
+    # ------------------------------------------------------------------
+    # wrap up using plotter
+    plotter.plotend(graph)
+
+
+def plot_polar_results(plotter, graph, kwargs):
+    # ------------------------------------------------------------------
+    # start the plotting process
+    if not plotter.plotstart(graph):
+        return
+    # ------------------------------------------------------------------
+    # get the arguments from kwargs
+    props = kwargs['props']
+    # get data from props
+    wl = props['FLAT_X']
+    pol = 100 * props['FLAT_POL']
+    null1 = 100.0 * props['FLAT_NULL1']
+    null2 = 100.0 * props['FLAT_NULL2']
+    stokes = props['STOKES']
+    method = props['METHOD']
+    nexp = props['NEXPOSURES']
+    # ------------------------------------------------------------------
+    # set up plot
+    fig, frame = graph.set_figure(plotter, nrows=1, ncols=1)
+    # ------------------------------------------------------------------
+    # set up title
+    title = 'Polarimetry: Stokes {0}, Method={1}, for {2} exposures'
+    titleargs = [stokes, method, nexp]
+    # ---------------------------------------------------------------------
+    # plot polarimetry data
+    frame.plot(wl, pol, label='Degree of Polarization')
+    # plot null1 data
+    frame.plot(wl, null1, label='Null Polarization 1')
+    # plot null2 data
+    frame.plot(wl, null2, label='Null Polarization 2')
+    # ---------------------------------------------------------------------
+    # set title and labels
+    xlabel = 'wavelength [nm]'
+    ylabel = 'Degree of polarization for Stokes {0} [%]'.format(stokes)
+    frame.set(title=title.format(*titleargs), xlabel=xlabel, ylabel=ylabel)
+    # ---------------------------------------------------------------------
+    # plot legend
+    frame.legend(loc=0)
+    # ------------------------------------------------------------------
+    # wrap up using plotter
+    plotter.plotend(graph)
+
+
+def plot_polar_stoke_i(plotter, graph, kwargs):
+    # ------------------------------------------------------------------
+    # start the plotting process
+    if not plotter.plotstart(graph):
+        return
+    # ------------------------------------------------------------------
+    # get the arguments from kwargs
+    props = kwargs['props']
+    # get data from props
+    wl = props['FLAT_X']
+    stokes_i = props['FLAT_STOKES_I']
+    stokes_ierr = props['FLAT_STOKES_I_ERR']
+    stokes = props['STOKES']
+    method = props['METHOD']
+    nexp = props['NEXPOSURES']
+    # ------------------------------------------------------------------
+    # set up plot
+    fig, frame = graph.set_figure(plotter, nrows=1, ncols=1)
+    # ------------------------------------------------------------------
+    # set up title
+    title = 'Polarimetry: Stokes {0}, Method={1}, for {2} exposures'
+    titleargs = [stokes, method, nexp]
+    # ---------------------------------------------------------------------
+    # plot polarimetry data
+    frame.errorbar(wl, stokes_i, yerr=stokes_ierr, fmt='-', label='Stokes I',
+                 alpha=0.5)
+    # ---------------------------------------------------------------------
+    # set title and labels
+    xlabel = 'wavelength [nm]'
+    ylabel = 'Stokes {0} total flux (ADU)'.format(stokes)
+    frame.set(title=title.format(*titleargs), xlabel=xlabel, ylabel=ylabel)
+    # ---------------------------------------------------------------------
+    # plot legend
+    frame.legend(loc=0)
+    # ------------------------------------------------------------------
+    # wrap up using plotter
+    plotter.plotend(graph)
+
+
+def plot_polar_lsd(plotter, graph, kwargs):
+    # ------------------------------------------------------------------
+    # start the plotting process
+    if not plotter.plotstart(graph):
+        return
+    # ------------------------------------------------------------------
+    # get the arguments from kwargs
+    pprops = kwargs['pprops']
+    lprops = kwargs['lprops']
+    # get data from props
+    vels = lprops['LSD_VELOCITIES']
+    zz = lprops['LSD_STOKES_I']
+    zgauss = lprops['LSD_STOKES_I_MODEL']
+    z_p = lprops['LSD_STOKES_VQU']
+    z_np = lprops['LSD_NULL']
+    stokes = pprops['STOKES']
+    # ------------------------------------------------------------------
+    # set up plot
+    fig, frames = graph.set_figure(plotter, nrows=1, ncols=3)
+    # ------------------------------------------------------------------
+    frame = frames[0]
+    frame.plot(vels, zz, '-')
+    frame.plot(vels, zgauss, '-')
+    title = 'LSD Analysis'
+    ylabel = 'Stokes I profile'
+    xlabel = ''
+    # set title and labels
+    frame.set(title=title, xlabel=xlabel, ylabel=ylabel)
+    # ---------------------------------------------------------------------
+    frame = frames[1]
+    title = ''
+    frame.plot(vels, z_p, '-')
+    ylabel = 'Stokes {0} profile'.format(stokes)
+    xlabel = ''
+    # set title and labels
+    frame.set(title=title, xlabel=xlabel, ylabel=ylabel)
+    # ---------------------------------------------------------------------
+    frame = frames[2]
+    frame.plot(vels, z_np, '-')
+    xlabel = 'velocity (km/s)'
+    ylabel = 'Null profile'
+    # set title and labels
+    frame.set(title=title, xlabel=xlabel, ylabel=ylabel)
+    # ------------------------------------------------------------------
+    # wrap up using plotter
+    plotter.plotend(graph)
+
+
+polar_continuum = Graph('POLAR_CONTINUUM', kind='debug',
+                        func=plot_polar_continuum)
+polar_results = Graph('POLAR_RESULTS', kind='debug', func=plot_polar_results)
+polar_stokes_i = Graph('POLAR_STOKES_I', kind='debug', func=plot_polar_stoke_i)
+polar_lsd = Graph('POLAR_LSD', kind='debug', func=plot_polar_lsd)
+
+# add to definitions
+definitions += [polar_continuum, polar_results, polar_stokes_i, polar_lsd]
 
 
 # =============================================================================
