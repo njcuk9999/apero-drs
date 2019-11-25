3d19d04976e1794d58bc9c69a5bbc1d908111078
Neil Cook <neil.james.cook@gmail.com>
2019-11-25 18:23:56 -0500

    recipes.spirou - change pol_spirou name + add place holders for obj_spec_spirou and obj_pol_spirou

--- INTROOT2/terrapipe/recipes/spirou/obj_spec_spirou.py
+++ INTROOT2/terrapipe/recipes/spirou/obj_spec_spirou.py
@@ -10,16 +10,21 @@ Created on 2019-11-25 at 16:44
 @author: cook
 """
 from __future__ import division
+import os
 
 from terrapipe import core
 from terrapipe import locale
 from terrapipe.core import constants
-
+from terrapipe.io import drs_fits
+from terrapipe.io import drs_path
+from terrapipe.io import drs_image
+from terrapipe.tools.module.setup import drs_processing
+from terrapipe.core.instruments.spirou import recipe_definitions as rd
 
 # =============================================================================
 # Define variables
 # =============================================================================
-__NAME__ = 'obj_pol_spirou.py'
+__NAME__ = 'obj_spec_spirou.py'
 __INSTRUMENT__ = 'SPIROU'
 # Get constants
 Constants = constants.load(__INSTRUMENT__)
@@ -35,6 +40,13 @@ WLOG = core.wlog
 # Get the text types
 TextEntry = locale.drs_text.TextEntry
 TextDict = locale.drs_text.TextDict
+# get recipes
+CAL_EXTRACT = rd.cal_extract
+MK_TELLU = rd.obj_mk_tellu
+FIT_TELLU = rd.obj_fit_tellu
+MK_TEMPLATE = rd.obj_mk_template
+# get filetypes
+E2DSFF = rd.sf.out_ext_e2dsff
 
 
 # =============================================================================
@@ -48,7 +60,7 @@ TextDict = locale.drs_text.TextDict
 # Everything else is controlled from recipe_definition
 def main(directory=None, files=None, **kwargs):
     """
-    Main function for obj_pol_spirou.py
+    Main function for obj_spec_spirou.py
 
     :param directory: string, the night name sub-directory
     :param files: list of strings or string, the list of files to process
@@ -91,7 +103,75 @@ def __main__(recipe, params):
     # Main Code
     # ----------------------------------------------------------------------
     mainname = __NAME__ + '._main()'
+    # get parameters from inputs
+    drs_plot = params['DRS_PLOT']
+    # get files
+    ppfiles = params['INPUTS']['FILES'][1]
+    # set properties set in run file
+    params.set('STOP_AT_EXCEPTION', True)
+    params.set('TEST_RUN', False)
+    params.set('CORES', params['INPUTS']['CORES'])
 
+
+    # ----------------------------------------------------------------------
+    # get night names for each object
+    night_names, pp_basenames = [], []
+
+    # loop around pp files
+    for ppfile in ppfiles:
+        # append night names
+        night_names.append(drs_path.get_nightname(params, ppfile.filename))
+        # append base names
+        pp_basenames.append(ppfile.basename)
+
+    # ----------------------------------------------------------------------
+    # Extraction
+    # ----------------------------------------------------------------------
+    # arguments are: directory and telluric files
+    gargs = [night_names, pp_basenames]
+    gkwargs = dict()
+    gkwargs['--plot'] = drs_plot
+    gkwargs['--program'] = 'EXTRACT'
+    # run extraction
+    outlist = drs_processing.run_process(params, recipe, CAL_EXTRACT,
+                                         *gargs, **gkwargs)
+    # ----------------------------------------------------------------------
+    # get the fiber types from a list parameter
+    fiber_types = drs_image.get_fiber_types(params)
+    # storage for e2ds files
+    e2dsff_files = dict()
+    # loop around pp files
+    for ppfile in ppfiles:
+        # make e2ds files
+        for fiber in fiber_types:
+            # get copy file instance
+            e2ds_file = E2DSFF.newcopy(recipe=recipe, fiber=fiber)
+            # construct the filename from file instance
+            e2ds_file.construct_filename(params, infile=ppfile)
+            # check whether e2ds file exists
+            if os.path.exists(e2ds_file.filename):
+                # add to storage (for existing fiber)
+                if fiber in e2dsff_files:
+                    e2dsff_files[fiber].append(e2ds_file)
+                # add to storage (for new fiber)
+                else:
+                    e2dsff_files[fiber] = [e2ds_file]
+
+    # ----------------------------------------------------------------------
+    # Fit Tellurics
+    # ----------------------------------------------------------------------
+
+
+
+
+
+    # ----------------------------------------------------------------------
+    # Make template
+    # ----------------------------------------------------------------------
+
+    # ----------------------------------------------------------------------
+    # Fit Tellurics
+    # ----------------------------------------------------------------------
 
     # ----------------------------------------------------------------------
     # End of main code
