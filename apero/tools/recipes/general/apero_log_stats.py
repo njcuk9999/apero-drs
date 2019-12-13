e44ca0c169927ab44bf342879f5509c57aa8e756
Neil Cook <neil.james.cook@gmail.com>
2019-12-13 16:58:53 -0500

    tools.module.testing.drs_log_stats.py + tools.recipes.general.py - add log stat code to apero_log_stats.py

---
+++ apero/tools/recipes/general/apero_log_stats.py
@@ -0,0 +1,148 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+# CODE NAME HERE
+
+# CODE DESCRIPTION HERE
+
+Created on 2019-07-26 at 09:39
+
+@author: cook
+"""
+from __future__ import division
+from astropy.table import Table
+
+from collections import OrderedDict
+
+from apero import core
+from apero import locale
+from apero.core import constants
+from apero.tools.module.testing import drs_log_stats as logstats
+
+
+# =============================================================================
+# Define variables
+# =============================================================================
+__NAME__ = 'apero_log_stats.py'
+__INSTRUMENT__ = 'None'
+# Get constants
+Constants = constants.load(__INSTRUMENT__)
+# Get version and author
+__version__ = Constants['DRS_VERSION']
+__author__ = Constants['AUTHORS']
+__date__ = Constants['DRS_DATE']
+__release__ = Constants['DRS_RELEASE']
+# Get Logging function
+WLOG = core.wlog
+# Get the text types
+TextEntry = locale.drs_text.TextEntry
+TextDict = locale.drs_text.TextDict
+
+
+# =============================================================================
+# Define functions
+# =============================================================================
+# All recipe code goes in _main
+#    Only change the following from here:
+#     1) function calls  (i.e. main(arg1, arg2, **kwargs)
+#     2) fkwargs         (i.e. fkwargs=dict(arg1=arg1, arg2=arg2, **kwargs)
+#     3) config_main  outputs value   (i.e. None, pp, reduced)
+# Everything else is controlled from recipe_definition
+def main(instrument=None, **kwargs):
+    """
+    Main function for cal_dark_spirou.py
+
+    :param directory: string, the night name sub-directory
+    :param files: list of strings or string, the list of files to process
+    :param kwargs: any additional keywords
+
+    :type directory: str
+    :type files: list[str]
+
+    :keyword debug: int, debug level (0 for None)
+
+    :returns: dictionary of the local space
+    :rtype: dict
+    """
+    # assign function calls (must add positional)
+    fkwargs = dict(instrument=instrument, **kwargs)
+    # ----------------------------------------------------------------------
+    # deal with command line inputs / function call inputs
+    recipe, params = core.setup(__NAME__, __INSTRUMENT__, fkwargs)
+    # solid debug mode option
+    if kwargs.get('DEBUG0000', False):
+        return recipe, params
+    # ----------------------------------------------------------------------
+    # run main bulk of code (catching all errors)
+    llmain, success = core.run(__main__, recipe, params)
+    # ----------------------------------------------------------------------
+    # End Message
+    # ----------------------------------------------------------------------
+    return core.end_main(params, llmain, recipe, success, outputs='None')
+
+
+def __main__(recipe, params):
+    """
+    Main code: should only call recipe and params (defined from main)
+
+    :param recipe:
+    :param params:
+    :return:
+    """
+    # ----------------------------------------------------------------------
+    # Main Code
+    # ----------------------------------------------------------------------
+    # get arguments
+    nightname = params['INPUTS']['NIGHTNAME']
+    kind = params['INPUTS']['kind']
+    # load path from kind
+    if kind == 'red':
+        path = params['DRS_DATA_REDUC']
+    else:
+        path = params['DRS_DATA_WORKING']
+    path = '/scratch2/spirou/mini_data/reduced'
+
+    # set up plotting (no plotting before this)
+    recipe.plot.set_location(0)
+
+    # ----------------------------------------------------------------------
+    # Get log files
+    # ----------------------------------------------------------------------
+    # get log files
+    logfiles, nightnames = logstats.get_log_files(params, recipe, path,
+                                                  nightname)
+
+    # ----------------------------------------------------------------------
+    # Open log files
+    # ----------------------------------------------------------------------
+    mastertable = logstats.make_log_table(params, logfiles, nightnames)
+
+    # ----------------------------------------------------------------------
+    # print stats
+    # ----------------------------------------------------------------------
+    logstats.calculate_stats(params, recipe, mastertable)
+
+    # ------------------------------------------------------------------
+    # update recipe log file
+    # ------------------------------------------------------------------
+    # no quality control --> so set passed qc to True
+    recipe.log.no_qc(params)
+    # update log
+    recipe.log.end(params)
+
+    # ----------------------------------------------------------------------
+    # End of main code
+    # ----------------------------------------------------------------------
+    return core.return_locals(params, locals())
+
+
+# =============================================================================
+# Start of code
+# =============================================================================
+if __name__ == "__main__":
+    # run main with no arguments (get from command line - sys.argv)
+    ll = main()
+
+# =============================================================================
+# End of code
+# =============================================================================
