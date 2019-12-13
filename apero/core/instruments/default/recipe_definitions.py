08f8647f4587ce5acbaaa9d2519723c4737723e1
Neil Cook <neil.james.cook@gmail.com>
2019-12-13 16:57:51 -0500

    core.instruments.default.recipe_definitions.py - add logstats recipe

--- apero/core/instruments/default/recipe_definitions.py
+++ apero/core/instruments/default/recipe_definitions.py
@@ -24,6 +24,9 @@ __release__ = Constants['DRS_RELEASE']
 # =============================================================================
 directory = dict(name='directory', dtype='directory',
                  helpstr=Help['DIRECTORY_HELP'])
+# -----------------------------------------------------------------------------
+plot = dict(name='--plot', dtype=int, helpstr=Help['PLOT_HELP'],
+            default_ref='DRS_PLOT', minimum=0, maximum=2)
 
 # =============================================================================
 # List of usable recipes
@@ -33,17 +36,18 @@ drs_recipe = drs_recipe.DrsRecipe
 # Below one must define all recipes and put into the "recipes" list
 test = drs_recipe(__INSTRUMENT__)
 changelog = drs_recipe(__INSTRUMENT__)
+explorer = drs_recipe(__INSTRUMENT__)
 listing = drs_recipe(__INSTRUMENT__)
-reset = drs_recipe(__INSTRUMENT__)
+logstats = drs_recipe(__INSTRUMENT__)
 processing = drs_recipe(__INSTRUMENT__)
 remake_db = drs_recipe(__INSTRUMENT__)
 req_check = drs_recipe(__INSTRUMENT__)
+reset = drs_recipe(__INSTRUMENT__)
 validate = drs_recipe(__INSTRUMENT__)
-explorer = drs_recipe(__INSTRUMENT__)
 
 # push into a list
-recipes = [test, changelog, explorer, reset, processing,
-           remake_db, req_check, listing, validate]
+recipes = [test, changelog, explorer, processing, listing, logstats,
+           remake_db, req_check, reset, validate]
 
 # =============================================================================
 # Recipe definitions
@@ -101,7 +105,7 @@ test.set_kwarg(name='--filelist2', dtype='files', default=[], nargs='+',
                files=[sf.pp_file], helpstr='test 2', required=True)
 
 # -----------------------------------------------------------------------------
-# changelog.py
+# apero_changelog.py
 # -----------------------------------------------------------------------------
 changelog.name = 'apero_changelog.py'
 changelog.instrument = __INSTRUMENT__
@@ -110,7 +114,7 @@ changelog.set_arg(pos=0, name='preview', dtype='bool',
                   helpstr=Help['PREVIEW_HELP'])
 
 # -----------------------------------------------------------------------------
-# changelog.py
+# apero_explorer.py
 # -----------------------------------------------------------------------------
 
 explorer.name = 'apero_explorer.py'
@@ -121,17 +125,52 @@ explorer.set_arg(pos=0, name='instrument', dtype='options',
                  options=['SPIROU', 'NIRPS'])
 
 # -----------------------------------------------------------------------------
-# apero_reset.py
+# apero_listing.py
 # -----------------------------------------------------------------------------
-reset.name = 'apero_reset.py'
-reset.instrument = __INSTRUMENT__
-reset.description = Help['RESET_DESCRIPTION']
-reset.set_arg(pos=0, name='instrument', dtype='options',
-              helpstr=Help['RESET_INST_HELP'], options=['SPIROU', 'NIRPS'])
-reset.set_kwarg(name='--log', dtype='bool', default=True,
-                helpstr=Help['RESET_LOG_HELP'])
-reset.set_kwarg(name='--warn', dtype='bool', default=True,
-                helpstr=Help['RESET_WARN_HELP'])
+listing.name = 'apero_listing.py'
+listing.instrument = __INSTRUMENT__
+listing.description = Help['LISTING_DESC']
+listing.set_arg(pos=0, name='instrument', dtype='options',
+                helpstr=Help['LISTING_HELP_INSTRUMENT'],
+                options=['SPIROU', 'NIRPS'])
+listing.set_kwarg(name='--nightname', dtype=str, default='',
+                  helpstr=Help['LISTING_HELP_NIGHTNAME'])
+listing.set_kwarg(name='--kind', dtype=str, default='raw',
+                  options=['raw', 'tmp', 'red'],
+                  helpstr=Help['LISTING_HELP_KIND'])
+
+# -----------------------------------------------------------------------------
+# apero_log_stats.py
+# -----------------------------------------------------------------------------
+logstats.name = 'apero_log_stats.py'
+logstats.instrument = __INSTRUMENT__
+logstats.description = Help['LOGSTAT_DESC']
+logstats.set_debug_plots('LOGSTATS_BAR')
+logstats.set_summary_plots()
+logstats.set_arg(pos=0, name='instrument', dtype='options',
+                helpstr=Help['LOGSTAT_HELP_INSTRUMENT'],
+                options=['SPIROU', 'NIRPS'])
+logstats.set_kwarg(name='--nightname', dtype=str, default='',
+                  helpstr=Help['LOGSTAT_HELP_NIGHTNAME'])
+logstats.set_kwarg(name='--kind', dtype=str, default='red',
+                  options=['tmp', 'red'],
+                  helpstr=Help['LOGSTAT_HELP_KIND'])
+logstats.set_kwarg(**plot)
+
+# -----------------------------------------------------------------------------
+# apero_mkdb.py
+# -----------------------------------------------------------------------------
+remake_db.name = 'apero_mkdb.py'
+remake_db.instrument = __INSTRUMENT__
+remake_db.description = Help['REMAKE_DESC']
+remake_db.set_arg(pos=0, name='instrument', dtype='options',
+                  helpstr=Help['REMAKE_HELP_INSTRUMENT'],
+                  options=['SPIROU', 'NIRPS'])
+remake_db.set_kwarg(name='--kind', dtype='options',
+                    options=['calibration', 'telluric'],
+                    default_ref='REMAKE_DATABASE_DEFAULT',
+                    helpstr=Help['REMAKE_HELP_KIND'], default='calibration')
+
 
 # -----------------------------------------------------------------------------
 # apero_processing.py
@@ -158,20 +197,6 @@ processing.set_kwarg(name='--test', dtype=str, default='None',
                      options=['True', 'False', '1', '0', 'None'],
                      helpstr=Help['PROCESS_TEST_HELP'])
 
-# -----------------------------------------------------------------------------
-# apero_listing.py
-# -----------------------------------------------------------------------------
-listing.name = 'apero_listing.py'
-listing.instrument = __INSTRUMENT__
-listing.description = Help['LISTING_DESC']
-listing.set_arg(pos=0, name='instrument', dtype='options',
-                helpstr=Help['LISTING_HELP_INSTRUMENT'],
-                options=['SPIROU', 'NIRPS'])
-listing.set_kwarg(name='--nightname', dtype=str, default='',
-                  helpstr=Help['LISTING_HELP_NIGHTNAME'])
-listing.set_kwarg(name='--kind', dtype=str, default='raw',
-                  options=['raw', 'tmp', 'red'],
-                  helpstr=Help['LISTING_HELP_KIND'])
 
 # -----------------------------------------------------------------------------
 # apero_requirements-check.py
@@ -180,19 +205,20 @@ req_check.name = 'apero_dependencies.py'
 req_check.instrument = __INSTRUMENT__
 req_check.description = Help['DEPENDENCIES_DESCRIPTION']
 
+
 # -----------------------------------------------------------------------------
-# apero_mkdb.py
+# apero_reset.py
 # -----------------------------------------------------------------------------
-remake_db.name = 'apero_mkdb.py'
-remake_db.instrument = __INSTRUMENT__
-remake_db.description = Help['REMAKE_DESC']
-remake_db.set_arg(pos=0, name='instrument', dtype='options',
-                  helpstr=Help['REMAKE_HELP_INSTRUMENT'],
-                  options=['SPIROU', 'NIRPS'])
-remake_db.set_kwarg(name='--kind', dtype='options',
-                    options=['calibration', 'telluric'],
-                    default_ref='REMAKE_DATABASE_DEFAULT',
-                    helpstr=Help['REMAKE_HELP_KIND'], default='calibration')
+reset.name = 'apero_reset.py'
+reset.instrument = __INSTRUMENT__
+reset.description = Help['RESET_DESCRIPTION']
+reset.set_arg(pos=0, name='instrument', dtype='options',
+              helpstr=Help['RESET_INST_HELP'], options=['SPIROU', 'NIRPS'])
+reset.set_kwarg(name='--log', dtype='bool', default=True,
+                helpstr=Help['RESET_LOG_HELP'])
+reset.set_kwarg(name='--warn', dtype='bool', default=True,
+                helpstr=Help['RESET_WARN_HELP'])
+
 
 # -----------------------------------------------------------------------------
 # apero_validate.py
