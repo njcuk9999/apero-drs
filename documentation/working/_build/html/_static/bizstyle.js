e9388cea78039163705784ac93ef2f4a166a8504
Neil Cook <neil.james.cook@gmail.com>
2020-02-06 17:18:16 -0500

    documentation.working - update python installation, code links

--- documentation/working/_build/html/_static/bizstyle.js
+++ documentation/working/_build/html/_static/bizstyle.js
@@ -36,6 +36,6 @@ $(window).resize(function(){
         $("li.nav-item-0 a").text("Top");
     }
     else {
-        $("li.nav-item-0 a").text("APERO 0.6.018 documentation");
+        $("li.nav-item-0 a").text("APERO 0.6.036 documentation");
     }
 });
