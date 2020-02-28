61b7d8f745b7716ab7812078fd3ca263e305989f
Neil Cook <neil.james.cook@gmail.com>
2020-02-28 12:23:01 -0500

    core.instruments.default.recipe_defintions.py - update dtype for --trigger argument

--- apero/core/instruments/default/recipe_definitions.py
+++ apero/core/instruments/default/recipe_definitions.py
@@ -243,7 +243,7 @@ processing.set_kwarg(name='--cores', dtype=str, default='None',
 processing.set_kwarg(name='--test', dtype=str, default='None',
                      options=['True', 'False', '1', '0', 'None'],
                      helpstr=Help['PROCESS_TEST_HELP'])
-processing.set_kwarg(name='--trigger', dtype=bool, default=False,
+processing.set_kwarg(name='--trigger', dtype='bool', default=False,
                      helpstr=Help['PROCESS_TRIGGER_HELP'])
 
 # -----------------------------------------------------------------------------
