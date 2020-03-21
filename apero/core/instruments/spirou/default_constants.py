12a4fdc7bb5981f2389fa6d0fae067660e3b2547
njcuk9999 <neil.james.cook@gmail.com>
2020-03-21 13:21:58 -0400

    core.instruments.spirou.default_constants.py - change default mask to masque_sept18_andres_trans50.mas

--- apero/core/instruments/spirou/default_constants.py
+++ apero/core/instruments/spirou/default_constants.py
@@ -1827,7 +1827,7 @@ CCF_MASK_PATH.value = './data/spirou/ccf/'
 
 # Define the default CCF MASK to use
 CCF_DEFAULT_MASK = CCF_DEFAULT_MASK.copy(__NAME__)
-CCF_DEFAULT_MASK.value = 'gl581_Sep18_cleaned.mas'
+CCF_DEFAULT_MASK.value = 'masque_sept18_andres_trans50.mas'
 
 # Define the wavelength units for the mask
 CCF_MASK_UNITS = CCF_MASK_UNITS.copy(__NAME__)
