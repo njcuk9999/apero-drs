b0a5187c98fd2cca5e6960214fc49682f3d60a16
njcuk9999 <neil.james.cook@gmail.com>
2020-03-24 14:33:14 -0400

    tools.module.setup.drs_processing.py - do not check for master if recipe is None

--- apero/tools/module/setup/drs_processing.py
+++ apero/tools/module/setup/drs_processing.py
@@ -958,10 +958,11 @@ def generate_ids(params, runtable, mod, rlist=None, **kwargs):
             run_object.runstring = '{0} --debug={1}'.format(*dargs)
             run_object.update()
         # deal with passing master argument
-        if input_recipe.master:
-            dargs = [run_object.runstring, 'True']
-            run_object.runstring = '{0} --master={1}'.format(*dargs)
-            run_object.update()
+        if input_recipe is not None:
+            if input_recipe.master:
+                dargs = [run_object.runstring, 'True']
+                run_object.runstring = '{0} --master={1}'.format(*dargs)
+                run_object.update()
         # append to list
         if not skip:
             # log that we have validated run
