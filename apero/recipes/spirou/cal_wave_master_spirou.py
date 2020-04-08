1f332ad277d58291b4552390bb461a7e7e355a7d
njcuk9999 <neil.james.cook@gmail.com>
2020-04-08 18:36:13 -0400

    core.instruments.spirou.recipe_definitions.py - remove old cal_wave

--- apero/recipes/spirou/cal_wave_master_spirou.py
+++ apero/recipes/spirou/cal_wave_master_spirou.py
@@ -18,8 +18,7 @@ from apero.core.core import drs_database
 from apero.io import drs_image
 from apero.io import drs_fits
 from apero.science.calib import flat_blaze
-# TODO: When decided between wave and wave_master/wave_night - remove
-from apero.science.calib import wave1 as wave
+from apero.science.calib import wave
 from apero.science.extract import other as extractother
 
 
