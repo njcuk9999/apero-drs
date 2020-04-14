b3c12646f50d0c8a332027668420392c19d6ae9a
njcuk9999 <neil.james.cook@gmail.com>
2020-04-14 13:07:25 -0400

    instruments.spirou.file_definitions.py - update KW_OUTPUT (should be WAVEM)

--- apero/core/instruments/spirou/file_definitions.py
+++ apero/core/instruments/spirou/file_definitions.py
@@ -697,7 +697,7 @@ calib_file.addset(out_leak_master)
 # wave files (master)
 # -----------------------------------------------------------------------------
 # wave solution using hc only
-out_wavem_hc = drs_finput('WAVEM_HC', KW_OUTPUT='WAVE_HC',
+out_wavem_hc = drs_finput('WAVEM_HC', KW_OUTPUT='WAVEM_HC',
                           fibers=['AB', 'A', 'B', 'C'],
                           filetype='.fits',
                           intype=[out_ext_e2ds, out_ext_e2dsff],
@@ -706,7 +706,7 @@ out_wavem_hc = drs_finput('WAVEM_HC', KW_OUTPUT='WAVE_HC',
                           outfunc=out.calib_file)
 
 # wave solution using hc + fp
-out_wavem_fp = drs_finput('WAVEM_FP', KW_OUTPUT='WAVE_FP',
+out_wavem_fp = drs_finput('WAVEM_FP', KW_OUTPUT='WAVEM_FP',
                           fibers=['AB', 'A', 'B', 'C'],
                           filetype='.fits',
                           intype=[out_ext_e2ds, out_ext_e2dsff],
