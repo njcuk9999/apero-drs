3501db99320f1c087ad2ffbcab16765de60652ce
njcuk9999 <neil.james.cook@gmail.com>
2020-04-24 11:50:47 -0400

    io.drs_fits.py + science.preprocessing.identification.py - fix for preprocessing to use input file (was changed due to copyother change)

--- apero/science/preprocessing/identification.py
+++ apero/science/preprocessing/identification.py
@@ -61,7 +61,7 @@ def drs_infile_id(params, recipe, given_drs_file):
     func_name = __NAME__ + '.drs_file_id()'
     # ID DRS FILE
     found, kind = drs_fits.id_drs_file(params, recipe, given_drs_file,
-                                       nentries=1)
+                                       nentries=1, use_input_file=True)
     # return found and drs file that matches the correct type
     return found, kind
 
