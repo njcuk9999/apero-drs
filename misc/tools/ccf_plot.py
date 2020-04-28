3a248bbd5d153fc24e06d70f3845ff3eef40f3cd
Neil Cook <neil.james.cook@gmail.com>
2020-04-28 09:32:54 -0400

    misc.tools.ccf_plot.py - update graph

--- misc/tools/ccf_plot.py
+++ misc/tools/ccf_plot.py
@@ -13,6 +13,7 @@ Created on 2020-01-07 at 18:56
 import numpy as np
 import matplotlib.pyplot as plt
 from astropy.io import fits
+from astropy.time import Time
 from astropy.table import Table
 from astropy import units as u
 from tqdm import tqdm
@@ -22,9 +23,9 @@ import glob
 # =============================================================================
 # Define variables
 # =============================================================================
-WORK_DIRECTORY = '/scratch2/spirou/mini_data/reduced'
+WORK_DIRECTORY = '/scratch3/rali/spirou/mini_data_closest_20200423/reduced'
 
-MASK = 'gl581_sep18_cleaned'
+MASK = 'masque_sept18_andres_trans50_AB'
 OBJNAME = 'Gl699'
 # -----------------------------------------------------------------------------
 TIMECOL = 'MJDMID'
@@ -32,11 +33,9 @@ RV_OBJ = 'RV_OBJ'
 RV_DRIFT = 'RV_DRIFT'
 RV_CORR = 'RV_CORR'
 
-
 # =============================================================================
 # Define functions
 # =============================================================================
-
 
 
 # =============================================================================
@@ -64,41 +63,45 @@ if __name__ == "__main__":
 
     # read the header keys
     for good_header in good_headers:
-        time_arr.append(good_header[TIMECOL])
+        time_arr.append(Time(good_header[TIMECOL], format='mjd'))
         rv_obj_arr.append(good_header[RV_OBJ])
         rv_drift_arr.append(good_header[RV_DRIFT])
         rv_corr_arr.append(good_header[RV_CORR])
 
+    # convert times to time array
+    times = Time(time_arr)
+    times.format = 'iso'  # times in iso YYYY-MM-DD
+
     # convert to numpy arrays
-    time_arr = np.array(time_arr)
     rv_obj_arr = np.array(rv_obj_arr)
     rv_drift_arr = np.array(rv_drift_arr)
     rv_corr_arr = np.array(rv_corr_arr)
 
-    OBJRV = np.nanmean(rv_corr_arr)
-
-    # get minimum time
-    mintime = np.min(time_arr)
-
+    OBJRV = np.nanmedian(rv_corr_arr)
+    # OBJRV = 0.0
 
     # plot
     plt.ioff()
     plt.close()
     fig, frame = plt.subplots(ncols=1, nrows=1)
 
-    frame.scatter(time_arr - mintime, 1000 * (rv_obj_arr - OBJRV),
-                  label='OBJ Non Corr - {0:.3f} km/s'.format(OBJRV))
-    frame.scatter(time_arr - mintime, 1000 * (rv_drift_arr),
-                  label='FP Drift')
-    frame.scatter(time_arr - mintime, 1000 * (rv_corr_arr - OBJRV),
-                  label='OBJ Corr - {0:.3f} km/s'.format(OBJRV))
+    frame.plot_date(times.plot_date, 1000 * (rv_obj_arr - OBJRV),
+                    marker='o', linestyle='None',
+                    label='OBJ Non Corr - {0:.3f} km/s'.format(OBJRV))
+    frame.plot_date(times.plot_date, 1000 * (rv_drift_arr),
+                    marker='o', linestyle='None',
+                    label='FP Drift')
+    frame.plot_date(times.plot_date, 1000 * (rv_corr_arr - OBJRV),
+                    marker='o', linestyle='None',
+                    label='OBJ Corr - {0:.3f} km/s'.format(OBJRV))
 
     frame.legend(loc=0)
+    frame.set(xlabel='Time', ylabel='RV [m/s]')
+    fig.autofmt_xdate()
+    frame.ticklabel_format(axis='y', useOffset=False)
 
-    frame.set(xlabel='Time [MJD] - {0}'.format(mintime),
-              ylabel='RV [m/s]')
     plt.show()
-
+    plt.close()
 
 # =============================================================================
 # End of code
