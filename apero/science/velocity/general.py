b35db6215401090264a729efcb307da684897182
njcuk9999 <neil.james.cook@gmail.com>
2020-06-01 14:18:10 -0400

    apero.science.velocity.general.py - add breakpoint to test bug

--- apero/science/velocity/general.py
+++ apero/science/velocity/general.py
@@ -730,6 +730,8 @@ def compute_ccf_science(params, recipe, infile, image, blaze, wavemap, bprops,
     ll_mask_d, ll_mask_ctr, w_mask = get_ccf_mask(params, **mkwargs)
 
     # calculate the CCF
+    # TODO: remove break point
+    constants.break_point(params)
     props = ccf_calculation(params, image, blaze, wavemap, berv, targetrv,
                             ccfwidth, ccfstep, ll_mask_ctr, w_mask,
                             fit_type, fiber)
@@ -876,6 +878,10 @@ def compute_ccf_fp(params, recipe, infile, image, blaze, wavemap, fiber,
     mkwargs = dict(filename=ccfmask, mask_width=mask_width,
                    mask_units=mask_units)
     ll_mask_d, ll_mask_ctr, w_mask = get_ccf_mask(params, **mkwargs)
+
+
+    # TODO: remove break point
+    constants.break_point(params)
     # calculate the CCF
     props = ccf_calculation(params, image, blaze, wavemap, berv, targetrv,
                             ccfwidth, ccfstep, ll_mask_ctr, w_mask, fit_type,
