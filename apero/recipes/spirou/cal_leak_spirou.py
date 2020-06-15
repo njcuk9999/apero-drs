af80ca63a657ed42d72291ea21fbdd9ed152c499
njcuk9999 <neil.james.cook@gmail.com>
2020-06-15 17:33:49 -0400

    apero.recipes.nirps_ha - update nirps recipes after spirou changes

--- apero/recipes/spirou/cal_leak_spirou.py
+++ apero/recipes/spirou/cal_leak_spirou.py
@@ -53,7 +53,7 @@ EXTRACT_NAME = 'cal_extract_spirou.py'
 # Everything else is controlled from recipe_definition
 def main(directory=None, files=None, **kwargs):
     """
-    Main function for cal_extract_spirou.py
+    Main function for cal_leak_spirou.py
 
     :param directory: string, the night name sub-directory
     :param files: list of strings or string, the list of files to process
