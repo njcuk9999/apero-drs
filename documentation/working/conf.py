0a71d9ad57fb3146da04be86285b8b6e2f8c649e
njcuk9999 <neil.james.cook@gmail.com>
2020-06-18 19:43:51 -0400

    update date/version/changelog/documentation

--- documentation/working/conf.py
+++ documentation/working/conf.py
@@ -22,7 +22,7 @@ copyright = '2019, Neil Cook'
 author = 'Neil Cook'
 
 # The full version, including alpha/beta/rc tags
-release = '0.6.101'
+release = '0.6.106'
 
 # -- General configuration ---------------------------------------------------
 
