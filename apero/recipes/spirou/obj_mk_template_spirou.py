d0c30a3819c329d7f91fc6c970c1b591fe47d3e8
njcuk9999 <neil.james.cook@gmail.com>
2020-07-02 15:21:32 -0400

    apero.recipes.spirou.obj_mk_tempalte_spirou.py - distinguish between getting files from disk or telluric database (telluric database files are cleaned for QC)

--- apero/recipes/spirou/obj_mk_template_spirou.py
+++ apero/recipes/spirou/obj_mk_template_spirou.py
@@ -124,10 +124,18 @@ def __main__(recipe, params):
     fiber = params['INPUTS']['FIBER']
     # ----------------------------------------------------------------------
     # get objects that match this object name
-    object_filenames = drs_fits.find_files(params, recipe, kind='red',
-                                           fiber=fiber,
-                                           KW_OBJNAME=objname,
-                                           KW_OUTPUT=filetype)
+    if params['MKTEMPLATE_FILESOURCE'].upper() == 'DISK':
+        object_filenames = drs_fits.find_files(params, recipe, kind='red',
+                                               fiber=fiber,
+                                               KW_OBJNAME=objname,
+                                               KW_OUTPUT=filetype)
+    else:
+        # define the type of files we want to locate in the telluric database
+        tkey = '{0}_{1}'.format(filetype, fiber)
+        object_filenames = telluric.get_tellu_objs(params, tkey,
+                                                   objnames=[objname])
+
+
     # deal with no files being present
     if len(object_filenames) == 0:
         wargs = [objname, filetype]
