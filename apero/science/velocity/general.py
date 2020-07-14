a2ca85f79c9faeef9c011c7b2eb4276257e8859e
Neil Cook <neil.james.cook@gmail.com>
2020-07-14 18:19:04 -0400

    apero.science.velocity.general.py - add todo about filename

--- apero/science/velocity/general.py
+++ apero/science/velocity/general.py
@@ -607,6 +607,7 @@ def locate_reference_file(params, recipe, infile):
     # set function name
     func_name = display_func(params, 'locate_reference_file', __NAME__)
     # get pp file name
+    # TODO: fix how we get pp file
     pp_filename = infile.filename.split('_pp')[0] + '_pp.fits'
     # get pseudo const
     pconst = constants.pload(params['INSTRUMENT'])
