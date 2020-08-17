c798e527821b08200362a7f785984d4dc7c84d24
Neil Cook <neil.james.cook@gmail.com>
2020-08-17 19:00:01 -0400

    apero.io.drs_data.py - make all relfolders relative to assets directory

--- apero/io/drs_data.py
+++ apero/io/drs_data.py
@@ -4,7 +4,7 @@
 drs data
 
 
-Used to load data files from the ./data/ directory only
+Used to load data files from the assets directory only
 
 Created on 2019-07-02 at 09:24
 
@@ -63,6 +63,7 @@ class LoadException(Exception):
 def load_linelist(params, **kwargs):
     # get parameters from params/kwargs
     func_name = kwargs.get('func', __NAME__ + '.load_full_flat_badpix()')
+    assetdir = pcheck(params, 'DRS_DATA_ASSET', 'assetsdir', kwargs, func_name)
     relfolder = pcheck(params, 'DRS_WAVE_DATA', 'directory', kwargs,
                        func_name)
     filename = pcheck(params, 'WAVE_LINELIST_FILE', 'filename', kwargs,
@@ -78,8 +79,9 @@ def load_linelist(params, **kwargs):
                     func_name)
     return_filename = kwargs.get('return_filename', False)
     # deal with return_filename
+    absfilename = os.path.join(assetdir, relfolder, filename)
     if return_filename:
-        return construct_path(params, filename, relfolder, func=func_name)
+        return absfilename
     # split table columns
     tablecols = list(map(lambda x: x.strip(), tablecols.split(',')))
     # add back to kwargs
@@ -89,16 +91,15 @@ def load_linelist(params, **kwargs):
 
     # return image
     try:
-        table, outf = load_table_file(params, filename, relfolder, kwargs,
-                                      func_name)
-        WLOG(params, '', TextEntry('40-017-00001', args=outf))
+        table = load_table_file(params, absfilename, kwargs, func_name)
+        WLOG(params, '', TextEntry('40-017-00001', args=absfilename))
         # push columns into numpy arrays and force to floats
         ll = np.array(table[wavecol], dtype=float)
         amp = np.array(table[ampcol], dtype=float)
         # return wavelength and amplitude columns
         return ll, amp
     except LoadException:
-        eargs = [filename, relfolder]
+        eargs = [filename, os.path.join(assetdir, relfolder)]
         WLOG(params, 'error', TextEntry('00-017-00002', args=eargs))
 
 
@@ -106,6 +107,7 @@ def load_cavity_files(params, required=True, **kwargs):
 
     func_name = __NAME__ + '.load_cavity_files()'
     # get parameters from params/kwargs
+    assetdir = pcheck(params, 'DRS_DATA_ASSET', 'assetsdir', kwargs, func_name)
     relfolder = pcheck(params, 'DRS_CALIB_DATA', 'directory', kwargs,
                        func_name)
 
@@ -114,10 +116,8 @@ def load_cavity_files(params, required=True, **kwargs):
     filename_ll = pcheck(params, 'CAVITY_LL_FILE', 'filename', kwargs,
                          func_name)
     # construct absolute filenames
-    absfilename_1m = construct_path(params, filename_1m, relfolder,
-                                    func=func_name)
-    absfilename_ll = construct_path(params, filename_ll, relfolder,
-                                    func=func_name)
+    absfilename_1m = os.path.join(assetdir, relfolder, filename_1m)
+    absfilename_ll = os.path.join(assetdir, relfolder, filename_ll)
     # check for absolute path existence
     exists1 = os.path.exists(absfilename_1m)
     exists2 = os.path.exists(absfilename_ll)
@@ -127,10 +127,8 @@ def load_cavity_files(params, required=True, **kwargs):
             return None, None
 
     # load text files
-    fit_1m, _ = load_text_file(params, filename_1m, relfolder, kwargs,
-                               func_name, dtype=float)
-    fit_ll, _ = load_text_file(params, filename_ll, relfolder, kwargs,
-                               func_name, dtype=float)
+    fit_1m = load_text_file(params, filename_1m, func_name, dtype=float)
+    fit_ll = load_text_file(params, filename_ll, func_name, dtype=float)
     # return arrays from text files
     return np.array(fit_1m), np.array(fit_ll)
 
@@ -139,42 +137,48 @@ def save_cavity_files(params, fit_1m_d, fit_ll_d, **kwargs):
     func_name = __NAME__ + '.save_cavity_files()'
     # TODO: Ask about when and where we save this file
     # get parameters from params/kwargs
+    assetdir = pcheck(params, 'DRS_DATA_ASSET', 'assetsdir', kwargs, func_name)
     relfolder = pcheck(params, 'DRS_CALIB_DATA', 'directory', kwargs,
                        func_name)
     filename_1m = pcheck(params, 'CAVITY_1M_FILE', 'filename', kwargs,
                          func_name)
     filename_ll = pcheck(params, 'CAVITY_LL_FILE', 'filename', kwargs,
                          func_name)
+    absfilename_1m = os.path.join(assetdir, relfolder, filename_1m)
+    absfilename_ll = os.path.join(assetdir, relfolder, filename_ll)
     # save the 1m file
-    save_text_file(params, filename_1m, relfolder, fit_1m_d, func_name)
+    save_text_file(params, absfilename_1m, fit_1m_d, func_name)
     # save the ll file
-    save_text_file(params, filename_ll, relfolder, fit_ll_d, func_name)
+    save_text_file(params, absfilename_ll, fit_ll_d, func_name)
 
 
 def load_full_flat_badpix(params, **kwargs):
     # get parameters from params/kwargs
     func_name = kwargs.get('func', __NAME__ + '.load_full_flat_badpix()')
+    assetdir = pcheck(params, 'DRS_DATA_ASSET', 'assetsdir', kwargs, func_name)
     relfolder = pcheck(params, 'DRS_BADPIX_DATA', 'directory', kwargs,
                        func_name)
     filename = pcheck(params, 'BADPIX_FULL_FLAT', 'filename', kwargs,
                       func_name)
     return_filename = kwargs.get('return_filename', False)
     # deal with return_filename
+    absfilename = os.path.join(assetdir, relfolder, filename)
     if return_filename:
-        return construct_path(params, filename, relfolder, func=func_name)
+        return absfilename
     # return image
     try:
-        image, outf = load_fits_file(params, filename, relfolder, func_name)
-        WLOG(params, '', TextEntry('40-012-00003', args=outf))
+        image = load_fits_file(params, absfilename, func_name)
+        WLOG(params, '', TextEntry('40-012-00003', args=absfilename))
         return image
     except LoadException:
-        eargs = [filename, relfolder, func_name]
+        eargs = [filename, os.path.join(assetdir, relfolder), func_name]
         WLOG(params, 'error', TextEntry('00-012-00001', args=eargs))
 
 
 def load_hotpix(params, **kwargs):
     # get parameters from params/kwargs
     func_name = kwargs.get('func', __NAME__ + '.load_full_flat_pp()')
+    assetdir = pcheck(params, 'DRS_DATA_ASSET', 'assetsdir', kwargs, func_name)
     relfolder = pcheck(params, 'DATA_ENGINEERING', 'directory', kwargs,
                        func_name)
     filename = pcheck(params, 'PP_HOTPIX_FILE', 'filename', kwargs,
@@ -184,74 +188,76 @@ def load_hotpix(params, **kwargs):
     kwargs['fmt'] = kwargs.get('fmt', 'csv')
     kwargs['datastart'] = 1
     # deal with return_filename
+    absfilename = os.path.join(assetdir, relfolder, filename)
     if return_filename:
-        return construct_path(params, filename, relfolder, func=func_name)
+        return absfilename
     # return table
     try:
-        table, outf = load_table_file(params, filename, relfolder, kwargs,
-                                      func_name)
-        WLOG(params, '', TextEntry('40-010-00011', args=outf))
+        table = load_table_file(params, absfilename, kwargs, func_name)
+        WLOG(params, '', TextEntry('40-010-00011', args=absfilename))
         return table
     except LoadException:
-        eargs = [filename, relfolder]
+        eargs = [filename, os.path.join(assetdir, relfolder)]
         WLOG(params, 'error', TextEntry('00-010-00002', args=eargs))
 
 
 def load_tapas(params, **kwargs):
     # get parameters from params/kwargs
     func_name = kwargs.get('func', __NAME__ + '.load_full_flat_pp()')
-    relfolder = pcheck(params, 'DATA_CORE', 'directory', kwargs,
+    assetdir = pcheck(params, 'DRS_DATA_ASSET', 'assetsdir', kwargs, func_name)
+    relfolder = pcheck(params, 'TELLU_LIST_DIRECOTRY', 'directory', kwargs,
                        func_name)
     filename = pcheck(params, 'TAPAS_FILE', 'filename', kwargs, func_name)
 
     tablefmt = pcheck(params, 'TAPAS_FILE_FMT', 'fmt', kwargs, func_name)
     return_filename = kwargs.get('return_filename', False)
     # deal with return_filename
+    absfilename = os.path.join(assetdir, relfolder, filename)
     if return_filename:
-        return construct_path(params, filename, relfolder, func=func_name)
+        return absfilename
     # add back to kwargs
     kwargs['fmt'] = tablefmt
     # return image
     try:
-        table, outf = load_table_file(params, filename, relfolder, kwargs,
-                                      func_name)
-        WLOG(params, '', TextEntry('40-999-00002', args=outf))
-        return table, outf
+        table = load_table_file(params, absfilename, kwargs, func_name)
+        WLOG(params, '', TextEntry('40-999-00002', args=absfilename))
+        return table, absfilename
     except LoadException:
-        eargs = [filename, relfolder]
+        eargs = [filename, os.path.join(assetdir, relfolder)]
         WLOG(params, 'error', TextEntry('00-010-00004', args=eargs))
 
 
 def load_object_list(params, **kwargs):
     # get parameters from params/kwargs
     func_name = kwargs.get('func', __NAME__ + '.load_full_flat_pp()')
-    relfolder = pcheck(params, 'DATA_CORE', 'directory', kwargs,
+    assetdir = pcheck(params, 'DRS_DATA_ASSET', 'assetsdir', kwargs, func_name)
+    relfolder = pcheck(params, 'DATABASE_DIR', 'directory', kwargs,
                        func_name)
     filename = pcheck(params, 'OBJ_LIST_FILE', 'filename', kwargs, func_name)
 
     tablefmt = pcheck(params, 'OBJ_LIST_FILE_FMT', 'fmt', kwargs, func_name)
     return_filename = kwargs.get('return_filename', False)
     # deal with return_filename
+    absfilename = os.path.join(assetdir, relfolder, filename)
     if return_filename:
-        return construct_path(params, filename, relfolder, func=func_name)
+        return absfilename
 
     # add back to kwargs
     kwargs['fmt'] = tablefmt
     # return image
     try:
-        table, outf = load_table_file(params, filename, relfolder, kwargs,
-                                      func_name)
-        WLOG(params, '', TextEntry('40-999-00003', args=outf))
+        table = load_table_file(params, absfilename, kwargs, func_name)
+        WLOG(params, '', TextEntry('40-999-00003', args=absfilename))
         return table
     except LoadException:
-        eargs = [filename, relfolder]
+        eargs = [filename, os.path.join(assetdir, relfolder)]
         WLOG(params, 'error', TextEntry('00-010-00005', args=eargs))
 
 
 def load_ccf_mask(params, **kwargs):
     # get parameters from params/kwargs
     func_name = kwargs.get('func', __NAME__ + '.load_ccf_mask()')
-
+    assetdir = pcheck(params, 'DRS_DATA_ASSET', 'assetsdir', kwargs, func_name)
     relfolder = pcheck(params, 'CCF_MASK_PATH', 'directory', kwargs,
                        func_name)
     filename = pcheck(params, 'CCF_MASK', 'filename', kwargs, func_name)
@@ -259,26 +265,26 @@ def load_ccf_mask(params, **kwargs):
     tablefmt = pcheck(params, 'CCF_MASK_FMT', 'fmt', kwargs, func_name)
     return_filename = kwargs.get('return_filename', False)
     # deal with return_filename
+    absfilename = os.path.join(assetdir, relfolder, filename)
     if return_filename:
-        return construct_path(params, filename, relfolder, func=func_name)
+        return absfilename
     # add back to kwargs
     kwargs['fmt'] = tablefmt
     kwargs['colnames'] = ['ll_mask_s', 'll_mask_e', 'w_mask']
     # return image
     try:
-        table, outf = load_table_file(params, filename, relfolder, kwargs,
-                                      func_name)
-        WLOG(params, '', TextEntry('40-020-00002', args=outf))
-        return table, outf
-
+        table = load_table_file(params, absfilename, kwargs, func_name)
+        WLOG(params, '', TextEntry('40-020-00002', args=absfilename))
+        return table, absfilename
     except LoadException:
-        eargs = [filename, relfolder]
+        eargs = [filename, os.path.join(assetdir, relfolder)]
         WLOG(params, 'error', TextEntry('00-020-00002', args=eargs))
 
 
 def load_sp_mask_lsd(params, temperature=None, **kwargs):
     # get parameters from params/kwargs
     func_name = kwargs.get('func', __NAME__ + '.load_sp_mask_lsd()')
+    assetdir = pcheck(params, 'DRS_DATA_ASSET', 'assetsdir', kwargs, func_name)
     relfolder = pcheck(params, 'POLAR_LSD_PATH', 'directory', kwargs,
                        func_name)
     filekey = pcheck(params, 'POLAR_LSD_FILE_KEY', 'filekey', kwargs,
@@ -289,7 +295,7 @@ def load_sp_mask_lsd(params, temperature=None, **kwargs):
     # get filename if None
     if filename is None:
         # get path to directory
-        fulldir = construct_path(params, directory=relfolder, func=func_name)
+        fulldir = os.path.join(assetdir, relfolder)
         # get all files
         allfiles = np.sort(glob.glob('{0}/{1}'.format(fulldir, filekey)))
         # loop around files and get their temperatures
@@ -321,8 +327,9 @@ def load_sp_mask_lsd(params, temperature=None, **kwargs):
         filename = basenames[pos]
     # ----------------------------------------------------------------------
     # deal with return_filename
+    absfilename = os.path.join(assetdir, relfolder, filename)
     if return_filename:
-        return construct_path(params, filename, relfolder, func=func_name)
+        return absfilename
     # ----------------------------------------------------------------------
     # file currently must be an ascii file and must start on line 1
     kwargs['fmt'] = 'ascii'
@@ -331,19 +338,19 @@ def load_sp_mask_lsd(params, temperature=None, **kwargs):
     # ----------------------------------------------------------------------
     # return image
     try:
-        table, outf = load_table_file(params, filename, relfolder, kwargs,
-                                      func_name)
-        WLOG(params, '', TextEntry('40-020-00002', args=outf))
-        return table, outf
+        table = load_table_file(params, absfilename, kwargs, func_name)
+        WLOG(params, '', TextEntry('40-020-00002', args=absfilename))
+        return table, absfilename
 
     except LoadException:
-        eargs = [filename, relfolder]
+        eargs = [filename, os.path.join(assetdir, relfolder)]
         WLOG(params, 'error', TextEntry('00-020-00002', args=eargs))
 
 
 def load_order_mask(params, **kwargs):
     # get parameters from params/kwargs
     func_name = kwargs.get('func', __NAME__ + '.load_sp_mask_lsd()')
+    assetdir = pcheck(params, 'DRS_DATA_ASSET', 'assetsdir', kwargs, func_name)
     relfolder = pcheck(params, 'POLAR_LSD_PATH', 'directory', kwargs,
                        func_name)
     filename = pcheck(params, 'POLAR_LSD_ORDER_MASK', 'filename', kwargs,
@@ -351,8 +358,9 @@ def load_order_mask(params, **kwargs):
     return_filename = kwargs.get('return_filename', False)
     # ----------------------------------------------------------------------
     # deal with return_filename
+    absfilename = os.path.join(assetdir, relfolder, filename)
     if return_filename:
-        return construct_path(params, filename, relfolder, func=func_name)
+        return absfilename
     # ----------------------------------------------------------------------
     # file currently must be an ascii file and must start on line 1
     kwargs['fmt'] = 'ascii'
@@ -361,87 +369,71 @@ def load_order_mask(params, **kwargs):
     # ----------------------------------------------------------------------
     # return image
     try:
-        table, outf = load_table_file(params, filename, relfolder, kwargs,
-                                      func_name)
-        WLOG(params, '', TextEntry('40-020-00002', args=outf))
-        return table, outf
-
+        table = load_table_file(params, absfilename, kwargs, func_name)
+        WLOG(params, '', TextEntry('40-020-00002', args=absfilename))
+        return table, absfilename
     except LoadException:
-        eargs = [filename, relfolder]
+        eargs = [filename, os.path.join(assetdir, relfolder)]
         WLOG(params, 'error', TextEntry('00-020-00002', args=eargs))
 
 
 # =============================================================================
 # Worker functions
 # =============================================================================
-def load_fits_file(params, filename, directory, func_name):
+def load_fits_file(params, filename, func_name):
     # load text dict
     textdict = TextDict(params['INSTRUMENT'], params['LANGUAGE'])
-    # construct filename
-    absfilename = construct_path(params, filename, directory,
-                                 func=func_name)
     # check that filepath exists and log an error if it was not found
-    if not os.path.exists(absfilename):
-        eargs = [absfilename, func_name]
+    if not os.path.exists(filename):
+        eargs = [filename, func_name]
         raise LoadException(textdict['01-001-00022'].format(*eargs))
     # read image
-    image = drs_fits.readfits(params, absfilename)
+    image = drs_fits.readfits(params, filename)
     # return image
-    return image, absfilename
+    return image
 
 
-def load_table_file(params, filename, directory, kwargs, func_name):
+def load_table_file(params, filename, kwargs, func_name):
     # load text dict
     textdict = TextDict(params['INSTRUMENT'], params['LANGUAGE'])
-    # construct filename
-    absfilename = construct_path(params, filename, directory,
-                                 func=func_name)
     # extra parameters
     fmt = kwargs.get('fmt', 'fits')
     colnames = kwargs.get('colnames', None)
     datastart = kwargs.get('datastart', 0)
     # check that filepath exists and log an error if it was not found
-    if not os.path.exists(absfilename):
-        eargs = [absfilename, func_name]
+    if not os.path.exists(filename):
+        eargs = [filename, func_name]
         raise LoadException(textdict['01-001-00022'].format(*eargs))
     # read table
-    table = drs_table.read_table(params, absfilename, fmt=fmt,
+    table = drs_table.read_table(params, filename, fmt=fmt,
                                  colnames=colnames, data_start=datastart)
     # return image
-    return table, absfilename
+    return table
 
 
-def load_text_file(params, filename, directory, kwargs, func_name=None,
-                   dtype=float):
+def load_text_file(params, filename, func_name=None, dtype=float):
     if func_name is None:
         func_name = __NAME__ + '.load_text_file()'
     # load text dict
     textdict = TextDict(params['INSTRUMENT'], params['LANGUAGE'])
-    # construct filename
-    absfilename = construct_path(params, filename, directory,
-                                 func=func_name)
     # check that filepath exists and log an error if it was not found
-    if not os.path.exists(absfilename):
-        eargs = [absfilename, func_name]
+    if not os.path.exists(filename):
+        eargs = [filename, func_name]
         raise LoadException(textdict['01-001-00022'].format(*eargs))
     # load text as list
-    textlist = drs_text.load_text_file(params, absfilename, comments='#',
+    textlist = drs_text.load_text_file(params, filename, comments='#',
                                        delimiter=' ')
     # deal with change list to numpy array
     textlist = np.array(textlist).astype(dtype)
-
     # return image
-    return textlist, absfilename
+    return textlist
 
 
-def save_text_file(params, filename, directory, array, func_name):
+def save_text_file(params, filename, array, func_name):
     if func_name is None:
         func_name = __NAME__ + '.save_text_file()'
-    # construct filename
-    absfilename = construct_path(params, filename, directory,
-                                 func=func_name)
     # save text file
-    drs_text.save_text_file(params, absfilename, array, func_name)
+    drs_text.save_text_file(params, filename, array, func_name)
 
 
 def construct_path(params, filename=None, directory=None, **kwargs):
