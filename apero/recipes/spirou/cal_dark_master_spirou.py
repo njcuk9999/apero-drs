0fb3afedcc83bd72abaa303fc7675e19c12822e8
Neil Cook <neil.james.cook@gmail.com>
2020-09-22 18:20:04 -0400

    drs_fits.find_files -- add filters arg (dict)

--- apero/recipes/spirou/cal_dark_master_spirou.py
+++ apero/recipes/spirou/cal_dark_master_spirou.py
@@ -112,7 +112,7 @@ def __main__(recipe, params):
             WLOG(params, 'error', emsg)
         # get all "filetype" filenames
         files = drs_fits.find_files(params, recipe, kind='tmp',
-                                    KW_DPRTYPE=filetype)
+                                    filters=dict(KW_DPRTYPE=filetype))
         # append to filenames
         filenames += list(files)
     # convert to numpy array
