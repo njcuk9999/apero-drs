3112929b275cf45977290e6e8cd22b4cd11b2d92
Neil Cook <neil.james.cook@gmail.com>
2020-10-08 18:18:50 -0400

    apero.tools.recipe.bin.apero_listing.py - update listing to update index database

--- apero/tools/recipes/bin/apero_listing.py
+++ apero/tools/recipes/bin/apero_listing.py
@@ -9,15 +9,14 @@ Created on 2019-09-16 at 13:48
 
 @author: cook
 """
-import os
-
 from apero.base import base
+from apero.base import drs_text
 from apero import lang
 from apero.core import constants
-from apero.core.core import drs_file
 from apero.core.core import drs_log
 from apero.core.utils import drs_startup
-from apero.tools.module import listing
+from apero.core.utils import drs_utils
+
 
 
 # =============================================================================
@@ -95,72 +94,40 @@ def __main__(recipe, params):
     nightname = params['INPUTS']['NIGHTNAME']
     # get the kidn from inputs
     kind = params['INPUTS']['KIND']
-    # get pseudo constants
-    pconst = constants.pload(params['INSTRUMENT'])
-    # get index file name
-    index_file_name = pconst.INDEX_OUTPUT_FILENAME()
+    # get the white list of nights from inputs
+    whitelist = params['INPUTS'].listp('WNIGHTNAMES')
+    if drs_text.null_text(whitelist, ['None', '', 'All']):
+        whitelist = None
+    # get the black list of nights from inputs
+    blacklist = params['INPUTS'].listp('BNIGHTNAMES')
+    if drs_text.null_text(blacklist, ['None', '', 'All']):
+        blacklist = None
+
     # ----------------------------------------------------------------------
     # Deal with kind
     # ----------------------------------------------------------------------
     # deal with kind
     if kind.lower() == 'raw':
-        # push files in to runs/raw_index
-        _ = drs_file.find_raw_files(params, recipe)
-
-        return drs_startup.return_locals(params, locals())
+        # update the index database (taking into account whitelist/blacklist)
+        indexdbm = drs_utils.update_index_db(params, kind='raw',
+                                             whitelist=whitelist,
+                                             blacklist=blacklist)
     elif kind.lower() == 'tmp':
-        path = params['DRS_DATA_WORKING']
-        columns = pconst.OUTPUT_FILE_HEADER_KEYS()
+        # update the index database (taking into account whitelist/blacklist)
+        indexdbm = drs_utils.update_index_db(params, kind='tmp',
+                                             whitelist=whitelist,
+                                             blacklist=blacklist)
     elif kind.lower() == 'red':
-        path = params['DRS_DATA_REDUC']
-        columns = pconst.OUTPUT_FILE_HEADER_KEYS()
+        # update the index database (taking into account whitelist/blacklist)
+        indexdbm = drs_utils.update_index_db(params, kind='red',
+                                             whitelist=whitelist,
+                                             blacklist=blacklist)
     else:
-        path = params['DRS_DATA_WORKING']
-        columns = pconst.OUTPUT_FILE_HEADER_KEYS()
-    # ----------------------------------------------------------------------
-    # Deal with night name
-    # ----------------------------------------------------------------------
-    if (nightname is None) or (nightname == ''):
-        nightnames = listing.get_nightnames(params, path)
-    else:
-        nightnames = [nightname]
-    # ----------------------------------------------------------------------
-    # Loop over night name
-    # ----------------------------------------------------------------------
-    # loop around night names
-    for nightname in nightnames:
-        # construct path
-        path1 = os.path.join(path, nightname)
-        # test path exists
-        if not os.path.exists(path1):
-            # log error and exit
-            eargs = [nightname, path]
-            WLOG(params, 'error', TextEntry('09-504-00001', args=eargs))
-        # else log the progress
-        else:
-            WLOG(params, 'info', TextEntry('40-504-00005', args=[nightname]))
-        # ----------------------------------------------------------------------
-        # Find all index files and remove them (we are remaking them)
-        # ----------------------------------------------------------------------
-        listing.remove_index_files(params, path1)
-        # ----------------------------------------------------------------------
-        # Get all files in this directory
-        # ----------------------------------------------------------------------
-        files = listing.get_all_files(params, path1)
-        # ----------------------------------------------------------------------
-        # Read header outputs for all these files
-        # ----------------------------------------------------------------------
-        output_dictionary = listing.get_outputs(params, files)
-        # ----------------------------------------------------------------------
-        # Create indexing for all these files
-        # ----------------------------------------------------------------------
-        # get the index path
-        index_path = os.path.join(path1, index_file_name)
-        # index
-        istore = drs_startup.indexing(params, output_dictionary, columns,
-                                      index_path)
-        # save to file
-        drs_startup.save_index_file(params, istore, index_path)
+        for data_kind in ['raw', 'tmp', 'red']:
+            # update the index database (taking into account whitelist/blacklist)
+            indexdbm = drs_utils.update_index_db(params, kind=data_kind,
+                                                 whitelist=whitelist,
+                                                 blacklist=blacklist)
 
     # ----------------------------------------------------------------------
     # End of main code
