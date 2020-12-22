2e0f2cb6fbaa794f839916517fd22fc6f451817f
Neil Cook <neil.james.cook@gmail.com>
2020-12-21 19:16:28 -0500

    apero.tools.module.database.manage_databases.py - add the new astro_obj.update_objects

--- apero/tools/module/database/database_gui.py
+++ apero/tools/module/database/database_gui.py
@@ -103,7 +103,8 @@ class DatabaseHolder:
         # start database
         database = drs_db.database_wrapper(self.kind, self.path)
         # push dataframe to replace SQL table
-        database.add_from_pandas(df, if_exists='replace', index=False,
+        database.add_from_pandas(df, table=self.kind,
+                                 if_exists='replace', index=False,
                                  commit=True)
         # print we are saving database
         print('Saving database {0}'.format(self.name))
