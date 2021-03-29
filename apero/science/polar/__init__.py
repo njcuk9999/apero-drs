14093030d0c5a6e86c512472a54ad6c7e3c205f2
Neil Cook <neil.james.cook@gmail.com>
2021-03-29 16:41:55 -0400

    apero.recipes.spirou.obj_pol_spirou.py - start integrating polar code

--- apero/science/polar/__init__.py
+++ apero/science/polar/__init__.py
@@ -9,29 +9,12 @@ Created on 2019-01-17 at 14:31
 
 @author: cook
 """
-from apero.science.polar import gen_pol
-from apero.science.polar import lsd
-
-__all__ = ['validate_polar_files']
+__all__ = []
 
 # =============================================================================
 # Define functions
 # =============================================================================
-calculate_polarimetry = gen_pol.calculate_polarimetry
 
-calculate_stokes_i = gen_pol.calculate_stokes_i
-
-calculate_continuum = gen_pol.calculate_continuum
-
-generate_statistics = gen_pol.generate_statistics
-
-lsd_analysis_wrapper = lsd.lsd_analysis_wrapper
-
-validate_polar_files = gen_pol.validate_polar_files
-
-quality_control = gen_pol.quality_control
-
-write_files = gen_pol.write_files
 
 # =============================================================================
 # End of code
