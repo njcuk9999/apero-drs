b8d54bc3cc0131ebdb2dd73947458305e8c42f67
Neil Cook <neil.james.cook@gmail.com>
2021-07-14 15:53:22 -0400

    add pp_ff and ext_ff to runs

--- apero/core/instruments/nirps_ha/recipe_definitions.py
+++ apero/core/instruments/nirps_ha/recipe_definitions.py
@@ -1441,6 +1441,8 @@ pp_seq_opt.add(apero_preprocess, name='PP_HC1HC1', files=[files.raw_hc1_hc1],
                recipe_kind='pre-hchc')
 pp_seq_opt.add(apero_preprocess, name='PP_FPFP', files=[files.raw_fp_fp],
                recipe_kind='pre-fpfp')
+pp_seq_opt.add(apero_extract, name='PP_FF', files=[files.raw_flat_flat],
+               recipe_kind='pre-ff')
 pp_seq_opt.add(apero_preprocess, name='PP_DFP', files=[files.raw_dark_fp],
                recipe_kind='pre-dfp')
 pp_seq_opt.add(apero_preprocess, name='PP_SKY', files=[files.raw_dark_dark_sky],
@@ -1607,6 +1609,8 @@ eng_seq.add(apero_extract, name='EXT_HC1HC1', files=[files.pp_hc1_hc1],
             recipe_kind='extract-hchc')
 eng_seq.add(apero_extract, name='EXT_FPFP', files=[files.pp_fp_fp],
             recipe_kind='extract-fpfp')
+eng_seq.add(apero_extract, name='EXT_FF', files=[files.pp_flat_flat],
+            recipe_kind='extract-ff')
 eng_seq.add(apero_extract, name='EXT_DFP', files=[files.pp_dark_fp],
             recipe_kind='extract-dfp')
 eng_seq.add(apero_extract, name='EXT_SKY', files=[files.pp_dark_dark_sky],
