ce76f9e7494d81484c8e9677732ada7c92d8b66b
Neil Cook <neil.james.cook@gmail.com>
2021-09-13 15:35:20 -0400

    apero.tools.module.processing.drs_processing.py - must not update database in test run for Pool and Process

--- apero/tools/module/processing/drs_processing.py
+++ apero/tools/module/processing/drs_processing.py
@@ -2221,12 +2221,15 @@ def _multi_process_process(params, runlist, cores, groupname=None,
             # debug log: MULTIPROCESS - joining job {0}
             WLOG(params, 'debug', textentry('90-503-00021', args=[pit]))
             proc.join()
+        # ---------------------------------------------------------------------
         # update the index database (taking into account include/exclude lists)
         #    we have to loop around block kinds to prevent recipe from updating
         #    the index database every time a new recipe starts
         # this is really important as we have disabled updating for parallel
         #  runs to make it more efficient
-        update_index_db(params)
+        # do not update if we are running a test
+        if not params['TEST_RUN']:
+            update_index_db(params)
 
     # return return_dict
     return dict(return_dict)
