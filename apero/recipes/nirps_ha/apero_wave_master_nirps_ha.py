df16820fb1df62ef779c71211cd275a73a15cfd0
Neil Cook <neil.james.cook@gmail.com>
2021-09-22 12:58:08 -0400

    flat + wave - combine with sum not median

--- apero/recipes/nirps_ha/apero_wave_master_nirps_ha.py
+++ apero/recipes/nirps_ha/apero_wave_master_nirps_ha.py
@@ -135,11 +135,11 @@ def __main__(recipe, params):
     # combine input images if required
     elif params['INPUT_COMBINE_IMAGES']:
         # get combined file
-        cond1 = drs_file.combine(params, recipe, hcfiles, math='median')
+        cond1 = drs_file.combine(params, recipe, hcfiles, math='sum')
         hcfiles = [cond1[0]]
         # get combined file
         if fpfiles is not None:
-            cond2 = drs_file.combine(params, recipe, fpfiles, math='median')
+            cond2 = drs_file.combine(params, recipe, fpfiles, math='sum')
             fpfiles = [cond2[0]]
         combine = True
     else:
