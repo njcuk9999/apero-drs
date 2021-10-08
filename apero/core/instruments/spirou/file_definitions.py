ebd8c11ea4fa5dc3d85817e8146fe8b7f735bb17
Neil Cook <neil.james.cook@gmail.com>
2021-10-08 15:49:27 -0400

    apero.core.instruments.*.file_definitions.py - add TELLU_MODEL to file_definitions.py

--- apero/core/instruments/spirou/file_definitions.py
+++ apero/core/instruments/spirou/file_definitions.py
@@ -1049,7 +1049,7 @@ out_tellu_trans = drs_finput('TELLU_TRANS', hkeys=dict(KW_OUTPUT='TELLU_TRANS'),
 out_tellu_model = drs_finput('TRANS_MODEL', hkeys=dict(KW_OUTPUT='TRANS_MODEL'),
                              fibers=['AB', 'A', 'B'], filetype='.fits',
                              basename = 'trans_model_{0}', # add fiber manually
-                             dbname='telluric', dbkey='TELLU_TAPAS',
+                             dbname='telluric', dbkey='TELLU_MODEL',
                              outfunc=out.set_file)
 
 # add make_telluric outputs to output fileset
