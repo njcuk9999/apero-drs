866549df26ed5556868040bce5c3a247452b74e5
Neil Cook <neil.james.cook@gmail.com>
2021-11-05 12:12:58 -0400

    update date/version/changelog/docs/update notes

--- documentation/working/conf.py
+++ documentation/working/conf.py
@@ -22,7 +22,7 @@ copyright = '2019, Neil Cook'
 author = 'Neil Cook'
 
 # The full version, including alpha/beta/rc tags
-release = '0.7.173'
+release = '0.7.179'
 
 # -- General configuration ---------------------------------------------------
 
