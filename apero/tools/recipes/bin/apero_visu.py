e1e90cf84af9e03467a8863f891e1f16be19e5f0
Neil Cook <neil.james.cook@gmail.com>
2022-02-22 14:11:05 -0500

    test visualisations

--- apero/tools/recipes/bin/apero_visu.py
+++ apero/tools/recipes/bin/apero_visu.py
@@ -9,23 +9,91 @@ Created on 2022-02-08
 
 @author: cook
 """
+import numpy as np
+import os
+import shutil
+from typing import Dict, List, Tuple
+
+from apero.base import base
+from apero.core import constants
+from apero.core.core import drs_log
+from apero.core.core import drs_database
+from apero.core.core import drs_text
+from apero.core.utils import drs_startup
+from apero.tools.module.listing import drs_get
 
 # =============================================================================
 # Define variables
 # =============================================================================
+__NAME__ = 'apero_visu.py'
+__INSTRUMENT__ = 'None'
+__PACKAGE__ = base.__PACKAGE__
+__version__ = base.__version__
+__author__ = base.__author__
+__date__ = base.__date__
+__release__ = base.__release__
+# Get Logging function
+WLOG = drs_log.wlog
+ParamDict = constants.ParamDict
 
 
 # =============================================================================
 # Define functions
 # =============================================================================
+def main(**kwargs):
+    """
+    Main function for apero_explorer.py
+
+    :param instrument: str, the instrument name
+    :param kwargs: additional keyword arguments
+
+    :type instrument: str
+
+    :keyword debug: int, debug level (0 for None)
+
+    :returns: dictionary of the local space
+    :rtype: dict
+    """
+    # assign function calls (must add positional)
+    fkwargs = dict(**kwargs)
+    # ----------------------------------------------------------------------
+    # deal with command line inputs / function call inputs
+    recipe, params = drs_startup.setup(__NAME__, __INSTRUMENT__, fkwargs,
+                                       enable_plotter=False)
+    # solid debug mode option
+    if kwargs.get('DEBUG0000', False):
+        return recipe, params
+    # ----------------------------------------------------------------------
+    # run main bulk of code (catching all errors)
+    llmain, success = drs_startup.run(__main__, recipe, params)
+    # ----------------------------------------------------------------------
+    # End Message
+    # ----------------------------------------------------------------------
+    return drs_startup.end_main(params, llmain, recipe, success, outputs='None')
+
+
+def __main__(recipe, params):
+    """
+    Main function - using user inputs (or gui inputs) filters files and
+    copies them to a new location
+
+    :param instrument: string, the instrument name
+    :type: str
+    :return: returns the local namespace as a dictionary
+    :rtype: dict
+    """
+    # ----------------------------------------------------------------------
+    # End of main code
+    # ----------------------------------------------------------------------
+    return drs_startup.return_locals(params, locals())
 
 
 # =============================================================================
 # Start of code
 # =============================================================================
 if __name__ == "__main__":
-    # print hello world
-    print('Hello World')
+    # run main with no arguments (get from command line - sys.argv)
+    ll = main()
 
 # =============================================================================
 # End of code
