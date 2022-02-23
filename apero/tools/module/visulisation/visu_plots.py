07fe61b8cb62b671a5a345554301c2927e4e6879
Neil Cook <neil.james.cook@gmail.com>
2022-02-23 13:19:08 -0500

    update visualisation test code

--- apero/tools/module/visulisation/visu_plots.py
+++ apero/tools/module/visulisation/visu_plots.py
@@ -13,16 +13,13 @@ import numpy as np
 import warnings
 
 import bokeh
-from bokeh.themes import built_in_themes
-from bokeh.io import curdoc, show, output_file
 from bokeh.plotting import figure
-from bokeh.layouts import grid, row, column
+from bokeh.layouts import grid, row, column, Spacer
 from bokeh.models import HoverTool, CheckboxButtonGroup
-from bokeh.models import ColumnDataSource, Slider, TextInput, Button
-from bokeh.models import Range1d, Dropdown
-from bokeh.models.widgets.inputs import AutocompleteInput
-from bokeh.models.widgets import Paragraph
-from bokeh.models.widgets import DataTable, DateFormatter, TableColumn
+from bokeh.models import ColumnDataSource, Slider, Button
+from bokeh.models import Range1d, Dropdown, RangeSlider
+from bokeh.models.widgets import DataTable, TableColumn
+from bokeh.models.widgets import  Div
 
 from apero.core import constants
 from apero.core import math as mp
@@ -35,6 +32,14 @@ from apero.tools.module.visulisation import visu_core
 DEBUG = True
 # get params
 PARAMS = constants.load()
+# define log text
+LOG_HTML = '<strong>Log:</strong><br><br>'
+# tool tip
+e2ds_span = """
+<div>
+<span style="font-size: 12px; color: {color};">{name}: (x=@{x}, y=@{y})</span>
+</div>
+"""
 
 
 # =============================================================================
@@ -51,6 +56,9 @@ def test_plot(**kwargs) -> bokeh.models.Model:
     plot1.yaxis.axis_label = kwargs['ylabel']
 
     return grid([plot1])
+
+
+
 
 
 # =============================================================================
@@ -70,18 +78,22 @@ class SpectrumPlot:
         self.obs_dirs = visu_core.get_obs_dirs()
         self.identifiers = []
         # line variables
-        self.line_bkind = ['red', 'red', 'red', 'red']
-        self.line_labels = ['e2ds', 'tcorr', 'recon', 'skymodel']
-        self.line_otypes = ['EXT_E2DS_FF', 'TELLU_OBJ', 'TELLU_RECON',
-                            'TELLU_PCLEAN']
-        self.line_norm = ['med', 'med', None, 'max']
-        self.line_blaze_cor = [True, True, False, False]
-        self.line_oext = [1, 1, 1, 4]
-        self.line_active = [0, 1, 2, 3]
-        self.line_colors = ['black', 'red', 'blue', 'orange']
-        self.line_alphas = [0.5, 0.5, 0.5, 0.5]
-        self.line_dc = [0, 0, 0, 1]
-        self.lines = [None, None, None, None]
+        self.line_bkind = ['red', 'red', 'red', 'red', 'red']
+        self.line_labels = ['e2dsff', 'e2ds', 'tcorr', 'recon', 'skymodel']
+        self.line_otypes = ['EXT_E2DS_FF', 'EXT_E2DS', 'TELLU_OBJ',
+                            'TELLU_RECON', 'TELLU_PCLEAN']
+        self.line_norm = ['med', 'med', 'med', None, 'max']
+        self.line_blaze_cor = [True, True, True, False, False]
+        self.line_oext = [1, 1, 1, 1, 4]
+        self.line_active = [0, 2, 3, 4]
+        self.line_colors = ['black', 'purple', 'red', 'blue', 'orange']
+        self.line_hexcolors = ['#212f3d ', '#9b59b6', ' #e74c3c', ' #3498db',
+                               '#e67e22']
+        self.line_alphas = [0.75, 0.75, 0.75, 0.75, 0.75]
+        self.line_widths = [1.25, 1.25, 1.25, 1, 1]
+        self.line_dc = [0, 0, 0, 0, 1]
+        self.lines = [None, None, None, None, None]
+        # data source
         self.source = ColumnDataSource()
         # object table
         self.obj_hkeys = ['KW_OBJNAME', 'KW_DPRTYPE', 'KW_DATE_OBS',
@@ -94,6 +106,8 @@ class SpectrumPlot:
         self.object_cols = [TableColumn(field='keys', title=''),
                             TableColumn(field='values', title='')]
         self.current_filename = 'None'
+        # create div spinner
+        self.div_spinner = Div(text="",width=120,height=120)
         # widgets
         self.obs_dir_widget = None
         self.identifier_widget = None
@@ -106,8 +120,11 @@ class SpectrumPlot:
         # other variables
         self.xmin = 0
         self.xmax = 1
+        self.xstep = 0.1
         self.ymin = 0
         self.ymax = 1
+        self.yrange = Range1d(self.ymin, self.ymax)
+        self.xrange = Range1d(self.xmin, self.xmax)
         # whether we currently have identifier loaded
         self.loaded_params = dict(identifier=self.identifier,
                                   obs_dir=self.obs_dir,
@@ -119,7 +136,10 @@ class SpectrumPlot:
         self.create()
 
     def create(self):
+        # add hover tool
+        self.hover = HoverTool(tooltips=self.tooltip())
 
+        # add widgets
         self.widgets = []
         # ---------------------------------------------------------------------
         # create obs dir text widget
@@ -161,15 +181,43 @@ class SpectrumPlot:
         self.lines_widget = CheckboxButtonGroup(labels=self.line_labels,
                                                 active=self.line_active)
         self.lines_widget.on_change('active', self.update_lines)
+        self.lines_widget.disabled = True
         self.widgets.append(self.lines_widget)
+        # ---------------------------------------------------------------------
+        self.wave_widget = RangeSlider(title='Adjust wavelength range',
+                                       start=self.xmin,
+                                       end=self.xmax,
+                                       value=(self.xmin, self.xmax),
+                                       step=self.xstep)
+        # wave_widgetcallback = CustomJS(args=dict(p=self.figure),
+        #                                code="""
+        #                                     var a = cb_obj.value;
+        #                                     p.x_range.start = a[0];
+        #                                     p.x_range.end = a[1];
+        #                                     """)
+        self.wave_widget.on_change('value', self.update_wave_axis)
+        self.wave_widget.disabled = True
+        self.widgets.append(self.wave_widget)
         # ---------------------------------------------------------------------
         # Add object box
         self.object_table = DataTable(source=self.object_source,
                                       columns=self.object_cols)
         self.update_hkeys()
+        self.object_table.disabled = True
         self.widgets.append(self.object_table)
         # ---------------------------------------------------------------------
+        # add spacer
+        self.widget_spacer = Spacer(width=640, height=200)
+        self.widgets.append(self.widget_spacer)
+        # ---------------------------------------------------------------------
+        # logger
+        self.log_widget = Div(text=LOG_HTML, background='aliceblue',
+                              width=640, height=128, align='end')
+        self.widgets.append(self.log_widget)
 
+    # =========================================================================
+    # Update functions
+    # =========================================================================
     def update_obs_dir(self, event):
         self.obs_dir = str(event.item)
         # update
@@ -186,6 +234,35 @@ class SpectrumPlot:
         self.order_num = int(self.order_num_widget.value)
         self.update_hkeys()
 
+    def update_waveord_limits(self):
+        # get wavemap for current order
+        key = 'wavemap[{0}]'.format(self.order_num)
+        if key not in self.source.data:
+            return
+        waveord = self.source.data[key]
+        # update limtes
+        self.xmin = np.min(waveord)
+        self.xmax = np.max(waveord)
+        self.xstep = np.mean(np.diff(waveord))
+        # update widget
+        self.wave_widget.start = self.xmin
+        self.wave_widget.end = self.xmax
+        self.wave_widget.step = self.xstep
+        self.wave_widget.value = (self.xmin, self.xmax)
+
+    def update_wave_axis(self, attrname, old, new):
+        _ = attrname, old, new
+        low, high = self.wave_widget.value
+        # self.figure.x_range.bounds = (low, high)
+        self.figure.x_range.start = low
+        self.figure.x_range.end = high
+        # self.yrange.start = self.ymin
+        # self.yrange.end = self.ymax
+        # self.xrange.start = low
+        # self.xrange.end = high
+        # self.figure.y_range = self.yrange
+        # self.figure.x_range = self.xrange
+
     def update_fiber(self, event):
         self.fiber = event.item
 
@@ -195,9 +272,9 @@ class SpectrumPlot:
 
     def update_graph_on_click(self):
         self.update_graph()
+        self.update_waveord_limits()
 
     def update_graph(self):
-        # _ = attrname, old, new
         self.update_hkeys()
         # get loaded params
         lparams = dict(identifier=self.identifier, obs_dir=self.obs_dir,
@@ -206,7 +283,6 @@ class SpectrumPlot:
         if DEBUG:
             print('Update')
             print(lparams)
-
         # check condition to update file
         cond_update = False
         for key in self.loaded_params:
@@ -215,11 +291,16 @@ class SpectrumPlot:
 
         # if identifier is the same as loaded we just check line visibility
         if cond_update or not self.valid:
+            # update files
+            self.logger('Loading data...')
             self.update_files()
+            self.reset_logger()
         # update line visibility
         if self.valid:
             # replot
+            self.logger('Plotting data...')
             self.plot()
+            self.reset_logger()
             # update line visibility
             self.update_line_visibility()
 
@@ -233,12 +314,14 @@ class SpectrumPlot:
         # loop around lines and change visibility
         for it in range(0, len(self.lines)):
             if it in switch:
-                self.lines[it].visible = True
-            else:
+                if self.lines[it] is None:
+                    self.plot_line(it)
+                else:
+                    self.lines[it].visible = True
+            elif self.lines[it] is not None:
                 self.lines[it].visible = False
-
-    def error(self):
-        pass
+        # update tool tip
+        self.hover.tooltips = self.tooltip()
 
     def update_hkeys(self, header=None):
         # deal with not having header
@@ -283,10 +366,7 @@ class SpectrumPlot:
         new_keys += ['PATH', 'OBS_DIR', 'ID']
         new_values += [PARAMS['DRS_DATA_REDUC'], self.obs_dir,
                        self.identifier]
-        # update value
-        if DEBUG:
-            print(list(zip(new_keys, new_values)))
-
+        # update object source
         sdict = dict(keys=new_keys, values=new_values)
         self.object_source.data = sdict
 
@@ -325,6 +405,10 @@ class SpectrumPlot:
         if np.sum(self.line_blaze_cor) > 0:
             if filename is not None:
                 blaze, blazefile = visu_core.get_calib(header, 'BLAZE')
+                # push into source
+                for order_num in range(self.order_max):
+                    key = 'blaze[{0}]'.format(order_num)
+                    self.source.data[key] = blaze[order_num]
             else:
                 if DEBUG:
                     print('Cannot find data')
@@ -332,7 +416,11 @@ class SpectrumPlot:
                 return
         # ----------------------------------------------------------------------
         # get the wave solution
-        # TODO: get wave solution and change x axis
+        wavemap, wavefile = visu_core.get_calib(header, 'WAVE')
+        # push into source
+        for order_num in range(self.order_max):
+            key = 'wavemap[{0}]'.format(order_num)
+            self.source.data[key] = wavemap[order_num]
         # -----------------------------------------------------------------
         # copy source dict
         sdict = dict(self.source.data)
@@ -358,6 +446,11 @@ class SpectrumPlot:
                 self.valid = False
                 return
             else:
+
+                # enable some widgets and set valid
+                self.wave_widget.disabled = False
+                self.lines_widget.disabled = False
+                self.object_table = False
                 self.valid = True
                 # set loaded params
                 self.loaded_params = dict(identifier=self.identifier,
@@ -368,20 +461,20 @@ class SpectrumPlot:
             # update max order
             self.order_max = data.shape[0]
             self.order_num_widget.end = self.order_max
-            self.xmin = 0
-            self.xmax = data.shape[1]
+            self.update_waveord_limits()
             # -----------------------------------------------------------------
             # correct for blaze
             if self.line_blaze_cor[it] and blaze is not None:
                 data = data / blaze
-
+            # -----------------------------------------------------------------
             # get dc level
             dclevel = self.line_dc[it]
+            # -----------------------------------------------------------------
             # push into storage
             for order_num in range(data.shape[0]):
                 # add x values
                 sxname = 'x_{0}[{1}]'.format(name, order_num)
-                sdict[sxname] = np.arange(data.shape[1])
+                sdict[sxname] = wavemap[order_num]
                 # add y values
                 syname = 'flux_{0}[{1}]'.format(name, order_num)
                 with warnings.catch_warnings(record=True) as _:
@@ -391,8 +484,13 @@ class SpectrumPlot:
                         norm = np.nanmax(data[order_num])
                     else:
                         norm = np.ones_like(data[order_num])
-                    sdict[syname] = (data[order_num] / norm) + dclevel
-
+                    # norm and add dc level
+                    newdata = (data[order_num] / norm)
+                # -------------------------------------------------------------
+                # finally add the new data to sdict
+                #    (with a dc level if required)
+                sdict[syname] = newdata + dclevel
+            # -----------------------------------------------------------------
             # get x and y values for start position
             sxname0 = 'x_{0}[{1}]'.format(name, self.order_num)
             syname0 = 'flux_{0}[{1}]'.format(name, self.order_num)
@@ -404,46 +502,86 @@ class SpectrumPlot:
         # update source
         self.source.data = sdict
 
+    # =========================================================================
+    # Features
+    # =========================================================================
     def plot(self):
-        # get order number
-        order_num = self.order_num
+
         # reset ymin and ymax
         self.ymin, self.ymax = 0, 1
+        # set axis labels
+        self.figure.xaxis.axis_label = 'Wavelength [nm]'
+        self.figure.yaxis.axis_label = 'Normalized Flux'
+        # add hover
+        self.figure.add_tools(self.hover)
         # loop around lines
         for it in range(len(self.line_labels)):
-            # get name
-            name = self.line_labels[it]
-            color = self.line_colors[it]
-            alpha = self.line_alphas[it]
+            self.plot_line(it)
 
-            # get x and y values
-            sxname = 'x_{0}[{1}]'.format(name, order_num)
-            syname = 'flux_{0}[{1}]'.format(name, order_num)
-            # assign current values
-            cxname = 'current_{0}_x'.format(name)
-            cyname = 'current_{0}_y'.format(name)
-            # update current
-            self.source.data[cxname] = self.source.data[sxname]
-            self.source.data[cyname] = self.source.data[syname]
-            # adjust ymin and ymax
-            y = self.source.data[syname]
-            self.ymin = np.min([self.ymin, np.nanmin(y)])
-            self.ymax = np.max([self.ymax, np.nanmax(y)])
+    def plot_line(self, it):
+        # get which checkboxs are active
+        switch = self.lines_widget.active
+        # get order number
+        order_num = self.order_num
+        # get name
+        name = self.line_labels[it]
+        color = self.line_colors[it]
+        alpha = self.line_alphas[it]
+        lwidth = self.line_widths[it]
+        # get x and y values
+        sxname = 'x_{0}[{1}]'.format(name, order_num)
+        syname = 'flux_{0}[{1}]'.format(name, order_num)
+        # assign current values
+        cxname = 'current_{0}_x'.format(name)
+        cyname = 'current_{0}_y'.format(name)
+        # update current
+        self.source.data[cxname] = self.source.data[sxname]
+        self.source.data[cyname] = self.source.data[syname]
+        # adjust ymin and ymax
+        y = self.source.data[syname]
+        self.ymin = np.min([self.ymin, np.nanmin(y)])
+        self.ymax = np.max([self.ymax, np.nanmax(y)])
+        # only plot if active
+        if self.lines[it] is None and it in switch:
             # deal with debugging
             if DEBUG:
                 dargs = [name, order_num]
                 print('Plotting {0} [Order={1}]'.format(*dargs))
+            # plot line
+            line = self.figure.line(cxname, cyname, source=self.source,
+                                    color=color, alpha=alpha,
+                                    legend_label=name, line_width=lwidth)
+            # keep lines
+            self.lines[it] = line
 
-            if self.lines[it] is None:
-                # plot line
-                line = self.figure.line(cxname, cyname, source=self.source,
-                                        color=color, alpha=alpha,
-                                        legend_label=name)
-                # keep lines
-                self.lines[it] = line
-        # set x and y range
-        self.figure.y_range = Range1d(self.ymin, self.ymax)
-        self.figure.x_range = Range1d(self.xmin, self.xmax)
+    def logger(self, text):
+        print(text)
+        self.log_widget.text = str(LOG_HTML) + text
+
+    def reset_logger(self):
+        self.log_widget.text = str(LOG_HTML)
+
+    def tooltip(self) -> str:
+        # deal with no line widget yet
+        if self.lines_widget is None:
+            return ''
+        # add start of the box
+        tooltipstr = '<div>'
+        # get which checkboxs are active
+        switch = self.lines_widget.active
+        # loop around line labels (has to be all of them)
+        for it in range(len(self.line_labels)):
+            if it in switch:
+                name = self.line_labels[it]
+                cxname = 'current_{0}_x'.format(name)
+                cyname = 'current_{0}_y'.format(name)
+                color = self.line_hexcolors[it]
+                tooltipstr += e2ds_span.format(x=cxname, y=cyname, name=name,
+                                               color=color)
+        # add the end of the box
+        tooltipstr += '</div>'
+        # return the tool tip string
+        return tooltipstr
 
 
 def e2ds_plot(**kwargs) -> bokeh.models.Model:
