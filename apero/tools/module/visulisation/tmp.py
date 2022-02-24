5ae6a820bbf308cb33f032166327c8340e924b0a
Neil Cook <neil.james.cook@gmail.com>
2022-02-24 14:27:53 -0500

    update requirements for astro_visu

--- apero/tools/module/visulisation/tmp.py
+++ apero/tools/module/visulisation/tmp.py
@@ -1,18 +1,13 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 """
-# CODE NAME HERE
-
-# CODE DESCRIPTION HERE
+This is just a test code for the tmp py file written by apero_visu and
+visu_core do not use
 
 Created on 2022-02-22
 
 @author: cook
 """
-
-from bokeh.io import curdoc
-from apero.tools.module.visulisation import visu_plots
-
 from bokeh.io import curdoc
 from apero.tools.module.visulisation import visu_plots
 
