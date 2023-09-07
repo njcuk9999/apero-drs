3a0331d6136a15ad1ab4f1ef49b19d2e7797cd38
Neil Cook <neil.james.cook@gmail.com>
2023-09-07 10:47:24 -0400

    [APERO] apero_lbl_compile - skip done should be False - we should always recompute lbl_compile

--- apero/recipes/spirou/apero_lbl_compile_spirou.py
+++ apero/recipes/spirou/apero_lbl_compile_spirou.py
@@ -102,7 +102,7 @@ def __main__(recipe: DrsRecipe, params: ParamDict) -> Dict[str, Any]:
     kwargs['instrument'] = params['INSTRUMENT']
     kwargs['data_dir'] = params['LBL_PATH']
     kwargs['data_source'] = 'APERO'
-    kwargs['skip_done'] = params['INPUTS'].get('SKIP_DONE', True)
+    kwargs['skip_done'] = params['INPUTS'].get('SKIP_DONE', False)
     # deal with data type
     if objname in params.listp('LBL_SPECIFIC_DATATYPES', dtype=str):
         data_type = objname
