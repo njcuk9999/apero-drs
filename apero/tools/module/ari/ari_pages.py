c8e8b2cfe3f4871680748ec9ad0f2d3fc1f8e19e
Neil Cook <neil.james.cook@gmail.com>
2024-03-15 12:14:48 -0400

    [ARI] Edit html of recipe and finder pages

--- apero/tools/module/ari/ari_pages.py
+++ apero/tools/module/ari/ari_pages.py
@@ -757,6 +757,14 @@ def add_finder_table(params: ParamDict, data_dict: Dict[str, Any]):
     
     <div class="related" role="navigation" aria-label="related navigation">
       <h3>Navigation</h3>
+      <ul>
+        <li class="right" style="margin-right: 10px">
+          <a href="http://apero.exoplanets.ca/" title="Home">Home</a></li>
+        <li class="right" style="margin-right: 10px">
+          <a href="http://apero.exoplanets.ca/genindex.html" title="General Index"
+             accesskey="I">index</a></li>
+        <li class="nav-item nav-item-0"><a href="http://apero.exoplanets.ca/ari/home">APERO Reduction interface</a> &#187;</li>
+      </ul>
     </div>
     <div class="footer" role="contentinfo">
         &#169; Copyright {0}, Neil Cook.
@@ -898,6 +906,14 @@ def add_recipe_tables(params: ParamDict, table: Table, machine_name: str):
     
     <div class="related" role="navigation" aria-label="related navigation">
       <h3>Navigation</h3>
+      <ul>
+        <li class="right" style="margin-right: 10px">
+          <a href="http://apero.exoplanets.ca/" title="Home">Home</a></li>
+        <li class="right" style="margin-right: 10px">
+          <a href="http://apero.exoplanets.ca/genindex.html" title="General Index"
+             accesskey="I">index</a></li>
+        <li class="nav-item nav-item-0"><a href="http://apero.exoplanets.ca/ari/home">APERO Reduction interface</a> &#187;</li>
+      </ul>
     </div>
     <div class="footer" role="contentinfo">
         &#169; Copyright {0}, Neil Cook.
