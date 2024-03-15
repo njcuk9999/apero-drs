da199222055e8de432239f587eca4cf4dc5a71e7
Neil Cook <neil.james.cook@gmail.com>
2024-03-15 12:13:39 -0400

    [ARI] Edit html of recipe and finder pages

--- apero/tools/module/ari/ari_pages.py
+++ apero/tools/module/ari/ari_pages.py
@@ -693,6 +693,18 @@ def add_finder_table(params: ParamDict, data_dict: Dict[str, Any]):
     # set html body
     # Take directly from one of the sphinx pages (this is a massive hack)
     html_body1 = """
+      <div class="related" role="navigation" aria-label="related navigation">
+      <h3>Navigation</h3>
+      <ul>
+        <li class="right" style="margin-right: 10px">
+          <a href="http://apero.exoplanets.ca/" title="Home">Home</a></li>
+        <li class="right" style="margin-right: 10px">
+          <a href="http://apero.exoplanets.ca/genindex.html" title="General Index"
+             accesskey="I">index</a></li>
+        <li class="nav-item nav-item-0"><a href="http://apero.exoplanets.ca/ari/home">APERO Reduction interface</a> &#187;</li>
+      </ul>
+    </div>
+    
       <div class="pageheader">
 
       <ul>
@@ -815,6 +827,18 @@ def add_recipe_tables(params: ParamDict, table: Table, machine_name: str):
     # set html body
     # Take directly from one of the sphinx pages (this is a massive hack)
     html_body1 = """
+    <div class="related" role="navigation" aria-label="related navigation">
+      <h3>Navigation</h3>
+      <ul>
+        <li class="right" style="margin-right: 10px">
+          <a href="http://apero.exoplanets.ca/" title="Home">Home</a></li>
+        <li class="right" style="margin-right: 10px">
+          <a href="http://apero.exoplanets.ca/genindex.html" title="General Index"
+             accesskey="I">index</a></li>
+        <li class="nav-item nav-item-0"><a href="http://apero.exoplanets.ca/ari/home">APERO Reduction interface</a> &#187;</li>
+      </ul>
+    </div>
+    
     <div class="pageheader">
 
     <ul>
