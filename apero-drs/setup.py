cfb33f6dcdb87ffdfbfd4d0774a4f4cff7e8879a
Neil Cook <neil.james.cook@gmail.com>
2024-10-16 20:14:54 -0400

    [APERO] split apero into aperocore + apero

--- apero-drs/setup.py
+++ apero-drs/setup.py
@@ -8,6 +8,8 @@ Created on 2019-01-17 at 15:24
 @author: cook
 """
 import sys
+import os
+from pathlib import Path
 
 from setuptools import setup
 
@@ -16,9 +18,12 @@ from setuptools import setup
 # =============================================================================
 # Define script name
 __NAME__ = 'setup.py'
-# dev mode
-DEV_MODE = False
+# Get the package directory from this file name
+PACKAGE_DIR = Path(os.path.dirname(os.path.realpath(__file__)))
 
+SCRIPT_PATHS = [os.path.join('apero', 'tools', 'recipes', 'bin'),
+                os.path.join('apero', 'tools', 'recipes', 'dev'),
+                os.path.join('apero', 'tools', 'recipes', 'utils')]
 
 # =============================================================================
 # Define functions
@@ -63,6 +68,26 @@ def load_requirements() -> list:
     return modules
 
 
+
+def load_scripts() -> list:
+    """
+    Load scripts from file
+    :return:
+    """
+    scripts = []
+    # loop around script paths
+    for strpath in SCRIPT_PATHS:
+        path = PACKAGE_DIR.joinpath(strpath)
+        # loop around files in path
+        for filepath in path.iterdir():
+            if filepath.name == '__init__.py':
+                continue
+            if filepath.is_file() and os.access(filepath, os.X_OK):
+                scripts.append(os.path.relpath(str(filepath)))
+    # return modules
+    return scripts
+
+
 # =============================================================================
 # Start of code
 # =============================================================================
@@ -72,6 +97,7 @@ if __name__ == "__main__":
     setup(name='apero',
           version=get_version(),
           packages=['apero'],
+          scripts=load_scripts(),
           url='http://apero.exoplanets.ca',
           license='MIT',
           author='Neil Cook',
