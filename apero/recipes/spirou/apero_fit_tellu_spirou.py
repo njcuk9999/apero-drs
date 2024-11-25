c4d40376a2f9ffbdd09572da8231a125c75c7906
Neil Cook <neil.james.cook@gmail.com>
2024-11-25 11:25:06 -0500

    [APERO] telluric correction - sky corr

--- apero/recipes/spirou/apero_fit_tellu_spirou.py
+++ apero/recipes/spirou/apero_fit_tellu_spirou.py
@@ -6,6 +6,8 @@ apero_fit_tellu_spirou.py [obs dir] [files]
 Using all transmission files, we fit the absorption of a given science
 observation.
 
+Created on 2019-09-05 at 14:58
+
 @author: cook
 """
 from typing import Any, Dict, List, Optional, Tuple, Union
@@ -56,7 +58,7 @@ textentry = lang.textentry
 def main(obs_dir: Optional[str] = None, files: Optional[List[str]] = None,
          **kwargs) -> Union[Dict[str, Any], Tuple[DrsRecipe, ParamDict]]:
     """
-    Main function for apero_fit_tellu_spirou.py
+    Main function for apero_fit_tellu
 
     :param obs_dir: string, the night name sub-directory
     :param files: list of strings or string, the list of files to process
@@ -233,6 +235,36 @@ def __main__(recipe: DrsRecipe, params: ParamDict) -> Dict[str, Any]:
         template_props = telluric.shift_template(params, recipe, image,
                                                  template_props, refprops,
                                                  wprops, bprops)
+        # ------------------------------------------------------------------
+        # mask bad regions
+        # ------------------------------------------------------------------
+        # Some regions cannot be telluric corrected and the data should be
+        #   just set to NaNs to avoid bad corrections
+        infile.data = telluric.mask_bad_regions(params, infile.data,
+                                                wprops['WAVEMAP'],
+                                                pconst=pconst)
+
+        # ------------------------------------------------------------------
+        # apply sky correction
+        # ------------------------------------------------------------------
+        if dprtype in params.listp('ALLOWED_SKYCORR_DPRTYPES', dtype=str):
+            # correct sky using model and B fiber
+            scprops = telluric.correct_sky_with_ref(params, recipe, infile,
+                                                    wprops, rawfiles, combine,
+                                                    calibdbm, telludbm)
+            # update infile
+            infile.data = scprops[f'CORR_EXT_{fiber}']
+            # turn off cleaning of OH lines in pre-cleaning
+            clean_ohlines = False
+        else:
+            # correct sky using model and B fiber
+            scprops = telluric.correct_sky_no_ref(params, recipe, infile,
+                                                  wprops, rawfiles, combine,
+                                                  calibdbm, telludbm)
+            # update infile
+            infile.data = scprops[f'CORR_EXT_{fiber}']
+            # turn off cleaning of OH lines in pre-cleaning
+            clean_ohlines = False
 
         # ------------------------------------------------------------------
         # telluric pre-cleaning
