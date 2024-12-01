95d04ec4452705badaaae7c0760f951023ce3254
Neil Cook <neil.james.cook@gmail.com>
2024-12-01 09:08:09 -0500

    [APERO] update version+changelog

--- apero/base/base.py
+++ apero/base/base.py
@@ -33,10 +33,10 @@ from astropy.time import Time, TimeDelta
 __PACKAGE__ = 'apero'
 __PATH__ = Path(__file__).parent.parent
 __INSTRUMENT__ = 'None'
-__version__ = '0.7.291'
+__version__ = '0.7.292'
 __author__ = ['N. Cook', 'E. Artigau', 'F. Bouchy', 'M. Hobson', 'C. Moutou',
               'I. Boisse', 'E. Martioli']
-__date__ = '2024-10-29'
+__date__ = '2024-12-01'
 __release__ = 'beta release'
 # do this once per drs import
 __now__ = Time.now()
