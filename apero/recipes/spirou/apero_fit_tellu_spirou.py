3427f8647d950fc9dc9408188091a457be27ea37
Neil Cook <neil.james.cook@gmail.com>
2024-12-12 10:00:58 -0500

    [APERO] Nirps needs sky_model_he and spirou needs to still use sky_pcas (until we have a sky model)

--- apero/recipes/spirou/apero_fit_tellu_spirou.py
+++ apero/recipes/spirou/apero_fit_tellu_spirou.py
@@ -247,24 +247,24 @@ def __main__(recipe: DrsRecipe, params: ParamDict) -> Dict[str, Any]:
         # ------------------------------------------------------------------
         # apply sky correction
         # ------------------------------------------------------------------
-        if dprtype in params.listp('ALLOWED_SKYCORR_DPRTYPES', dtype=str):
-            # correct sky using model and B fiber
-            scprops = telluric.correct_sky_with_ref(params, recipe, infile,
-                                                    wprops, rawfiles, combine,
-                                                    calibdbm, telludbm)
-            # update infile
-            infile.data = scprops[f'CORR_EXT_{fiber}']
-            # turn off cleaning of OH lines in pre-cleaning
-            clean_ohlines = False
-        else:
-            # correct sky using model and B fiber
-            scprops = telluric.correct_sky_no_ref(params, recipe, infile,
-                                                  wprops, rawfiles, combine,
-                                                  calibdbm, telludbm)
-            # update infile
-            infile.data = scprops[f'CORR_EXT_{fiber}']
-            # turn off cleaning of OH lines in pre-cleaning
-            clean_ohlines = False
+        # if dprtype in params.listp('ALLOWED_SKYCORR_DPRTYPES', dtype=str):
+        #     # correct sky using model and B fiber
+        #     scprops = telluric.correct_sky_with_ref(params, recipe, infile,
+        #                                             wprops, rawfiles, combine,
+        #                                             calibdbm, telludbm)
+        #     # update infile
+        #     infile.data = scprops[f'CORR_EXT_{fiber}']
+        #     # turn off cleaning of OH lines in pre-cleaning
+        #     clean_ohlines = False
+        # else:
+        #     # correct sky using model and B fiber
+        #     scprops = telluric.correct_sky_no_ref(params, recipe, infile,
+        #                                           wprops, rawfiles, combine,
+        #                                           calibdbm, telludbm)
+        #     # update infile
+        #     infile.data = scprops[f'CORR_EXT_{fiber}']
+        #     # turn off cleaning of OH lines in pre-cleaning
+        #     clean_ohlines = False
 
         # ------------------------------------------------------------------
         # telluric pre-cleaning
