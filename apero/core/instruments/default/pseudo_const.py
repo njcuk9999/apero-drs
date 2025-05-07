7c179acf729adcf8a8d90a26f3520847b1f7f7e7
cook <neil.james.cook@gmail.com>
2025-05-07 11:03:10 -0400

    [APERO] Update astrometrics to have DR3 + deal with bug with found and unfound objects

--- apero/core/instruments/default/pseudo_const.py
+++ apero/core/instruments/default/pseudo_const.py
@@ -1530,7 +1530,7 @@ class DefaultPseudoConstants:
                    'https://gea.esac.esa.int/tap-server/tap')
         tap_dict['Gaia DR3 '] = dict()
         tap_dict['Gaia DR3 ']['QUERY'] = QUERY1.format(**qkargs)
-        tap_dict['Gaia DR3 ']['URL'] = str(params['TAP_GAIA_EDR3_URL'])
+        tap_dict['Gaia DR3 ']['URL'] = str(params['TAP_GAIA_DR3_URL'])
         # ---------------------------------------------------------------------
         # Gaia EDR3
         # ---------------------------------------------------------------------
