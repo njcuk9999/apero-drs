564db818d4b1e0e1c27fbbbb49866d9b66ae78a6
Neil Cook <neil.james.cook@gmail.com>
2025-05-16 08:33:45 -0400

    citation + database test + poster plots

--- apero/core/math/gen_math.py
+++ apero/core/math/gen_math.py
@@ -938,7 +938,8 @@ def square_medbin(image: np.ndarray, binexpo: int = 8) -> np.ndarray:
 
 
 def lowpassfilter(input_vect: np.ndarray, width: int = 101,
-                  k=1, frac_valid_min=0) -> np.ndarray:
+                  k=1, frac_valid_min=0,
+                  trim_valid: bool = False) -> np.ndarray:
     """
     Computes a low-pass filter of an input vector.
 
@@ -1021,6 +1022,18 @@ def lowpassfilter(input_vect: np.ndarray, width: int = 101,
     # splining the vector
     spline = InterpolatedUnivariateSpline(xmed, ymed, k=k, ext=3)
     lowpass = spline(np.arange(len(input_vect)))
+
+    # deal with trimming to valid domain
+    if trim_valid:
+        # get the valid regions of this order
+        valid_pos = np.where(np.isfinite(input_vect))[0]
+        # get the start and end of the valid region (so we don't over
+        # interpolate the edges)
+        min_valid, max_valid = np.min(valid_pos), np.max(valid_pos)
+        # fill the invalid regions with nans
+        lowpass[0:min_valid] = np.nan
+        lowpass[max_valid:] = np.nan
+
     # return the low pass filtered input vector
     return lowpass
 
