31242433e18eee9530ce76dc86f0519b8ba5690c
Neil Cook <neil.james.cook@gmail.com>
2025-12-01 09:26:52 -0500

    Update changelog notes, version and date, remove use of pkg_resources

--- apero/base/base.py
+++ apero/base/base.py
@@ -33,10 +33,10 @@ from astropy.time import Time, TimeDelta
 __PACKAGE__ = 'apero'
 __PATH__ = Path(__file__).parent.parent
 __INSTRUMENT__ = 'None'
-__version__ = '0.7.295'
+__version__ = '0.7.296'
 __author__ = ['N. Cook', 'E. Artigau', 'F. Bouchy', 'M. Hobson', 'C. Moutou',
               'I. Boisse', 'E. Martioli']
-__date__ = '2025-10-06'
+__date__ = '2025-12-01'
 __release__ = 'beta release'
 # do this once per drs import
 __now__ = Time.now()
