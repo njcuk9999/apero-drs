315a36546998871e0afa9a85e7a6c6d87783a0c5
Neil Cook <neil.james.cook@gmail.com>
2026-01-21 11:46:48 -0500

    [APERO.apero_postprocess] Update header to include KW_OUTPUT key with DrsOutFile name

--- apero-drs/apero/core/drs_file.py
+++ apero-drs/apero/core/drs_file.py
@@ -7761,6 +7761,8 @@ class DrsOutFile(DrsInputFile):
         # finally add the number of extensions to the primary header
         nextend_comment = 'Number of standard extensions'
         header['NEXTEND'] = (len(self.extensions) - 1, nextend_comment)
+        # Update the KW_OUTPUT key to the DrsOutFile name
+        header[params['KW_OUTPUT'][0]] = str(self.name)
         # add header back to extensions
         self.extensions[0].header = header
 
