e88337d75ea980e4bc4e8295cb309b80a2e295c5
Neil Cook <neil.james.cook@gmail.com>
2026-02-20 10:51:17 -0500

    compress files where possible (to get below 100Mb per file)

--- apero/core/instruments/ilocater/default_constants.py
+++ apero/core/instruments/ilocater/default_constants.py
@@ -387,7 +387,7 @@ PP_DARK_MED_BINNUM.value = 32
 
 #   Defines the pp hot pixel file (located in the data folder)
 PP_HOTPIX_FILE = PP_HOTPIX_FILE.copy(__NAME__)
-PP_HOTPIX_FILE.value = 'static_hotpix_ilocater.fits'
+PP_HOTPIX_FILE.value = 'static_hotpix_ilocater.fits.gz'
 
 # Define the pp hot pixel format
 PP_HOTPIX_FMT = PP_HOTPIX_FMT.copy(__NAME__)
@@ -395,7 +395,7 @@ PP_HOTPIX_FMT.value = 'fits'
 
 #   Defines the pp amplifier bias model (located in the data folder)
 PP_AMP_ERROR_MODEL = PP_AMP_ERROR_MODEL.copy(__NAME__)
-PP_AMP_ERROR_MODEL.value = 'static_dark_curr_ilocater.fits'
+PP_AMP_ERROR_MODEL.value = 'static_dark_curr_ilocater.fits.gz'
 
 # Defines the pp led flat file (located in the data folder)
 PP_LED_FLAT_FILE = PP_LED_FLAT_FILE.copy(__NAME__)
