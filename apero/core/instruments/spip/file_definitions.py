755e5c5c7e12bd285c4f63a9f3f5beec849728a9
Neil Cook <neil.james.cook@gmail.com>
2026-03-31 13:07:14 -0400

    [APERO.file_defintiions] LBL_MASK is not required

--- apero/core/instruments/spip/file_definitions.py
+++ apero/core/instruments/spip/file_definitions.py
@@ -1664,10 +1664,11 @@ lbl_file.addset(lbl_template_file)
 
 # lbl mask file
 lbl_mask_file = drs_linput('LBL_MASK',
-                          filetype='.fits', path='masks',
-                          basename='LBL_MASK_{obj}_spip', datatype='table',
-                          outclass=lbl_ofile,
-                          description='Telluric mask file')
+                           filetype='.fits', path='masks',
+                           basename='LBL_MASK_{obj}_spip', datatype='table',
+                           outclass=lbl_ofile,
+                           description='Telluric mask file',
+                           required=False)
 lbl_file.addset(lbl_mask_file)
 
 # lbl fits files
