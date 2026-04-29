f221eae30b05fd6cf780a78283228076d4bdd2fe
Neil Cook <neil.james.cook@gmail.com>
2026-04-29 13:26:40 -0400

    [APERO.VISU] Add warnings handling for median calculations in visu_info_plots

--- apero/tools/module/visulisation/visu_info_plots.py
+++ apero/tools/module/visulisation/visu_info_plots.py
@@ -560,8 +560,9 @@ def lbl_trumpet_plot(frame, wavemap: np.ndarray, y: np.ndarray,
                    marker='o', alpha=0.5, color=on_color,
                    label=mask_on_label, ls='None')
     # set limits to 5 sigma away from median
-    median = np.nanmedian(y)
-    low, high = np.nanpercentile(y, [low_percentile, high_percentile])
+    with warnings.catch_warnings(record=True) as _:
+        median = np.nanmedian(y)
+        low, high = np.nanpercentile(y, [low_percentile, high_percentile])
     frame.set(ylim=[low, high])
     # plot the median line
     frame.axhline(median, color='red', ls='--',
@@ -676,7 +677,7 @@ def post_drs_post_s(params: ParamDict, filename: str, identity: str = ''):
     # get main fiber
     fiber = get_main_fiber()
     # get the header
-    header = fits.getheader(filename, extname='UniformVelocity'.format(fiber))
+    header = fits.getheader(filename, extname='UniformVelocity')
     # get the data for this plot
     table = Table.read(filename, hdu='UniformVelocity')
     # get columns from table
@@ -771,7 +772,8 @@ def post_drs_post_v(params: ParamDict, filename: str, identity: str = ''):
         # get the combined CCF for this file
         ccf_row = np.asarray(table['CCF{0:02d}'.format(row)], dtype=float)
         # normalize ccf
-        ccf_row = ccf_row / np.nanmedian(ccf_row)
+        with warnings.catch_warnings(record=True) as _:
+            ccf_row = ccf_row / np.nanmedian(ccf_row)
         # push into vector
         all_ccf[row] = ccf_row
     # -----------------------------------------------------------------
@@ -792,7 +794,8 @@ def post_drs_post_v(params: ParamDict, filename: str, identity: str = ''):
         # y2 1sig is the 84th percentile of all ccfs
         ccf_props['y2_2sig'] = np.nanpercentile(all_ccf, upper_sig2, axis=0)
         # med ccf is the median ccf (50th percentile)
-        ccf_props['med_ccf'] = np.nanmedian(all_ccf, axis=0)
+        with warnings.catch_warnings(record=True) as _:
+            ccf_props['med_ccf'] = np.nanmedian(all_ccf, axis=0)
         # get other properties using the ari core function
         ccf_props = ari_core.fit_ccf(ccf_props)
     # plot ccf
