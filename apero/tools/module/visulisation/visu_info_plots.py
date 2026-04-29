f9c5bf5888b8e893b97d061c3bbb54ae1ce0743f
Neil Cook <neil.james.cook@gmail.com>
2026-04-29 11:46:09 -0400

    [APERO.VISU] Add check for Telluric correction data presence in visu_info_plots.py

--- apero/tools/module/visulisation/visu_info_plots.py
+++ apero/tools/module/visulisation/visu_info_plots.py
@@ -683,7 +683,7 @@ def post_drs_post_s(params: ParamDict, filename: str, identity: str = ''):
     spectrum = np.array(table['Flux{0}'.format(fiber)])
     # note tcorr may not be present (if it failed QC)
     tcorr_key = 'Flux{0}TelluCorrected'.format(fiber)
-    if tcorr_key in table:
+    if tcorr_key in table.colnames:
         tcorr = np.array(table[tcorr_key])
     # if we don't have any tcorr data then just fill with nans
     else:
